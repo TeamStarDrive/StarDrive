b21082b8e0922779fdcd320e3542ce1fe72c2f38
Michael Shoots <michael.shoots@live.com>
2014-06-26 13:10:44 -0400

    My fix to beam issue; beams have default duration of 2 except when a weapon is give beam duration value greater than 0
    --HG--
    branch : McShooterz

--- Ship_Game/Beam.cs
+++ Ship_Game/Beam.cs
@@ -46,7 +46,8 @@ namespace Ship_Game
 
 		public bool followMouse;
 
-		public float Duration;
+        //added by McShooterz: changed back to default
+		public float Duration = 2f;
 
 		public float BeamOffsetAngle;
 
