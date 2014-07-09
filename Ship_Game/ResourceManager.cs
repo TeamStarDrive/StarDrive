d215824d892440abaa9274009d120b5789da7b1c
Michael Shoots <michael.shoots@live.com>
2014-07-09 14:12:06 -0400

    StarterShips loads from mods instead of Content folder, if mod has folder
    --HG--
    branch : McShooterz2

--- Ship_Game/ResourceManager.cs
+++ Ship_Game/ResourceManager.cs
@@ -1891,24 +1891,33 @@ namespace Ship_Game
 		public static void LoadShips()
 		{
 			Ship_Game.ResourceManager.ShipsDict.Clear();
-			FileInfo[] textList = Ship_Game.ResourceManager.GetFilesFromDirectory("Content/StarterShips");
-			XmlSerializer serializer0 = new XmlSerializer(typeof(ShipData));
-			FileInfo[] fileInfoArray = textList;
-			for (int i = 0; i < (int)fileInfoArray.Length; i++)
-			{
-				FileStream stream = fileInfoArray[i].OpenRead();
-				ShipData newShipData = (ShipData)serializer0.Deserialize(stream);
-				stream.Close();
-				stream.Dispose();
-				Ship newShip = Ship.CreateShipFromShipData(newShipData);
-				newShip.SetShipData(newShipData);
-				newShip.reserved = true;
-				if (newShip.InitForLoad())
-				{
-					newShip.InitializeStatus();
-					Ship_Game.ResourceManager.ShipsDict[newShipData.Name] = newShip;
-				}
-			}
+            //Added by McShooterz: Changed how StarterShips loads from mod if folder exists
+            XmlSerializer serializer0 = new XmlSerializer(typeof(ShipData));
+            FileInfo[] textList;
+            if (GlobalStats.ActiveMod != null && Directory.Exists(string.Concat(Ship_Game.ResourceManager.WhichModPath, "/StarterShips")))
+            {
+                textList = Ship_Game.ResourceManager.GetFilesFromDirectory(string.Concat("Mods/", GlobalStats.ActiveMod.ModPath, "/StarterShips/"));
+            }
+            else
+            {
+                textList = Ship_Game.ResourceManager.GetFilesFromDirectory("Content/StarterShips");           
+            }
+            FileInfo[] fileInfoArray = textList;
+            for (int i = 0; i < (int)fileInfoArray.Length; i++)
+            {
+                FileStream stream = fileInfoArray[i].OpenRead();
+                ShipData newShipData = (ShipData)serializer0.Deserialize(stream);
+                stream.Close();
+                stream.Dispose();
+                Ship newShip = Ship.CreateShipFromShipData(newShipData);
+                newShip.SetShipData(newShipData);
+                newShip.reserved = true;
+                if (newShip.InitForLoad())
+                {
+                    newShip.InitializeStatus();
+                    Ship_Game.ResourceManager.ShipsDict[newShipData.Name] = newShip;
+                }
+            }
 			FileInfo[] filesFromDirectory = Ship_Game.ResourceManager.GetFilesFromDirectory("Content/SavedDesigns");
 			for (int j = 0; j < (int)filesFromDirectory.Length; j++)
 			{
