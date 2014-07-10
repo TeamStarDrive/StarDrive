a275549770d66d97a14c805b065cdded5820ed8b
Michael Shoots <michael.shoots@live.com>
2014-07-10 19:11:50 -0400

    New file support, /ShipNames/ShipNames.xml: defines random names that can be assigned to ships when built by empire and role, additionally new ship UI information, ship design and role under ship name.
    --HG--
    branch : McShooterz2

---
+++ Ship_Game/ShipNames.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ship_Game
+{
+    public class ShipNames
+    {
+
+        public List<EmpireEntry> EmpireEntries;
+
+        public struct EmpireEntry
+        {
+            public string ShipType;
+            public List<RoleNames> NamesByRoles;
+        };
+
+        public struct RoleNames
+        {
+            public string Role;
+            public List<string> Names;
+        };
+
+        //Check to see if there are names to use
+        public bool CheckForName(string Empire, string Role)
+        {
+            for (int i = 0; i < EmpireEntries.Count; i++)
+            {
+                if (Empire == EmpireEntries[i].ShipType)
+                {
+                    for (int j = 0; j < EmpireEntries[i].NamesByRoles.Count; j++)
+                    {
+                        if (Role == EmpireEntries[i].NamesByRoles[j].Role && EmpireEntries[i].NamesByRoles[j].Names.Count != 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public string GetName(string Empire, string Role)
+        {
+            for (int i = 0; i < EmpireEntries.Count; i++)
+            {
+                if (Empire == EmpireEntries[i].ShipType)
+                {
+                    for (int j = 0; j < EmpireEntries[i].NamesByRoles.Count; j++)
+                    {
+                        if (Role == EmpireEntries[i].NamesByRoles[j].Role)
+                        {
+                            Random r = new Random();
+                            int randint = r.Next(0, EmpireEntries[i].NamesByRoles[j].Names.Count);
+                            return EmpireEntries[i].NamesByRoles[j].Names[randint];
+                        }
+                    }
+                }
+            }
+            return "failed to name";
+        }
+    }
+}
