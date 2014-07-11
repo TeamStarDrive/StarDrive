eb9108264ab9bd88927f937f3934fa96b0fd5709
Michael Shoots <michael.shoots@live.com>
2014-07-11 13:46:28 -0400

    Constructors for new classes
    --HG--
    branch : McShooterz2

--- Ship_Game/HostileFleets.cs
+++ Ship_Game/HostileFleets.cs
@@ -8,12 +8,17 @@ namespace Ship_Game
     //Added by McShooterz: defines new enemy fleets to spawn in place of Remnant
     public class HostileFleets
     {
-        public List<Fleet> Fleets = new List<Fleet>();
+        public List<Fleet> Fleets;
 
         public struct Fleet
         {
             public string Empire;
             public List<string> Ships;
+        }
+
+        public HostileFleets()
+        {
+            this.Fleets = new List<Fleet>();
         }
     }
 }
