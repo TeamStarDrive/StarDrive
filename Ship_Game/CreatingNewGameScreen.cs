0e9634aa08e300b1bdf5e5327b3408384576413a
Michael Shoots <michael.shoots@live.com>
2014-07-15 22:04:02 -0400

    random naming support for AI flagship
    --HG--
    branch : McShooterz2

--- Ship_Game/CreatingNewGameScreen.cs
+++ Ship_Game/CreatingNewGameScreen.cs
@@ -549,6 +549,9 @@ namespace Ship_Game
                                     ship3.Position = planet1.Position + new Vector2(-2500f, -2000f);
                                     ship3.loyalty = index;
                                     ship3.Initialize();
+                                    //Added by McShooterz: Starting ship support for automatic naming
+                                    if (GlobalStats.ActiveMod != null && Ship_Game.ResourceManager.ShipNames.CheckForName(ship3.loyalty.data.Traits.ShipType, ship3.Role))
+                                        ship3.VanityName = Ship_Game.ResourceManager.ShipNames.GetName(ship3.loyalty.data.Traits.ShipType, ship3.Role);
                                     ship3.SetHome(planet1);
                                     ship3.DoOrbit(planet1);
                                     ship3.GetSO().World = Matrix.CreateTranslation(new Vector3(ship3.Position, 0.0f));
