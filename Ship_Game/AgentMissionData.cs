78b7ccc1f433be83682c76a29e0e4ce7cd678298
Michael Shoots <michael.shoots@live.com>
2014-07-28 11:02:08 -0400

    Added more agent values to be modified in AgentMissionData
    --HG--
    branch : feature/Push here instead of develop

--- Ship_Game/AgentMissionData.cs
+++ Ship_Game/AgentMissionData.cs
@@ -10,6 +10,10 @@ namespace Ship_Game
     {
         public short AgentCost = 200;
         public short ExpPerLevel = 2;
+        public float RandomLevelBonus = 5f;
+        public float RandomDefenceLevelBonus = 3f;
+        public float MinRollPerLevel = 3f;
+        public float MaxRoll = 90f;
 
         //Training
         public short TrainingCost = 50;
