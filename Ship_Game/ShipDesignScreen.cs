290478c5721e4e6cf9b7524ba613effe0751f3fc
Michael Shoots <michael.shoots@live.com>
2014-08-13 21:47:06 -0400

    Change troop strength to float, changed HealperTurn to a float, made troop healing a calculation per update, added heal per turn to modules in ship design screen
    --HG--
    branch : McShooterz2

--- Ship_Game/ShipDesignScreen.cs
+++ Ship_Game/ShipDesignScreen.cs
@@ -1914,6 +1914,16 @@ namespace Ship_Game
                             this.DrawStat(ref modTitlePos, Localizer.Token(6011), (float)(-mod.PowerDrawAtWarp), "Power draw persecond when Warp engaged");
                             modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
                         }
+                        if (mod.TroopCapacity > 0f)
+                        {
+                            this.DrawStat(ref modTitlePos, Localizer.Token(336), (float)mod.TroopCapacity, 173);
+                            modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
+                        }
+                        if (mod.HealPerTurn > 0)
+                        {
+                            this.DrawStat(ref modTitlePos, Localizer.Token(6129), mod.HealPerTurn, 174);
+                            modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
+                        }
 						return;
 					}
                     if (mod.ModuleType == ShipModuleType.Hangar)
@@ -1961,8 +1971,6 @@ namespace Ship_Game
                     }
 					else
 					{
-                        this.DrawStat(ref modTitlePos, Localizer.Token(128), (float)mod.Cost * UniverseScreen.GamePaceStatic, 84);
-                        modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
 						this.DrawStat(ref modTitlePos, Localizer.Token(123), (float)EmpireManager.GetEmpireByName(ShipDesignScreen.screen.PlayerLoyalty).data.MassModifier * mod.Mass, 79);
 						modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
 						this.DrawStat(ref modTitlePos, Localizer.Token(124), (float)mod.HealthMax + mod.HealthMax * (float)EmpireManager.GetEmpireByName(ShipDesignScreen.screen.PlayerLoyalty).data.Traits.ModHpModifier, 80);
@@ -1985,6 +1993,13 @@ namespace Ship_Game
 						}
 						modTitlePos.X = modTitlePos.X + 152f;
 						modTitlePos.Y = starty;
+                        this.DrawStat(ref modTitlePos, Localizer.Token(128), (float)mod.Cost * UniverseScreen.GamePaceStatic, 84);
+                        modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
+                        if (mod.HealPerTurn > 0)
+                        {
+                            this.DrawStat(ref modTitlePos, Localizer.Token(6129), mod.HealPerTurn, 174);
+                            modTitlePos.Y = modTitlePos.Y + (float)Fonts.Arial12Bold.LineSpacing;
+                        }
                         if (mod.TransporterRange > 0)
                         {
                             this.DrawStat(ref modTitlePos, Localizer.Token(126), (float)mod.TransporterRange, 168);
