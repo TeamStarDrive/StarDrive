3c7272d3a1826c2080d999e2820b6ce0c7211106
Michael Shoots <michael.shoots@live.com>
2014-09-22 12:09:18 -0400

    Fixes to issues with changing when stats are calculated for ships.
    --HG--
    branch : McShooterz2

--- Ship_Game/Goal.cs
+++ Ship_Game/Goal.cs
@@ -762,7 +762,7 @@ namespace Ship_Game
                             if (ResourceManager.ShipsDict[index].Role == "scout")
                                 list2.Add(ResourceManager.ShipsDict[index]);
                         }
-                        IOrderedEnumerable<Ship> orderedEnumerable = Enumerable.OrderByDescending<Ship, float>((IEnumerable<Ship>)list2, (Func<Ship, float>)(ship => ship.PowerFlowMax - ship.PowerDraw));
+                        IOrderedEnumerable<Ship> orderedEnumerable = Enumerable.OrderByDescending<Ship, float>((IEnumerable<Ship>)list2, (Func<Ship, float>)(ship => ship.PowerFlowMax - ship.ModulePowerDraw));
                         if (Enumerable.Count<Ship>((IEnumerable<Ship>)orderedEnumerable) <= 0)
                             break;
                         planet1.ConstructionQueue.Add(new QueueItem()
