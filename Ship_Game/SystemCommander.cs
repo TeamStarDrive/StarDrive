dc0b11098eeead09107cd49d1ee9d02d730386e1
crunchygremlin <crunchygremlin@gmail>
2014-10-06 21:57:47 -0700

    add hashset back to system commander.
    --HG--
    branch : feature/Push here instead of develop

--- Ship_Game/SystemCommander.cs
+++ Ship_Game/SystemCommander.cs
@@ -39,7 +39,7 @@ namespace Ship_Game
         public void AssignTargets()
         {
             this.EnemyClumpsDict.Clear();
-            List<Ship> ShipsAlreadyConsidered = new List<Ship>();
+            HashSet<Ship> ShipsAlreadyConsidered = new HashSet<Ship>();
             foreach (KeyValuePair<Guid, Ship> entry in this.ShipsDict)
             {
                 Ship ship = entry.Value;
@@ -83,7 +83,7 @@ namespace Ship_Game
                     from clumpPos in ClumpsList
                     orderby Vector2.Distance(this.system.Position, clumpPos.Center)
                     select clumpPos;
-                List<Ship> AssignedShips = new List<Ship>();
+                HashSet<Ship> AssignedShips = new HashSet<Ship>();
                 foreach (Ship enemy in this.EnemyClumpsDict[distanceSorted.First<Ship>()])
                 {
                     float AssignedStr = 0f;
