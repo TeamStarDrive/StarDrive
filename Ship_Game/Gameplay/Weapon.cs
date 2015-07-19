386d4ec86fdc5f4df9856810cab0e6e10008c3f4
crunchygremlin <crunchygremlin@gmail>
2015-07-19 15:31:59 -0700

    Think the fire code is all rebuilt. PD might need a little work. fixed a few things along the way.
    --HG--
    branch : release/july

--- Ship_Game/Gameplay/Weapon.cs
+++ Ship_Game/Gameplay/Weapon.cs
@@ -1055,7 +1055,8 @@ namespace Ship_Game.Gameplay
 
 		public virtual void Fire(Vector2 direction, GameplayObject target)
 		{
-            if (this.owner.engineState == Ship.MoveState.Warp || this.timeToNextFire > 0f)
+            
+            if (this.owner.engineState == Ship.MoveState.Warp || this.timeToNextFire > 0f ||! this.owner.CheckRangeToTarget(this,target))
 				return;
 			this.owner.InCombatTimer = 15f;
 
@@ -1220,7 +1221,7 @@ namespace Ship_Game.Gameplay
 
 		public virtual void FireSalvo(Vector2 direction, GameplayObject target)
 		{
-			if (this.owner.engineState == Ship.MoveState.Warp)
+			if (this.owner.engineState == Ship.MoveState.Warp ) //|| (this.owner !=null && this.owner.CheckIfInsideFireArc(this,target)))
 			{
 				return;
 			}
@@ -1567,14 +1568,15 @@ namespace Ship_Game.Gameplay
 					continue;
 				}
                 //this.FireSalvo(salvo.Direction, this.SalvoTarget);
-                if (this.SalvoTarget != null)
+                
+                if (this.SalvoTarget !=null && this.owner.CheckIfInsideFireArc(this,SalvoTarget))
                 {
                     if (this.Tag_Guided)
                         this.FireSalvo(salvo.Direction, SalvoTarget);
                     else
                         this.GetOwner().GetAI().CalculateAndFire(this, SalvoTarget, true);
                 }
-                else
+                else if (this.SalvoTarget != null)
                     this.FireSalvo(salvo.Direction, null);
 				this.SalvoList.QueuePendingRemoval(salvo);
 			}
