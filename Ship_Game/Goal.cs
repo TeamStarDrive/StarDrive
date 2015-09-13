1e599693a358450ac9e8ddcbe78e894dce9782e5
fbedard007 <fbedard@videotron.ca>
2015-09-13 10:20:10 -0400

    Should prevent crash when building ship not in ShipsDict. (I think it came from game saved with 822).
    --HG--
    branch : feature/september features

--- Ship_Game/Goal.cs
+++ Ship_Game/Goal.cs
@@ -225,6 +225,8 @@ namespace Ship_Game
             switch (this.Step)
             {
                 case 0:
+                    if (this.beingBuilt == null)
+                        this.beingBuilt = ResourceManager.ShipsDict[this.ToBuildUID];
                     Planet planet1 = (Planet)null;
                     List<Planet> list = new List<Planet>();
                     foreach (Planet planet2 in this.empire.GetPlanets())
@@ -246,7 +248,7 @@ namespace Ship_Game
                             planet1 = planet2;
                         }
                     }
-                    if (planet1 == null || this.beingBuilt == null)
+                    if (planet1 == null)
                         break;
                     this.PlanetBuildingAt = planet1;
                         planet1.ConstructionQueue.Add(new QueueItem()
@@ -286,6 +288,8 @@ namespace Ship_Game
             switch (this.Step)
             {
                 case 0:
+                    if (this.beingBuilt == null)
+                        this.beingBuilt = ResourceManager.ShipsDict[this.ToBuildUID];
                     Planet planet1 = (Planet)null;
                     List<Planet> list = new List<Planet>();
                     foreach (Planet planet2 in this.empire.GetPlanets())
@@ -296,7 +300,7 @@ namespace Ship_Game
                     int num1 = 9999999;
                     foreach (Planet planet2 in list)
                     {
-                        if (planet2.CombatTimer > 0f)  //fbedard
+                        if (planet2.ParentSystem.combatTimer > 0f)  //fbedard
                         {
                             int num2 = 0;
                             foreach (QueueItem queueItem in (List<QueueItem>)planet2.ConstructionQueue)
@@ -310,9 +314,23 @@ namespace Ship_Game
                             }
                         }
                     }
-                    if (planet1 == null || this.beingBuilt == null)
+                    if (planet1 == null)
+                        foreach (Planet planet2 in list)
+                        {
+                            int num2 = 0;
+                            foreach (QueueItem queueItem in (List<QueueItem>)planet2.ConstructionQueue)
+                                num2 += (int)((queueItem.Cost - queueItem.productionTowards) / planet2.GetMaxProductionPotential());//planet2.NetProductionPerTurn);
+                            if (planet2.ConstructionQueue.Count == 0)
+                                num2 = (int)((this.beingBuilt.GetCost(this.empire) - planet2.ProductionHere) / planet2.GetMaxProductionPotential());//planet2.NetProductionPerTurn);
+                            if (num2 < num1)
+                            {
+                                num1 = num2;
+                                planet1 = planet2;
+                            }
+                        }
+                    if (planet1 == null)
                         break;
-                    planet1.ConstructionQueue.Add(new QueueItem()
+                        planet1.ConstructionQueue.Add(new QueueItem()
                     {
                         isShip = true,
                         QueueNumber = planet1.ConstructionQueue.Count,
