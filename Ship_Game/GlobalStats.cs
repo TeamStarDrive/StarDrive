2f70078482a8577e9f7a89a26dcd8cbfedb3d820
crunchygremlin <crunchygremlin@gmail>
2015-10-03 16:15:16 -0700

    1003
    --HG--
    branch : develop

--- Ship_Game/GlobalStats.cs
+++ Ship_Game/GlobalStats.cs
@@ -10,7 +10,7 @@ namespace Ship_Game
 	public sealed class GlobalStats
 	{
         public static string branch = "RC";
-        public static string Version = "1002";
+        public static string Version = "1003";
         public static int ComparisonCounter;
 
 		public static int Comparisons;
