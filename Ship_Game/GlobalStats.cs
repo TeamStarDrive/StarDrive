154d186bfddb43ac197ea5ca3212feefdc33f4c2
crunchygremlin <crunchygremlin@gmail>
2015-10-12 04:56:02 -0700

    Partial fix for racial tech blacking colonization for all races. Its fixed it just will still show in the UI when excluded but the AI should not be able to use it.
    --HG--
    branch : develop

--- Ship_Game/GlobalStats.cs
+++ Ship_Game/GlobalStats.cs
@@ -10,7 +10,7 @@ namespace Ship_Game
 	public sealed class GlobalStats
 	{
         public static string branch = "RC";
-        public static string Version = "1012";
+        public static string Version = "1012A";
         public static int ComparisonCounter;
 
 		public static int Comparisons;
