7e823ef9c7a6f5a2359f1156f3f557022e09553b
crunchygremlin <crunchygremlin@gmail>
2016-02-26 06:00:28 -0800

    updated AI governors to use new freight system. fixed some bugs in AI governors. Updated AI colony governor type assignments. Ai should be able to generate more research and have planets better supplied. It will also choose research governors more.
    --HG--
    branch : develop

--- Ship_Game/Goal.cs
+++ Ship_Game/Goal.cs
@@ -159,26 +159,47 @@ namespace Ship_Game
                             list.Add(planet2);
                     }
                     int num1 = 9999999;
+                    int x = 0;
                     foreach (Planet planet2 in list.OrderBy(planet =>
                         {
                             float weight = 0;
-                            if(planet.colonyType == Planet.ColonyType.Industrial)
-                            {
-                                weight += 4;
+                            switch (planet.colonyType)
+                            {                                
+                                case Planet.ColonyType.Core:
+                                    weight += 4;
+                                    break;
+                                case Planet.ColonyType.Colony:
+                                    break;
+                                case Planet.ColonyType.Industrial:
+                                    weight += 2;
+                                    break;
+                                case Planet.ColonyType.Research:
+                                    weight -= 6;
+                                    break;
+                                case Planet.ColonyType.Agricultural:
+                                    weight -= 6;
+                                    break;
+                                case Planet.ColonyType.Military:
+                                    weight += 2;
+                                    break;
+                                case Planet.ColonyType.TradeHub:
+                                    weight += 2;
+                                    break;
+                                default:
+                                    break;
                             }
-                            if (planet.colonyType == Planet.ColonyType.Core || planet.colonyType == Planet.ColonyType.Military || planet.colonyType == Planet.ColonyType.TradeHub)
-                            {
-                                weight += 2;
-                            }
+                            //weight -= planet.ConstructionQueue.Count;
+                 
                             weight += planet.developmentLevel;
                             weight += planet.MineralRichness;
 
                             return weight;
                         }))
                     {
-                        if (planet2.HasShipyard)
+                        if (planet2.HasShipyard && x < this.empire.GetPlanets().Count *.2)
                         {
                             int num2 = 0;
+                            x++;
                             foreach (QueueItem queueItem in (List<QueueItem>)planet2.ConstructionQueue)
                                 num2 += (int)((queueItem.Cost - queueItem.productionTowards) / planet2.GetMaxProductionPotential());
                             if (planet2.ConstructionQueue.Count == 0)
@@ -244,14 +265,51 @@ namespace Ship_Game
                         this.beingBuilt = ResourceManager.ShipsDict[this.ToBuildUID];
                     Planet planet1 = (Planet)null;
                     List<Planet> list = new List<Planet>();
-                    foreach (Planet planet2 in this.empire.GetPlanets())
+                    foreach (Planet planet2 in this.empire.GetPlanets().OrderBy(planet =>
+                    {
+                        float weight = 0;
+                        switch (planet.colonyType)
+                        {
+                            case Planet.ColonyType.Core:
+                                weight += 4;
+                                break;
+                            case Planet.ColonyType.Colony:
+                                break;
+                            case Planet.ColonyType.Industrial:
+                                weight += 2;
+                                break;
+                            case Planet.ColonyType.Research:
+                                weight -= 6;
+                                break;
+                            case Planet.ColonyType.Agricultural:
+                                weight -= 6;
+                                break;
+                            case Planet.ColonyType.Military:
+                                weight += 2;
+                                break;
+                            case Planet.ColonyType.TradeHub:
+                                weight += 2;
+                                break;
+                            default:
+                                break;
+                        }
+                        //weight -= planet.ConstructionQueue.Count;
+
+                        weight += planet.developmentLevel;
+                        weight += planet.MineralRichness;
+
+                        return weight;
+                    }))
                     {
                         if (planet2.HasShipyard  && planet2.colonyType != Planet.ColonyType.Research)
                             list.Add(planet2);
                     }
                     int num1 = 9999999;
+                    int x = 0;
                     foreach (Planet planet2 in list)
                     {
+                        if (x > this.empire.GetPlanets().Count * .2f)
+                            break;
                         int num2 = 0;
                         foreach (QueueItem queueItem in (List<QueueItem>)planet2.ConstructionQueue)
                             num2 += (int)((queueItem.Cost - queueItem.productionTowards) / planet2.GetMaxProductionPotential());//planet2.NetProductionPerTurn);
@@ -263,7 +321,7 @@ namespace Ship_Game
                             planet1 = planet2;
                         }
                     }
-                    if (planet1 == null)
+                    if (planet1 == null )
                         break;
                     this.PlanetBuildingAt = planet1;
                     planet1.ConstructionQueue.Add(new QueueItem()
