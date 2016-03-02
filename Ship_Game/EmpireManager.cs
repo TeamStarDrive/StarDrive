6a1da8cde9ff8004163a7f1a74156976bb2c683e
crunchygremlin <crunchygremlin@gmail>
2016-03-02 01:10:51 -0800

    Change diplomacy intelligence to be based on relationship and relationship of your relationships. trade will give a lot of basic information. alliance will give more detailed information. moles will give the same information as an alliance.
    --HG--
    branch : develop

--- Ship_Game/EmpireManager.cs
+++ Ship_Game/EmpireManager.cs
@@ -35,5 +35,39 @@ namespace Ship_Game
             }
             return (Empire)null;
         }
+        public static List<Empire> GetAllies(Empire e)
+        {
+            Ship_Game.Gameplay.Relationship rel;
+            List<Empire> allies = new List<Empire>();
+            foreach (Empire empire in EmpireManager.EmpireList)
+            {
+
+                if (empire.isPlayer || e.isFaction || e.MinorRace )
+                    continue;
+                e.GetRelations().TryGetValue(empire, out rel);
+                if (rel == null || !rel.Known || !rel.Treaty_Alliance)
+                    continue;
+
+                allies.Add(empire);
+            }
+            return allies;
+        }
+        public static List<Empire> GetTradePartners(Empire e)
+        {
+            Ship_Game.Gameplay.Relationship rel;
+            List<Empire> allies = new List<Empire>();
+            foreach (Empire empire in EmpireManager.EmpireList)
+            {
+
+                if (empire.isPlayer || e.isFaction || e.MinorRace)
+                    continue;
+                e.GetRelations().TryGetValue(empire, out rel);
+                if (rel == null || !rel.Known || !rel.Treaty_Trade)
+                    continue;
+
+                allies.Add(empire);
+            }
+            return allies;
+        }
 	}
 }
