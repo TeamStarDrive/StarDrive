a303df085e673d2fd119de6ddef1b5cbce0e03da
crunchygremlin <crunchygremlin@gmail>
2016-03-02 01:21:06 -0800

    Fix number of opponents not working. Thanks to Qwerty Drive for this one.
    --HG--
    branch : develop

--- Ship_Game/CreatingNewGameScreen.cs
+++ Ship_Game/CreatingNewGameScreen.cs
@@ -269,6 +269,59 @@ namespace Ship_Game
                             removalCollection.Add(empireData);                        
                     }
                     int num = removalCollection.Count - this.numOpponents;
+                    int shipsPurged = 0;
+                    float SpaceSaved = GC.GetTotalMemory(true);
+                    for (int opponents = 0; opponents < num; ++opponents)
+                    {
+                        int index2 = (int)RandomMath.RandomBetween(0.0f, (float)(removalCollection.Count + 1));
+                        if (index2 > removalCollection.Count - 1)
+                            index2 = removalCollection.Count - 1;
+
+                        if (false)
+                        {
+                            List<string> shipkill = new List<string>();
+
+                            foreach (KeyValuePair<string, Ship> ship in ResourceManager.ShipsDict)
+                            {
+                                if (ship.Value.shipData.ShipStyle == removalCollection[index2].Traits.ShipType)
+                                {
+                                    bool killSwitch = true;
+                                    foreach (Empire ebuild in EmpireManager.EmpireList)
+                                    {
+                                        if (ebuild.ShipsWeCanBuild.Contains(ship.Key))
+                                            killSwitch = false;
+                                        break;
+                                    }
+
+
+                                    if (killSwitch)
+                                        foreach (Ship mship in this.data.MasterShipList)
+                                        {
+                                            if (ship.Key == mship.Name)
+                                            {
+                                                killSwitch = false;
+                                                break;
+                                            }
+                                        }
+                                    if (killSwitch)
+                                    {
+                                        shipsPurged++;
+
+                                        // System.Diagnostics.Debug.WriteLine("Removed "+ship.Value.shipData.Role.ToString()+" : " + ship.Key + " from: " + ship.Value.shipData.ShipStyle);
+                                        shipkill.Add(ship.Key);
+                                    }
+                                }
+                            }
+                            foreach (string shiptoclear in shipkill)
+                            {
+                                ResourceManager.ShipsDict.Remove(shiptoclear);
+                            } 
+                        }
+                        removalCollection.RemoveAt(index2);
+                    }
+
+                    System.Diagnostics.Debug.WriteLine("Ships Purged: " + shipsPurged.ToString());
+                    System.Diagnostics.Debug.WriteLine("Memory purged: " + (SpaceSaved - GC.GetTotalMemory(true)).ToString());
                                            
                     foreach (EmpireData data in (List<EmpireData>)removalCollection)
                     {
@@ -320,56 +373,8 @@ namespace Ship_Game
                                 empire.AddRelationships(e, new Relationship(e.data.Traits.Name));
                         }
                     }
-                    ResourceManager.MarkShipDesignsUnlockable();
-                    int shipsPurged = 0;
-                    float SpaceSaved = GC.GetTotalMemory(true);
-                    for (int opponents = 0; opponents < num; ++opponents)
-                    {
-                        int index2 = (int)RandomMath.RandomBetween(0.0f, (float)(removalCollection.Count + 1));
-                        if (index2 > removalCollection.Count - 1)
-                            index2 = removalCollection.Count - 1;
-                        if (false)
-                        {
-                            List<string> shipkill = new List<string>();
-
-                            foreach (KeyValuePair<string, Ship> ship in ResourceManager.ShipsDict)
-                            {
-                                if (ship.Value.shipData.ShipStyle == removalCollection[index2].Traits.ShipType)
-                                {
-                                    bool killSwitch = true;
-                                    foreach (Empire ebuild in EmpireManager.EmpireList)
-                                    {
-                                        if (ebuild.ShipsWeCanBuild.Contains(ship.Key))
-                                            killSwitch = false;
-                                        break;
-                                    }
-
-
-                                    if (killSwitch)
-                                        foreach (Ship mship in this.data.MasterShipList)
-                                        {
-                                            if (ship.Key == mship.Name)
-                                            {
-                                                killSwitch = false;
-                                                break;
-                                            }
-                                        }
-                                    if (killSwitch)
-                                    {
-                                        shipsPurged++;
-
-                                        // System.Diagnostics.Debug.WriteLine("Removed "+ship.Value.shipData.Role.ToString()+" : " + ship.Key + " from: " + ship.Value.shipData.ShipStyle);
-                                        shipkill.Add(ship.Key);
-                                    }
-                                }
-                            }
-                            foreach (string shiptoclear in shipkill)
-                            {
-                                ResourceManager.ShipsDict.Remove(shiptoclear);
-                            }
-                            removalCollection.RemoveAt(index2);
-                        }
-                    }
+                    ResourceManager.MarkShipDesignsUnlockable();                    
+                    
 
                     System.Diagnostics.Debug.WriteLine("Ships Purged: " + shipsPurged.ToString());
                     System.Diagnostics.Debug.WriteLine("Memory purged: " + (SpaceSaved - GC.GetTotalMemory(true)).ToString());
