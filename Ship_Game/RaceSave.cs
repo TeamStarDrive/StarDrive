550fcd91998a231647825d5cadbb4f7d5f34cc1e
EVWeb <evweb@galacticsw.com>
2016-03-12 19:56:41 -0800

    Reworked Save and Load Screens for Future Work Converted Save Game, Save Race, Load Game, and Load Race to all use the same superclass Generic Load Save Screen to prevent so much duplicate code. Added RaceSave class to store extra data over EmpireData for saving custom races.  ( currently not used yet )
    --HG--
    branch : EVWeb

---
+++ Ship_Game/RaceSave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game
+{
+    public sealed class RaceSave
+    {
+        public EmpireData data;
+        public string name;
+        public ModEntry mod;
+
+        public RaceSave(EmpireData data, string name)
+        {
+            this.data = data;
+            this.name = name;
+            this.mod = GlobalStats.ActiveMod;
+        }
+    }
+}
