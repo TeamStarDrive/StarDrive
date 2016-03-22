1090eb9b0ae77a014d254f49a435f777c6f329a6
EVWeb <evweb@galacticsw.com>
2016-03-21 23:52:24 -0700

    Updated Save/Load Screens Added deleting of files Made code more unified and less override methods Todo: Convert fleet saving/loading screens to new method
    --HG--
    branch : EVWeb

--- Ship_Game/LoadSetupScreen.cs
+++ Ship_Game/LoadSetupScreen.cs
@@ -15,43 +15,30 @@ namespace Ship_Game
     {
         private RaceDesignScreen screen;
 
-        private SetupSave SS;
-
         public LoadSetupScreen(RaceDesignScreen screen) : base(SLMode.Load, "", "Load Saved Setup", "")
         {
             this.screen = screen;
-        }
-
-        protected override FileHeader GetFileHeader(ScrollList.Entry e)
-        {
-            FileHeader fh = new FileHeader();
-            SetupSave data = e.item as SetupSave;
-
-            fh.FileName = data.Name;
-            fh.Info = data.Date;
-            fh.ExtraInfo = (data.ModName != "" ? String.Concat("Mod: ", data.ModName) : "Default");
-            fh.icon = ResourceManager.TextureDict["ShipIcons/Wisp"];
-
-            return fh;
+            this.Path = string.Concat(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "/StarDrive/Saved Setups/");
         }
 
         protected override void Load()
         {
-            if (this.SS != null)
+            if (this.selectedFile != null)
             {
-                GlobalStats.FTLInSystemModifier = this.SS.FTLModifier;
-                GlobalStats.EnemyFTLInSystemModifier = this.SS.EnemyFTLModifier;
-                GlobalStats.OptionIncreaseShipMaintenance = this.SS.OptionIncreaseShipMaintenance;
-                GlobalStats.MinimumWarpRange = this.SS.MinimumWarpRange;
-                GlobalStats.MemoryLimiter = this.SS.MemoryLimiter;
-                GlobalStats.TurnTimer = this.SS.TurnTimer;
-                GlobalStats.preventFederations = this.SS.preventFederations;
-                GlobalStats.GravityWellRange = this.SS.GravityWellRange;
-                GlobalStats.ExtraPlanets = this.SS.ExtraPlanets;
-                GlobalStats.StartingPlanetRichness = this.SS.StartingPlanetRichness;
-                GlobalStats.PlanetaryGravityWells = this.SS.PlanetaryGravityWells;
-                GlobalStats.WarpInSystem = this.SS.WarpInSystem;
-                this.screen.SetCustomSetup(this.SS.GameDifficulty, this.SS.StarEnum, this.SS.Galaxysize, this.SS.Pacing, this.SS.ExtraRemnant, this.SS.numOpponents, this.SS.mode);
+                SetupSave ss = this.selectedFile.Data as SetupSave;
+                GlobalStats.FTLInSystemModifier = ss.FTLModifier;
+                GlobalStats.EnemyFTLInSystemModifier = ss.EnemyFTLModifier;
+                GlobalStats.OptionIncreaseShipMaintenance = ss.OptionIncreaseShipMaintenance;
+                GlobalStats.MinimumWarpRange = ss.MinimumWarpRange;
+                GlobalStats.MemoryLimiter = ss.MemoryLimiter;
+                GlobalStats.TurnTimer = ss.TurnTimer;
+                GlobalStats.preventFederations = ss.preventFederations;
+                GlobalStats.GravityWellRange = ss.GravityWellRange;
+                GlobalStats.ExtraPlanets = ss.ExtraPlanets;
+                GlobalStats.StartingPlanetRichness = ss.StartingPlanetRichness;
+                GlobalStats.PlanetaryGravityWells = ss.PlanetaryGravityWells;
+                GlobalStats.WarpInSystem = ss.WarpInSystem;
+                this.screen.SetCustomSetup(ss.GameDifficulty, ss.StarEnum, ss.Galaxysize, ss.Pacing, ss.ExtraRemnant, ss.numOpponents, ss.mode);
             }
             else
             {
@@ -62,20 +49,17 @@ namespace Ship_Game
 
         protected override void SetSavesSL()        // Set list of files to show
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            List<SetupSave> saves = new List<SetupSave>();
-            FileInfo[] filesFromDirectory = HelperFunctions.GetFilesFromDirectory(string.Concat(path, "/StarDrive/Saved Setups/"));
+            List<FileData> saves = new List<FileData>();
+            FileInfo[] filesFromDirectory = HelperFunctions.GetFilesFromDirectory(this.Path);
             for (int i = 0; i < (int)filesFromDirectory.Length; i++)
             {
                 Stream file = filesFromDirectory[i].OpenRead();
                 try
                 {
-                    //EmpireData data = (EmpireData)ResourceManager.HeadeSSerializer.Deserialize(file);
                     XmlSerializer serializer1 = new XmlSerializer(typeof(SetupSave));
                     SetupSave data = (SetupSave)serializer1.Deserialize(file);
                     if (string.IsNullOrEmpty(data.Name) || data.Version < 308)
                     {
-                        //file.Close();
                         file.Dispose();
                         continue;
                     }
@@ -84,44 +68,34 @@ namespace Ship_Game
                     {
                         if (data.ModPath != GlobalStats.ActiveMod.ModPath)
                         {
-                            //file.Close();
                             file.Dispose();
                             continue;
                         }
                     }
                     else if (!string.IsNullOrEmpty(data.ModPath))
                     {
-                        //file.Close();
                         file.Dispose();
                         continue;
                     }
-                    saves.Add(data);
-                    //file.Close();
+
+                    string info = data.Date;
+                    string extraInfo = (data.ModName != "" ? String.Concat("Mod: ", data.ModName) : "Default");
+                    saves.Add(new FileData(filesFromDirectory[i], data, data.Name, info, extraInfo));
                     file.Dispose();
                 }
                 catch
                 {
-                    //file.Close();
                     file.Dispose();
                 }
-                //Label0:
-                //  continue;
             }
-            IOrderedEnumerable<SetupSave> sortedList =
+            IOrderedEnumerable<FileData> sortedList =
                 from data in saves
-                orderby data.Name ascending
+                orderby data.FileName ascending
                 select data;
-            foreach (SetupSave data in sortedList)
+            foreach (FileData data in sortedList)
             {
-                this.SavesSL.AddItem(data);
+                this.SavesSL.AddItem(data).AddItemWithCancel(data.FileLink);
             }
-        }
-
-        protected override void SwitchFile(ScrollList.Entry e)
-        {
-            this.SS = (e.item as SetupSave);
-            AudioManager.PlayCue("sd_ui_accept_alt3");
-            this.EnterNameArea.Text = (e.item as SetupSave).Name;
         }
     }
 }
