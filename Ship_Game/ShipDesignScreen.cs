a9858d07662b8d3e08b3666ccef44a7ad1aa9776
Gretman <arcmage5@hotmail.com>
2016-10-01 21:38:49 -0700

    Tweaking debug in the Shipyard. This will be infinitely useful to me when I do my ship overhaul.
    --HG--
    branch : Gretman

--- Ship_Game/ShipDesignScreen.cs
+++ Ship_Game/ShipDesignScreen.cs
@@ -207,9 +207,11 @@ namespace Ship_Game
 		public ShipDesignScreen(EmpireUIOverlay EmpireUI)
 		{
 			this.EmpireUI = EmpireUI;
-			base.TransitionOnTime = TimeSpan.FromSeconds(0.25);
-            
-		}
+			base.TransitionOnTime = TimeSpan.FromSeconds(2);
+#if GRETMAN
+            Debug = true;
+#endif
+        }
         private void AddToTechList(HashSet<string> techlist)
         {
             foreach (string tech in techlist)
@@ -798,17 +800,11 @@ namespace Ship_Game
 					toRemove = null;
 					foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
 					{
-						if (slotdata.Position != SlotPos)
-						{
-							continue;
-						}
+						if (slotdata.Position != SlotPos) continue;
 						toRemove = slotdata;
 						break;
 					}
-					if (toRemove == null)
-					{
-						return;
-					}
+					if (toRemove == null) return;
 					this.ActiveHull.ModuleSlotList.Remove(toRemove);
 					this.ChangeHull(this.ActiveHull);
 					return;
@@ -818,38 +814,12 @@ namespace Ship_Game
 					toRemove = null;
 					foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
 					{
-						if (slotdata.Position != SlotPos)
-						{
-							continue;
-						}
+						if (slotdata.Position != SlotPos) continue;
 						toRemove = slotdata;
 						break;
 					}
-					if (toRemove == null)
-					{
-						return;
-					}
+					if (toRemove == null) return;
 					toRemove.Restrictions = Restrictions.I;
-					this.ChangeHull(this.ActiveHull);
-					return;
-				}
-				case ShipDesignScreen.SlotModOperation.IO:
-				{
-					toRemove = null;
-					foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
-					{
-						if (slotdata.Position != SlotPos)
-						{
-							continue;
-						}
-						toRemove = slotdata;
-						break;
-					}
-					if (toRemove == null)
-					{
-						return;
-					}
-					toRemove.Restrictions = Restrictions.IO;
 					this.ChangeHull(this.ActiveHull);
 					return;
 				}
@@ -858,46 +828,90 @@ namespace Ship_Game
 					toRemove = null;
 					foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
 					{
-						if (slotdata.Position != SlotPos)
-						{
-							continue;
-						}
+						if (slotdata.Position != SlotPos) continue;
 						toRemove = slotdata;
 						break;
 					}
-					if (toRemove == null)
-					{
-						return;
-					}
+					if (toRemove == null) return;
 					toRemove.Restrictions = Restrictions.O;
 					this.ChangeHull(this.ActiveHull);
 					return;
 				}
-				case ShipDesignScreen.SlotModOperation.Add:
-				{
-					return;
-				}
-				case ShipDesignScreen.SlotModOperation.E:
-				{
-					toRemove = null;
-					foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
-					{
-						if (slotdata.Position != SlotPos)
-						{
-							continue;
-						}
-						toRemove = slotdata;
-						break;
-					}
-					if (toRemove == null)
-					{
-						return;
-					}
-					toRemove.Restrictions = Restrictions.E;
-					this.ChangeHull(this.ActiveHull);
-					return;
-				}
-				default:
+                case ShipDesignScreen.SlotModOperation.E:
+                {
+                    toRemove = null;
+                    foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
+                    {
+                        if (slotdata.Position != SlotPos) continue;
+                        toRemove = slotdata;
+                        break;
+                    }
+                    if (toRemove == null) return;
+                    toRemove.Restrictions = Restrictions.E;
+                    this.ChangeHull(this.ActiveHull);
+                    return;
+                }
+                case ShipDesignScreen.SlotModOperation.IO:
+                {
+                    toRemove = null;
+                    foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
+                    {
+                        if (slotdata.Position != SlotPos) continue;
+                        toRemove = slotdata;
+                        break;
+                    }
+                    if (toRemove == null) return;
+                    toRemove.Restrictions = Restrictions.IO;
+                    this.ChangeHull(this.ActiveHull);
+                    return;
+                }
+                case ShipDesignScreen.SlotModOperation.IE:
+                {
+                    toRemove = null;
+                    foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
+                    {
+                        if (slotdata.Position != SlotPos) continue;
+                        toRemove = slotdata;
+                        break;
+                    }
+                    if (toRemove == null) return;
+                    toRemove.Restrictions = Restrictions.IE;
+                    this.ChangeHull(this.ActiveHull);
+                    return;
+                }
+                case ShipDesignScreen.SlotModOperation.OE:
+                {
+                    toRemove = null;
+                    foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
+                    {
+                        if (slotdata.Position != SlotPos) continue;
+                        toRemove = slotdata;
+                        break;
+                    }
+                    if (toRemove == null) return;
+                    toRemove.Restrictions = Restrictions.OE;
+                    this.ChangeHull(this.ActiveHull);
+                    return;
+                }
+                case ShipDesignScreen.SlotModOperation.IOE:
+                {
+                    toRemove = null;
+                    foreach (ModuleSlotData slotdata in this.ActiveHull.ModuleSlotList)
+                    {
+                        if (slotdata.Position != SlotPos) continue;
+                        toRemove = slotdata;
+                        break;
+                    }
+                    if (toRemove == null) return;
+                    toRemove.Restrictions = Restrictions.IOE;
+                    this.ChangeHull(this.ActiveHull);
+                    return;
+                }
+                case ShipDesignScreen.SlotModOperation.Normal:
+                {
+                    return;
+                }
+                default:
 				{
 					return;
 				}
@@ -5388,7 +5402,7 @@ namespace Ship_Game
                     }
                     if (input.Right)
                         ++this.operation;
-                    if (this.operation > (ShipDesignScreen.SlotModOperation)6)
+                    if (this.operation > ShipDesignScreen.SlotModOperation.Normal)
                         this.operation = ShipDesignScreen.SlotModOperation.Delete;
                 }
                 this.HoveredModule = (ShipModule)null;
@@ -7573,10 +7587,14 @@ namespace Ship_Game
 		{
 			Delete,
 			I,
-			IO,
 			O,
-			Add,
-			E
+			E,
+			IO,
+			IE,
+            OE,
+            IOE,
+            Normal
+
 		}
 	}
 }
