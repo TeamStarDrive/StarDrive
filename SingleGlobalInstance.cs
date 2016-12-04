5f7c926a429e220a2e382cb5f07c0fbbbb506127
RedFox <jorma.rebane@gmail.com>
2016-12-04 12:29:05 +0200

    Minor refactoring of Main and SingleGlobalInstance
    --HG--
    branch : RedFox

--- SingleGlobalInstance.cs
+++ SingleGlobalInstance.cs
@@ -7,34 +7,21 @@ using System.Threading;
 
 public class SingleGlobalInstance : IDisposable
 {
-	public bool hasHandle;
+	private Mutex Mutex;
 
-	private Mutex mutex;
-
-    //adding for thread safe Dispose because class uses unmanaged resources 
-    private bool disposed;
-
-	public SingleGlobalInstance(int TimeOut)
+	public SingleGlobalInstance()
 	{
-		this.InitMutex();
 		try
 		{
-			if (TimeOut > 0)
-			{
-				this.hasHandle = this.mutex.WaitOne(TimeOut, false);
-			}
-			else
-			{
-				this.hasHandle = this.mutex.WaitOne(-1, false);
-			}
-			if (!this.hasHandle)
-			{
-				throw new Exception("An instance of StarDrive is already running");
-			}
+            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly()
+                                .GetCustomAttributes(typeof(GuidAttribute), false)[0]).Value;
+
+            Mutex = new Mutex(true, $"Global\\{{{appGuid}}}", out bool newMutexCreated);
+            if (!newMutexCreated)
+                throw new Exception("An instance of StarDrive is already running");
 		}
 		catch (AbandonedMutexException)
 		{
-			this.hasHandle = true;
 		}
 	}
 
@@ -46,32 +33,9 @@ public class SingleGlobalInstance : IDisposable
 
     ~SingleGlobalInstance() { Dispose(false); }
 
-    protected virtual void Dispose(bool disposing)
+    protected void Dispose(bool disposing)
     {
-        if (!disposed)
-        {
-            if (disposing)
-            {
-                if (this.mutex != null)
-                    this.mutex.Dispose();
-            }
-            this.disposed = true;
-            if (this.hasHandle && this.mutex != null)
-            {
-                this.mutex.ReleaseMutex();
-            }
-            this.mutex = null;
-        }
+        Mutex?.Dispose();
+        Mutex = null;
     }
-
-	private void InitMutex()
-	{
-		string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
-		string mutexId = string.Format("Global\\{{{0}}}", appGuid);
-		this.mutex = new Mutex(false, mutexId);
-		MutexAccessRule allowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
-		MutexSecurity securitySettings = new MutexSecurity();
-		securitySettings.AddAccessRule(allowEveryoneRule);
-		this.mutex.SetAccessControl(securitySettings);
-	}
 }
