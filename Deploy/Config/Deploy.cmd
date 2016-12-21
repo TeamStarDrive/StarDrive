86b5d3bce1ba573f54d0efa040e06dc9fb22d560
crunchygremlin <crunchygremlin@gmail>
2016-12-20 23:40:32 -0800

    updated deployment. release and debug configurations should work.
    --HG--
    branch : Crunchies cleanup

--- Deploy/Config/Deploy.cmd
+++ Deploy/Config/Deploy.cmd
@@ -2,17 +2,19 @@ rem %1  =$([System.DateTime]::Now.ToString(`MMdd`))
 if not exist ..\Config exit 0
 set year=%date:~-2%
 set month=%date:~4,2%
+set day=%date:~7,2%
 
 
 for /f "delims=" %%i in (..\..\.hg\branch) do (
 if not exist "..\config\config_%%i" (
-copy "..\config\config.txt" "..\config\config_%%i%2" 
-copy "..\config\include.txt" "..\config\include_%%i%2" 
+copy "..\config\config.txt" "..\config\config_%%i" 
+copy "..\config\include.txt" "..\config\include_%%i" 
 )
 echo %%i
 echo ..\7-Zip\7z A sd.7z @"..\Config\include_%%i"
-echo copy /b "..\7-Zip\7ZSD.sfx" + "..\Config\config_%%i" + sd.7z "%%i%1.exe"
+echo copy /b "..\7-Zip\7ZSD.sfx" + "..\Config\config_%%i" + sd.7z "%%i %month%%day%.exe"
+
 ..\7-Zip\7z A sd.7z @"..\Config\include_%%i"
-copy /b "..\7-Zip\7ZSD.sfx" + "..\Config\config_%%i" + sd.7z "%%i%year%%month%.exe"
+copy /b "..\7-Zip\7ZSD.sfx" + "..\Config\config_%%i" + sd.7z "%%i%month%%day%.exe"
 )
 
