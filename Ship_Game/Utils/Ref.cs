30601d5f9a46eb36ac1db95b5e65767598ebda4b
RedFox <jorma.rebane@gmail.com>
2016-12-27 02:05:23 +0200

    Really fixed AutomationWindow crash now.
    --HG--
    branch : RedFox

--- Ship_Game/Utils/Ref.cs
+++ Ship_Game/Utils/Ref.cs
@@ -21,6 +21,7 @@ namespace Ship_Game
 			Setter = setter;
 		}
 
+
         // Create a property reference to a FIELD or PROPERTY
         // Usage:
         //   new Ref<bool>( () => this.BoolValueToBind );
@@ -30,24 +31,47 @@ namespace Ship_Game
 
             // try to get the instance of the expression,
             // for instance fields/properties this should never be null
-            object obj = (body.Expression as ConstantExpression)?.Value;
+            object obj = body.Expression?.GetTargetInstance();
 
-            if (body.Member is PropertyInfo) // expression is a property
+            var member = body.Member;
+            if (member is PropertyInfo propInfo) // expression is a property
             {
-                var prop = (PropertyInfo)body.Member;
+                var prop = propInfo; // VS2017 RC requires a copy for lambda capture
                 Getter = () => (T)prop.GetValue(obj, BindingFlags.Default, null, null, null);
                 Setter =  x => prop.SetValue(obj, x, BindingFlags.Default, null, null, null);
             }
-            else if (body.Member is FieldInfo) // expression is a regular variable
+            else if (member is FieldInfo fieldInfo) // expression is a regular variable
             {
-                var field = (FieldInfo)body.Member;
+                var field = fieldInfo;
                 Getter = () => (T)field.GetValue(obj);
-                Setter =  x => field.SetValue(obj, x);
+                Setter = x => field.SetValue(obj, x);
             }
             else
             {
                 throw new MemberAccessException("Unbindable reference. Perhaps the bind expression is too complex?");
             }
+        }
+    }
+
+    internal static class RefHelper
+    {
+        internal static object GetTargetInstance(this Expression expr)
+        {
+            if (expr is ConstantExpression constexpr)
+                return constexpr.Value;
+
+            if (expr is MemberExpression membexpr)
+            {
+                object obj = membexpr.Expression?.GetTargetInstance();
+                var member = membexpr.Member;
+
+                if (member is PropertyInfo propInfo)
+                    return propInfo.GetValue(obj, BindingFlags.Default, null, null, null);
+
+                if (member is FieldInfo fieldInfo)
+                    return fieldInfo.GetValue(obj);
+            }
+            return null;
         }
     }
 }
