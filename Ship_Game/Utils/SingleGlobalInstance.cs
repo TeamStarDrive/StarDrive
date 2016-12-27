673d99fc427deaed25c8c14b4feaec28c0621b29
RedFox <jorma.rebane@gmail.com>
2016-12-27 04:15:58 +0200

    Some work on simplifying Dispose logic.
    --HG--
    branch : RedFox

--- Ship_Game/Utils/SingleGlobalInstance.cs
+++ Ship_Game/Utils/SingleGlobalInstance.cs
@@ -1,39 +1,38 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security.AccessControl;
-using System.Security.Principal;
 using System.Threading;
 
-public class SingleGlobalInstance : IDisposable
+namespace Ship_Game
 {
-	private Mutex Mutex;
-    public readonly bool UniqueInstance; // true if this is an unique game instance
-
-	public SingleGlobalInstance()
-	{
-		try
-		{
-            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly()
-                                .GetCustomAttributes(typeof(GuidAttribute), false)[0]).Value;
-            Mutex = new Mutex(true, $"Global\\{{{appGuid}}}", out UniqueInstance);
-		}
-		catch (AbandonedMutexException)
-		{
-		}
-	}
-
-    public void Dispose()
+    public sealed class SingleGlobalInstance : IDisposable
     {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
+        private Mutex Mutex;
+        public readonly bool UniqueInstance; // true if this is an unique game instance
 
-    ~SingleGlobalInstance() { Dispose(false); }
+        public SingleGlobalInstance()
+        {
+            try
+            {
+                string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly()
+                                    .GetCustomAttributes(typeof(GuidAttribute), false)[0]).Value;
+                Mutex = new Mutex(true, $"Global\\{{{appGuid}}}", out UniqueInstance);
+            }
+            catch (AbandonedMutexException)
+            {
+            }
+        }
 
-    protected void Dispose(bool disposing)
-    {
-        Mutex?.Dispose();
-        Mutex = null;
+        public void Dispose()
+        {
+            Mutex?.Dispose(ref Mutex);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SingleGlobalInstance()
+        {
+            Mutex?.Dispose(ref Mutex);
+        }
     }
 }
+
