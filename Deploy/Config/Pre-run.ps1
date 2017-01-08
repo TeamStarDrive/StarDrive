4a6073dbf7509e866a23f81e372f30ded4fdf89c
Gretman <arcmage5@hotmail.com>
2017-01-07 22:48:27 -0800

    Add new Pre-Script to Project
    --HG--
    branch : Gretman

---
+++ Deploy/Config/Pre-run.ps1
@@ -0,0 +1,19 @@
+﻿$Newfile = @()
+$updatefile = $true
+$ver = .\Deploy\TortoiseHg\hg.exe id -n -r .
+$branch = .\Deploy\TortoiseHg\hg.exe id -b
+$newline = "`[assembly`: AssemblyInformationalVersion(`"" + $branch + "_" + $ver + "`")`]"
+Get-Content "D:\codemod\Properties\AssemblyInfo.cs" | ForEach-Object {
+
+    $currentline = $_
+
+    if ($currentline -like "*AssemblyInformationalVersion*") 
+    {
+        if ($currentline.Split('"')[1] -eq $newline.Split('"')[1]) {$updatefile = $false}
+        $currentline = $newline
+    }
+    $newfile += $currentline
+}
+
+if ($updatefile) {Set-Content -Path ".\Properties\AssemblyInfo.cs" -Value $Newfile -Force}
+else {Write-Host -ForegroundColor Yellow "Not updating file, because version has not changed"}
