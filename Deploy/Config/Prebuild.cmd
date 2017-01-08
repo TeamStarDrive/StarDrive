f557550a64338e0879122038e1a85ccb9549c2eb
crunchygremlin <crunchygremlin@gmail>
2017-01-08 15:26:31 -0800

    include new ps1 script in deployment.
    --HG--
    branch : Crunchies cleanup

--- Deploy/Config/Prebuild.cmd
+++
@@ -1,8 +0,0 @@
-echo %1deploy\TortoiseHg\hg.exe
-(%1deploy\TortoiseHg\hg.exe id -n|| echo +) | find "+" && exit 0
-
-for /f %%r in ('%1deploy\TortoiseHg\hg.exe id -n -r .') do set hgrev=%%r
-for /f "delims=" %%b in ('%1/deploy\TortoiseHg\hg id -b') do set hgbranch=%%b
-
-%1/deploy/config/xml ed -L --update //add[@key='Version']/@value  -v "%hgrev%" %1\app.config
-%1/deploy/config/xml ed -L --update //add[@key='Branch']/@value  -v "%hgbranch%" %1\app.config
