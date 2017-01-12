9bf3dee7c1c6bf418c5e8a738cbf556d4d9b48cc
RedFox <jorma.rebane@gmail.com>
2017-01-12 21:18:09 +0200

    Improved Parallel.For and enumeration.
    --HG--
    branch : RedFox

--- SDUnitTests/TestCollectionExt.cs
+++ SDUnitTests/TestCollectionExt.cs
@@ -58,39 +58,60 @@ namespace SDUnitTests
         }
 
         [Test]
-        public void TestParallelForEach()
+        public void TestParallelRange()
         {
             var numbers = new int[133333337];
-            for (int i = 0; i < numbers.Length; ++i)
-                numbers[i] = i;
-
-            Parallel.For(0, 1000, (start, end) => { });
+            Parallel.For(0, numbers.Length, (start, end) =>
+            {
+                for (int i = start; i < end; ++i)
+                    numbers[i] = i;
+            });
 
             PerfTimer timer = PerfTimer.StartNew();
             long sum = 0;
-            numbers.ParallelForEach(value =>
+            numbers.ParallelRange(range =>
             {
-                Interlocked.Add(ref sum, value);
+                long isum = 0;
+                foreach (int value in range)
+                    isum += value;
+                Interlocked.Add(ref sum, isum);
             });
-            Console.WriteLine("ParallelFor  elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum);
+            Console.WriteLine("ParallelRange elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum);
 
             timer.Start();
             long sum2 = 0;
             foreach (int value in numbers)
                 sum2 += value;
-            Console.WriteLine("Singlethread elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum2);
+            Console.WriteLine("SingleThread  elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum2);
 
-            Assert.AreEqual(sum2, sum, "ParallelFor<T> result incorrect. Incorrect loop logic?");
+            Assert.AreEqual(sum2, sum, "ParallelRange result incorrect. Incorrect loop logic?");
+
+            // Test the parallel loop a second time to ensure it doesn't deadlock etc
+            int poolSize = Parallel.PoolSize;
+            timer.Start();
+            long sum3 = 0;
+            numbers.ParallelRange(range =>
+            {
+                long isum = 0;
+                foreach (int value in range)
+                    isum += value;
+                Interlocked.Add(ref sum3, isum);
+            });
+            Console.WriteLine("ParallelRange elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum3);
+
+            Assert.AreEqual(sum2, sum3, "ParallelRange result incorrect. Incorrect loop logic?");
+            Assert.AreEqual(poolSize, Parallel.PoolSize, "Parallel.For pool is growing, but it shouldn't. Incorrect ParallelTask states?");
         }
 
         [Test]
         public void TestParallelFor()
         {
             var numbers = new int[133333337];
-            for (int i = 0; i < numbers.Length; ++i)
-                numbers[i] = i;
-
-            Parallel.For(0, 1000, (start, end) => { });
+            Parallel.For(0, numbers.Length, (start, end) =>
+            {
+                for (int i = start; i < end; ++i)
+                    numbers[i] = i;
+            });
 
             PerfTimer timer = PerfTimer.StartNew();
             long sum = 0;
@@ -107,9 +128,25 @@ namespace SDUnitTests
             long sum2 = 0;
             foreach (int value in numbers)
                 sum2 += value;
-            Console.WriteLine("Singlethread elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum2);
+            Console.WriteLine("SingleThread elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum2);
 
-            Assert.AreEqual(sum2, sum, "ParallelFor<T> result incorrect. Incorrect loop logic?");
+            Assert.AreEqual(sum2, sum, "Parallel.For result incorrect. Incorrect loop logic?");
+
+            // Test the parallel loop a second time to ensure it doesn't deadlock etc
+            int poolSize = Parallel.PoolSize;
+            timer.Start();
+            long sum3 = 0;
+            Parallel.For(0, numbers.Length, (start, end) =>
+            {
+                long isum = 0;
+                for (int i = start; i < end; ++i)
+                    isum += numbers[i];
+                Interlocked.Add(ref sum3, isum);
+            });
+            Console.WriteLine("ParallelFor  elapsed: {0:0.0,4}s  result: {1}", timer.Elapsed, sum3);
+
+            Assert.AreEqual(sum2, sum3, "Parallel.For result incorrect. Incorrect loop logic?");
+            Assert.AreEqual(poolSize, Parallel.PoolSize, "Parallel.For pool is growing, but it shouldn't. Incorrect ParallelTask states?");
         }
     }
 }
