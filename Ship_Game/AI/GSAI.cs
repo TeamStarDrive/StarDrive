222123c6b7750d1a192b5fa68a0a20cbfb05c6fa
RedFox <jorma.rebane@gmail.com>
2017-01-28 02:28:36 +0200

    Removed some dead code in SunBurn
    --HG--
    branch : DevelopTexas

--- Ship_Game/AI/GSAI.cs
+++ Ship_Game/AI/GSAI.cs
@@ -2518,7 +2518,7 @@ namespace Ship_Game.AI
 			{
                 baseDefensePct = 0.35f;
 			}
-			float EntireStrength = 0f;
+			//float EntireStrength = 0f;
 			//foreach (Ship ship in this.empire.GetShips())
 			//{
 			//	EntireStrength = EntireStrength + ship.GetStrength();
