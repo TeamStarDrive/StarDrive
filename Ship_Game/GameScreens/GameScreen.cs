9575b4ecabd5cff3055f711fc158c2e58929cdd2
RedFox <jorma.rebane@gmail.com>
2017-03-27 04:54:00 +0300

    New shipmodule drawing. Still WIP, but surprisingly stable. Will need a lot of cleanup.
    --HG--
    branch : DevelopTexas

--- Ship_Game/GameScreens/GameScreen.cs
+++ Ship_Game/GameScreens/GameScreen.cs
@@ -1,14 +1,15 @@
 using Microsoft.Xna.Framework;
 using System;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Ship_Game
 {
-	public abstract class GameScreen : IDisposable
-	{
-		public bool IsLoaded;
-	    public bool AlwaysUpdate;
-	    private bool OtherScreenHasFocus;
+    public abstract class GameScreen : IDisposable
+    {
+        public bool IsLoaded;
+        public bool AlwaysUpdate;
+        private bool OtherScreenHasFocus;
         protected readonly Array<UIButton> Buttons = new Array<UIButton>();
         protected Texture2D BtnDefault;
         protected Texture2D BtnHovered;
@@ -18,14 +19,14 @@ namespace Ship_Game
                                 && ScreenState == ScreenState.TransitionOn 
                                 || ScreenState == ScreenState.Active;
 
-	    public bool IsExiting { get; protected set; }
-	    public bool IsPopup   { get; protected set; }
+        public bool IsExiting { get; protected set; }
+        public bool IsPopup   { get; protected set; }
 
-	    public ScreenManager ScreenManager { get; internal set; }
-	    public ScreenState   ScreenState   { get; protected set; }
-	    public TimeSpan TransitionOffTime { get; protected set; } = TimeSpan.Zero;
-	    public TimeSpan TransitionOnTime  { get; protected set; } = TimeSpan.Zero;
-	    public float TransitionPosition   { get; protected set; } = 1f;
+        public ScreenManager ScreenManager { get; internal set; }
+        public ScreenState   ScreenState   { get; protected set; }
+        public TimeSpan TransitionOffTime { get; protected set; } = TimeSpan.Zero;
+        public TimeSpan TransitionOnTime  { get; protected set; } = TimeSpan.Zero;
+        public float TransitionPosition   { get; protected set; } = 1f;
 
         public byte TransitionAlpha => (byte)(255f - TransitionPosition * 255f);
 
@@ -34,75 +35,75 @@ namespace Ship_Game
         public GameContentManager TransientContent;
 
         protected GameScreen(GameScreen parent)
-		{
+        {
             // hook the content chain to parent screen if possible
             TransientContent = new GameContentManager(parent?.TransientContent ?? Game1.Instance.Content, GetType().Name);
             ScreenManager    = parent?.ScreenManager ?? Game1.Instance.ScreenManager;
         }
 
-		public abstract void Draw(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
 
-		public virtual void ExitScreen()
-		{
-			ScreenManager.exitScreenTimer =.024f;
+        public virtual void ExitScreen()
+        {
+            ScreenManager.exitScreenTimer =.024f;
             if (TransitionOffTime != TimeSpan.Zero)
-			{
-				IsExiting = true;
-				return;
-			}
-			ScreenManager.RemoveScreen(this);
-		}
+            {
+                IsExiting = true;
+                return;
+            }
+            ScreenManager.RemoveScreen(this);
+        }
 
-		public virtual void HandleInput(InputState input)
-		{
-		}
+        public virtual void HandleInput(InputState input)
+        {
+        }
 
-		public virtual void LoadContent()
-		{
+        public virtual void LoadContent()
+        {
             BtnDefault = ResourceManager.TextureDict["EmpireTopBar/empiretopbar_btn_168px"];
             BtnHovered = ResourceManager.TextureDict["EmpireTopBar/empiretopbar_btn_168px_hover"];
             BtnPressed = ResourceManager.TextureDict["EmpireTopBar/empiretopbar_btn_168px_pressed"];
         }
 
-		public virtual void UnloadContent()
-		{
+        public virtual void UnloadContent()
+        {
             TransientContent?.Unload();
         }
 
-		public virtual void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
-		{
-			OtherScreenHasFocus = otherScreenHasFocus;
-			if (!IsExiting)
-			{
-				if (coveredByOtherScreen)
-				{
-					ScreenState = UpdateTransition(gameTime, TransitionOffTime, 1)
+        public virtual void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            OtherScreenHasFocus = otherScreenHasFocus;
+            if (!IsExiting)
+            {
+                if (coveredByOtherScreen)
+                {
+                    ScreenState = UpdateTransition(gameTime, TransitionOffTime, 1)
                                 ? ScreenState.TransitionOff : ScreenState.Hidden;
-					return;
-				}
-			    ScreenState = UpdateTransition(gameTime, TransitionOnTime, -1)
-			                ? ScreenState.TransitionOn : ScreenState.Active;
-			}
-			else
-			{
-				ScreenState = ScreenState.TransitionOff;
-			    if (UpdateTransition(gameTime, TransitionOffTime, 1))
                     return;
-			    ScreenManager.RemoveScreen(this);
-			    IsExiting = false;
-			}
-		}
+                }
+                ScreenState = UpdateTransition(gameTime, TransitionOnTime, -1)
+                            ? ScreenState.TransitionOn : ScreenState.Active;
+            }
+            else
+            {
+                ScreenState = ScreenState.TransitionOff;
+                if (UpdateTransition(gameTime, TransitionOffTime, 1))
+                    return;
+                ScreenManager.RemoveScreen(this);
+                IsExiting = false;
+            }
+        }
 
-		private bool UpdateTransition(GameTime gameTime, TimeSpan time, int direction)
-		{
-		    float transitionDelta = (time != TimeSpan.Zero ? (float)(gameTime.ElapsedGameTime.TotalMilliseconds / time.TotalMilliseconds) : 1f);
-			TransitionPosition += transitionDelta * direction;
-			if (TransitionPosition > 0f && TransitionPosition < 1f)
-				return true;
+        private bool UpdateTransition(GameTime gameTime, TimeSpan time, int direction)
+        {
+            float transitionDelta = (time != TimeSpan.Zero ? (float)(gameTime.ElapsedGameTime.TotalMilliseconds / time.TotalMilliseconds) : 1f);
+            TransitionPosition += transitionDelta * direction;
+            if (TransitionPosition > 0f && TransitionPosition < 1f)
+                return true;
 
-			TransitionPosition = MathHelper.Clamp(TransitionPosition, 0f, 1f);
-			return false;
-		}
+            TransitionPosition = MathHelper.Clamp(TransitionPosition, 0f, 1f);
+            return false;
+        }
 
         // Shared utility functions:
         protected UIButton Button(ref Vector2 pos, string launches, int localization)
@@ -131,6 +132,17 @@ namespace Ship_Game
             pos.Y += BtnDefault.Height + 15;
         }
 
+
+        // just draws a line, no fancy reprojections
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void DrawLine(Vector2 point1, Vector2 point2, Color color, float thickness = 1f)
+            => Primitives2D.DrawLine(ScreenManager.SpriteBatch, point1, point2, color, thickness);
+
+        // just draws a circle, no fancy reprojections
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void DrawCircle(Vector2 center, float radius, int sides, Color color, float thickness = 1f)
+            => Primitives2D.DrawCircle(ScreenManager.SpriteBatch, center, radius, sides, color, thickness);
+
         ~GameScreen() { Dispose(false); }
 
         public void Dispose()
@@ -139,9 +151,9 @@ namespace Ship_Game
             GC.SuppressFinalize(this);
         }
 
-	    protected virtual void Dispose(bool disposing)
-	    {
-	        TransientContent?.Dispose(ref TransientContent);
-	    }
+        protected virtual void Dispose(bool disposing)
+        {
+            TransientContent?.Dispose(ref TransientContent);
+        }
     }
 }
