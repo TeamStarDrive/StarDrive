06fa46c9f1c77a09b30539f38d8d18e3918b508e
crunchygremlin <crunchygremlin@gmail>
2017-04-18 03:51:29 -0700

    Change music state "NotPlaying" to "NotLoaded" change condition to true if state is null. Change shields back to batchremoval and use readwrtierlock hoping this is faster than globallock.
    --HG--
    branch : develop

--- Ship_Game/ShieldManager.cs
+++ Ship_Game/ShieldManager.cs
@@ -5,8 +5,8 @@ namespace Ship_Game
 {
 	public sealed class ShieldManager
 	{
-		private static readonly Array<Shield> ShieldList = new Array<Shield>();
-		private static readonly Array<Shield> PlanetaryShieldList = new Array<Shield>();
+		private static readonly BatchRemovalCollection<Shield> ShieldList = new BatchRemovalCollection<Shield>();
+		private static readonly BatchRemovalCollection<Shield> PlanetaryShieldList = new BatchRemovalCollection<Shield>();
 
 		private static Model     ShieldModel;
 		private static Texture2D ShieldTexture;
@@ -26,12 +26,13 @@ namespace Ship_Game
 
 		public static void Draw(Matrix view, Matrix projection)
 		{
-			lock (GlobalStats.ShieldLocker)
+			
 			{
-			    for (int index = 0; index < ShieldList.Count; index++)
+			    using (ShieldList.AcquireReadLock())
+                    for (int index = 0; index < ShieldList.Count; index++)
 			    {
 			        Shield shield = ShieldList[index];
-			        Vector3 shieldcenter = new Vector3(shield.Owner.Center, 0f);
+			        Vector3 shieldcenter = shield.Owner.Center.ToVec3();
 			        if (Empire.Universe.Frustum.Contains(shieldcenter) == ContainmentType.Disjoint)
 			            continue;
 			        if (shield.pointLight.Intensity <= 0f)
@@ -45,6 +46,7 @@ namespace Ship_Game
 			        shield.World = w;
 			        DrawShield(shield, view, projection);
 			    }
+                using(PlanetaryShieldList.AcquireReadLock())
 			    for (int index = 0; index < PlanetaryShieldList.Count; index++)
 			    {
 			        Shield shield = PlanetaryShieldList[index];
@@ -82,11 +84,9 @@ namespace Ship_Game
 
         public static void Clear()
         {
-            lock (GlobalStats.ShieldLocker)
-            {
-                ShieldList.Clear();
-                PlanetaryShieldList.Clear();
-            }
+            ShieldList.Clear();
+            PlanetaryShieldList.Clear();
+
         }
 
         public static Shield AddPlanetaryShield(Vector2 position)
@@ -102,8 +102,8 @@ namespace Ship_Game
                         * Matrix.CreateRotationZ(0.0f)
                         * Matrix.CreateTranslation(position.X, position.Y, 2500f)
             };
-            lock (GlobalStats.ShieldLocker)
-                PlanetaryShieldList.Add(shield);
+            
+            PlanetaryShieldList.Add(shield);
             return shield;
         }
 
@@ -119,8 +119,8 @@ namespace Ship_Game
                         * Matrix.CreateRotationZ(rotation)
                         * Matrix.CreateTranslation(owner.Center.X, owner.Center.Y, 0f)
 		    };
-		    lock (GlobalStats.ShieldLocker)
-			    ShieldList.Add(shield);
+		    
+			ShieldList.Add(shield);
 		}
 
         public static Shield AddShield(GameplayObject owner, float rotation, Vector2 center)
@@ -135,15 +135,15 @@ namespace Ship_Game
                         * Matrix.CreateRotationZ(rotation)
                         * Matrix.CreateTranslation(center.X, center.Y, 0.0f)
             };
-            lock (GlobalStats.ShieldLocker)
-                ShieldList.Add(shield);
+            
+            ShieldList.Add(shield);
             return shield;
         }
 
 		public static void Update()
-		{
-            if (Empire.Universe.viewState < UniverseScreen.UnivScreenState.SectorView)
+		{            
             {
+                using(PlanetaryShieldList.AcquireReadLock())
                 for (int index = 0; index < PlanetaryShieldList.Count; index++)
                 {
                     Shield shield = PlanetaryShieldList[index];
@@ -152,8 +152,8 @@ namespace Ship_Game
                         shield.pointLight.Enabled = false;
                     if (shield.texscale <= 0f)
                         continue;
-
-                    shield.World = ((Matrix.Identity
+                    if (Empire.Universe.viewState < UniverseScreen.UnivScreenState.SectorView)
+                        shield.World = ((Matrix.Identity
                                      * Matrix.CreateScale(shield.Radius / 100f))
                                     * Matrix.CreateRotationZ(shield.Rotation))
                                    * Matrix.CreateTranslation(shield.Center.X, shield.Center.Y, 2500f);
@@ -161,6 +161,7 @@ namespace Ship_Game
                     shield.displacement += 0.085f;
                     shield.texscale -= 0.185f;
                 }
+                using(ShieldList.AcquireReadLock())
                 for (int index = 0; index < ShieldList.Count; index++)
                 {
                     Shield shield = ShieldList[index];
@@ -176,7 +177,7 @@ namespace Ship_Game
                     }
                 }
             }
-		    lock (GlobalStats.ShieldLocker)
+		    using(ShieldList.AcquireWriteLock())
             {
                 for (int i = ShieldList.Count - 1; i >= 0; --i)
                 {
