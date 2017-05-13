a282b192a8fa8900ed22b0896b4b5438339c5103
crunchygremlin <crunchygremlin@gmail>
2017-05-13 13:23:33 -0700

    this is what prevents ship loads in the shipyard. Now as to why and how it became broken? i dont know. I am not even sure how to fix it.
    --HG--
    branch : develop

--- Ship_Game/SlotStruct.cs
+++ Ship_Game/SlotStruct.cs
@@ -1,24 +1,23 @@
 using Microsoft.Xna.Framework.Graphics;
 using Ship_Game.Gameplay;
-using System;
 
 namespace Ship_Game
 {
-	public sealed class SlotStruct
-	{
-		public Restrictions Restrictions;
-		public PrimitiveQuad pq;
-		public float facing;
-		public bool CheckedConduits;
-		public SlotStruct parent;
-		public ShipDesignScreen.ActiveModuleState state;
-		public bool Powered;
-		public ModuleSlotData slotReference;
-		public string ModuleUID;
-		public ShipModule module;
-		public string SlotOptions;
-		public Texture2D tex;
-		public bool ShowValid;
-		public bool ShowInvalid;
-	}
+    public sealed class SlotStruct
+    {
+        public Restrictions Restrictions;
+        public PrimitiveQuad PQ;
+        public float Facing;
+        public bool CheckedConduits;
+        public SlotStruct Parent;
+        public ShipDesignScreen.ActiveModuleState State;
+        public bool Powered;
+        public ModuleSlotData SlotReference;
+        public string ModuleUID;
+        public ShipModule Module;
+        public string SlotOptions;
+        public Texture2D Tex;
+        public bool ShowValid = true; //These values are not being set correctly in the shipyard so they are always default. false
+        public bool ShowInvalid = true;
+    }
 }
