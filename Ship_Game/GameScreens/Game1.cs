64c93576f406745271b4286fc59e8289eb4fc60d
SeanMirrsen <controllersean@mail.ru>
2017-06-28 13:47:44 +0400

    More fixing of salvo logic and AA level selection Removes 8xMSAA override option (why was that even there?)
    --HG--
    branch : CSJM_Actual

--- Ship_Game/GameScreens/Game1.cs
+++ Ship_Game/GameScreens/Game1.cs
@@ -135,24 +135,17 @@ namespace Ship_Game
             GraphicsAdapter a = e.GraphicsDeviceInformation.Adapter;
             PresentationParameters p = e.GraphicsDeviceInformation.PresentationParameters;
 
+            MultiSampleType samples = (MultiSampleType)GlobalStats.AntiAlias;
             if (a.CheckDeviceMultiSampleType(DeviceType.Hardware, a.CurrentDisplayMode.Format, 
-                                                   false, MultiSampleType.TwoSamples, out int quality))
+                                                   false, samples, out int quality))
             {
                 p.MultiSampleQuality = (quality == 1 ? 0 : 1);
-                p.MultiSampleType    = (MultiSampleType)GlobalStats.AntiAlias; //MultiSampleType.FourSamples
+                p.MultiSampleType    = samples;
             }
             else
             {
                 p.MultiSampleType    = MultiSampleType.None;
                 p.MultiSampleQuality = 0;
-            }
-
-            if (GlobalStats.AntiAlias8XOverride && a.CheckDeviceMultiSampleType(DeviceType.Hardware, a.CurrentDisplayMode.Format, false, MultiSampleType.EightSamples, out quality))
-            {
-                // even if a greater quality is returned, we only want quality 0
-                p.MultiSampleQuality = 0;
-                p.MultiSampleType = MultiSampleType.None; //EightSamples
-                    
             }
 
             e.GraphicsDeviceInformation.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PlatformContents;
