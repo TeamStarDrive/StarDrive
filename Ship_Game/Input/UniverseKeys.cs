afd5ede6eaab6bbf4b9eb2c6b04c3afb441f7379
RedFox <jorma.rebane@gmail.com>
2017-07-03 01:08:47 +0300

    New event based input system with remappable keys. This will be cool as heck
    --HG--
    branch : develop

---
+++ Ship_Game/Input/UniverseKeys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game
+{
+    public enum UniverseKeys
+    {
+
+
+
+
+        Fleet1,
+        Fleet2,
+        Fleet3,
+        Fleet4,
+        Fleet5,
+        Fleet6,
+        Fleet7,
+        Fleet8,
+        Fleet9,
+
+        ShowPieMenu,
+    }
+}
