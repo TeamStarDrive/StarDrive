98026c71e8285b0b1cc29ca1e4807dd56cf3cd7f
crunchygremlin <crunchygremlin@gmail>
2017-07-22 16:00:51 -0700

    fix tooltip rectangle and fade in fade out. add tooltip for shipyard arc change. engrish only atm.
    --HG--
    branch : develop

--- Ship_Game/Tooltips.cs
+++ Ship_Game/Tooltips.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace Ship_Game
 {
@@ -10,5 +11,6 @@ namespace Ship_Game
 		public Tooltips()
 		{
 		}
+
 	}
 }
