6cca884cdff5bb3298b055f22624cc769793dfdf
crunchygremlin <crunchygremlin@gmail>
2017-08-13 16:46:02 -0700

    fix issue #1180 fix loading of diplomacy dialogs causing a crash in diplomacy screens. fix stat values being 0 in shipyard
    --HG--
    branch : develop

--- Ship_Game/Gameplay/DialogOption.cs
+++ Ship_Game/Gameplay/DialogOption.cs
@@ -1,3 +1,4 @@
+using System.Xml.Serialization;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -8,11 +9,11 @@ namespace Ship_Game.Gameplay
     {
         public object Target;
 
-        // ReSharper disable once InconsistentNaming -- XML Compatibility
-        public int number;
+        [XmlElement(ElementName = "number")]
+        public int Number;
 
-        // ReSharper disable once InconsistentNaming -- XML Compatibility
-        public string words;
+        [XmlElement(ElementName = "words")]
+        public string Words;
 
         private Rectangle _clickRect;
 
@@ -28,15 +29,15 @@ namespace Ship_Game.Gameplay
 
         public DialogOption(int n, string w, Vector2 cursor, SpriteFont font)
         {
-            number = n;
-            words = w;
+            Number = n;
+            Words = w;
             int width = (int)font.MeasureString(w).X;
             _clickRect = new Rectangle((int)cursor.X, (int)cursor.Y, width, font.LineSpacing);
         }
 
         public void Draw(ScreenManager screenManager, SpriteFont font)
         {
-            HelperFunctions.DrawDropShadowText(screenManager, string.Concat(number.ToString(), ". ", words),
+            HelperFunctions.DrawDropShadowText(screenManager, string.Concat(Number.ToString(), ". ", Words),
                 new Vector2(_clickRect.X, _clickRect.Y), font, (Hover ? Color.White : new Color(255, 255, 255, 220)));
         }
 
