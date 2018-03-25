6379bcd62708c45335111b5318f27a1f576d44d5
crunchygremlin <crunchygremlin@gmail>
2018-03-25 00:25:06 -0700

    upload texas Alpha 300 fix crash in fleet design when loading design over existing. fix fleet save screen ui issue. more work on screen pausing.
    --HG--
    branch : develop

--- Ship_Game/GameScreens/SaveFleetDesignScreen.cs
+++ Ship_Game/GameScreens/SaveFleetDesignScreen.cs
@@ -5,27 +5,27 @@ using Ship_Game.AI;
 
 namespace Ship_Game
 {
-	public sealed class SaveFleetDesignScreen : GenericLoadSaveScreen, IDisposable
-	{
+    public sealed class SaveFleetDesignScreen : GenericLoadSaveScreen, IDisposable
+    {
         private Fleet f;
 
-		public SaveFleetDesignScreen(GameScreen parent, Fleet f) 
+        public SaveFleetDesignScreen(GameScreen parent, Fleet f) 
             : base(parent, SLMode.Save, f.Name, "Save Fleet As...", "Saved Fleets", "Saved Fleet already exists.  Overwrite?", 40)
         {
-			this.f = f;        // set save file data and starting name
+            this.f = f;        // set save file data and starting name
             this.Path = string.Concat(Dir.ApplicationData, "/StarDrive/Fleet Designs/");
         }
 
-		public override void DoSave()
-		{
-			FleetDesign d = new FleetDesign()
-			{
-				Name = this.EnterNameArea.Text
-			};
-			foreach (FleetDataNode node in f.DataNodes)
-			{
-				d.Data.Add(node);
-			}
+        public override void DoSave()
+        {
+            FleetDesign d = new FleetDesign()
+            {
+                Name = this.EnterNameArea.Text
+            };
+            foreach (FleetDataNode node in f.DataNodes)
+            {
+                d.Data.Add(node);
+            }
             try
             {
                 d.FleetIconIndex = f.FleetIconIndex;
@@ -34,19 +34,26 @@ namespace Ship_Game
                 Serializer.Serialize(WriteFileStream, d);
                 WriteFileStream.Close();
             }
-            catch { }
-		}
+            catch(Exception e)
+            {
+                Log.Error(e, "Save Fleet Failed");
+            }
+            finally
+            {
+                ExitScreen();
+            }
+        }
 
-		protected override void SetSavesSL()
-		{
-			foreach (FileInfo info in Dir.GetFiles(Path))
-			{
-			    this.SavesSL.AddItem(new FileData(info, info, info.NameNoExt())).AddItemWithCancel(info);
-			}
-			foreach (FileInfo info in Dir.GetFiles("Content/FleetDesigns"))
-			{
-			    this.SavesSL.AddItem(new FileData(info, info, info.NameNoExt()));
-			}
-		}
-	}
+        protected override void SetSavesSL()
+        {
+            foreach (FileInfo info in Dir.GetFiles(Path))
+            {
+                this.SavesSL.AddItem(new FileData(info, info, info.NameNoExt())).AddItemWithCancel(info);
+            }
+            foreach (FileInfo info in Dir.GetFiles("Content/FleetDesigns"))
+            {
+                this.SavesSL.AddItem(new FileData(info, info, info.NameNoExt()));
+            }
+        }
+    }
 }
