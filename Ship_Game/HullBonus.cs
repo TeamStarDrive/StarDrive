28108ab54601b344f8c18cdcd915bf80d79e044b
RedFox <jorma.rebane@gmail.com>
2018-05-09 03:06:52 +0300

    Refactored ShipDesignScreen ConduitGraphics
    --HG--
    branch : shipyard_refacto

--- Ship_Game/HullBonus.cs
+++ Ship_Game/HullBonus.cs
@@ -19,8 +19,16 @@ namespace Ship_Game
         public float DamageBonus; // % weapon damage increase
         public float ShieldBonus; // % shield power increase
 
-        public HullBonus()
-        {
-        }
+        public static HullBonus Default = new HullBonus { Hull = "" };
+       
+        public float ArmoredModifier  => 1.0f + ArmoredBonus;
+        public float SensorModifier   => 1.0f + SensorBonus;
+        public float SpeedModifier    => 1.0f + SpeedBonus;
+        public float CargoModifier    => 1.0f + CargoBonus;
+        public float FireRateModifier => 1.0f + FireRateBonus;
+        public float RepairModifier   => 1.0f + RepairBonus;
+        public float CostModifier     => 1.0f + CostBonus;
+        public float DamageModifier   => 1.0f + DamageBonus;
+        public float ShieldModifier   => 1.0f + ShieldBonus;
     }
 }
