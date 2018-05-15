9e0601c647554b75de93b4cf22c70cf9654076a9
RedFox <jorma.rebane@gmail.com>
2018-05-15 22:07:46 +0300

    Refactored all ToggleButton's to introduce some sanity into our UI code.
    --HG--
    branch : develop

--- Ship_Game/UI/ToggleButton.cs
+++ Ship_Game/UI/ToggleButton.cs
@@ -3,6 +3,107 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Ship_Game.UI
 {
+    public class ToggleButtonStyle
+    {
+        public int Width  { get; private set; }
+        public int Height { get; private set; }
+
+        public Texture2D Active   { get; private set; }
+        public Texture2D Inactive { get; private set; }
+        public Texture2D Hover    { get; private set; }
+        public Texture2D Press    { get; private set; }
+
+        public static readonly ToggleButtonStyle Formation = new ToggleButtonStyle
+        {
+            Width  = 24,
+            Height = 24,
+            Active   = ResourceManager.Texture("SelectionBox/button_formation_active"),
+            Inactive = ResourceManager.Texture("SelectionBox/button_formation_inactive"),
+            Hover    = ResourceManager.Texture("SelectionBox/button_formation_hover"),
+            Press    = ResourceManager.Texture("SelectionBox/button_formation_pressed"),
+        };
+
+        public static readonly ToggleButtonStyle Grid = new ToggleButtonStyle
+        {
+            Width  = 34,
+            Height = 24,
+            Active   = ResourceManager.Texture("SelectionBox/button_grid_active"),
+            Inactive = ResourceManager.Texture("SelectionBox/button_grid_inactive"),
+            Hover    = ResourceManager.Texture("SelectionBox/button_grid_hover"),
+            Press    = ResourceManager.Texture("SelectionBox/button_grid_pressed"),
+        };
+
+        public static readonly ToggleButtonStyle PlayerDesigns = new ToggleButtonStyle
+        {
+            Width  = 29,
+            Height = 20,
+            Active   = ResourceManager.Texture("SelectionBox/PlayerDesignsPressed"),
+            Inactive = ResourceManager.Texture("SelectionBox/PlayerDesignsActive"),
+            Hover    = ResourceManager.Texture("SelectionBox/button_grid_hover"),
+            Press    = ResourceManager.Texture("SelectionBox/button_grid_pressed"),
+        };
+
+        public static readonly ToggleButtonStyle ArrowLeft = new ToggleButtonStyle
+        {
+            Width  = 14,
+            Height = 35,
+            Active   = ResourceManager.Texture("SelectionBox/button_arrow_left"),
+            Inactive = ResourceManager.Texture("SelectionBox/button_arrow_left"),
+            Hover    = ResourceManager.Texture("SelectionBox/button_arrow_left_hover"),
+            Press    = ResourceManager.Texture("SelectionBox/button_arrow_left_hover"),
+        };
+
+        public static readonly ToggleButtonStyle ArrowRight = new ToggleButtonStyle
+        {
+            Width  = 14,
+            Height = 35,
+            Active   = ResourceManager.Texture("SelectionBox/button_arrow_right"),
+            Inactive = ResourceManager.Texture("SelectionBox/button_arrow_right"),
+            Hover    = ResourceManager.Texture("SelectionBox/button_arrow_right_hover"),
+            Press    = ResourceManager.Texture("SelectionBox/button_arrow_right_hover"),
+        };
+
+        public static readonly ToggleButtonStyle ButtonB = new ToggleButtonStyle
+        {
+            Width  = 25,
+            Height = 22,
+            Active   = ResourceManager.Texture("Minimap/button_B_normal"),
+            Inactive = ResourceManager.Texture("Minimap/button_B_normal"),
+            Hover    = ResourceManager.Texture("Minimap/button_B_hover"),
+            Press    = ResourceManager.Texture("Minimap/button_B_normal"),
+        };
+
+        public static readonly ToggleButtonStyle ButtonC = new ToggleButtonStyle
+        {
+            Width  = 25,
+            Height = 22,
+            Active   = ResourceManager.Texture("Minimap/button_C_normal"),
+            Inactive = ResourceManager.Texture("Minimap/button_C_normal"),
+            Hover    = ResourceManager.Texture("Minimap/button_hover"),
+            Press    = ResourceManager.Texture("Minimap/button_C_normal"),
+        };
+
+        public static readonly ToggleButtonStyle Button = new ToggleButtonStyle
+        {
+            Width  = 25,
+            Height = 22,
+            Active   = ResourceManager.Texture("Minimap/button_active"),
+            Inactive = ResourceManager.Texture("Minimap/button_normal"),
+            Hover    = ResourceManager.Texture("Minimap/button_hover"),
+            Press    = ResourceManager.Texture("Minimap/button_normal"),
+        };
+
+        public static readonly ToggleButtonStyle ButtonDown = new ToggleButtonStyle
+        {
+            Width  = 25,
+            Height = 26,
+            Active   = ResourceManager.Texture("Minimap/button_active"),
+            Inactive = ResourceManager.Texture("Minimap/button_down_inactive"),
+            Hover    = ResourceManager.Texture("Minimap/button_down_hover"),
+            Press    = ResourceManager.Texture("Minimap/button_down_inactive"),
+        };
+    }
+
     public class ToggleButton : UIElementV2
     {
         public object ReferenceObject;
@@ -10,18 +111,12 @@ namespace Ship_Game.UI
         public string Action = "";
 
         public bool Active;
-
         public bool Hover;
-
-        public int WhichToolTip;
-
-        public bool HasToolTip;
-
-        public Color BaseColor = Color.White;
-
         private bool Pressed;
 
-        private string HoverText;
+        public int WhichToolTip;
+        public bool HasToolTip;
+        public Color BaseColor = Color.White;
 
         private readonly Texture2D PressTexture;
         private readonly Texture2D HoverTexture;
@@ -36,48 +131,56 @@ namespace Ship_Game.UI
         public delegate void ClickHandler(ToggleButton button);
         public event ClickHandler OnClick;
 
-        public ToggleButton(Rectangle r, string activePath, string inactivePath, string hoverPath, string pressPath, string iconPath, UIElementV2 container = null) : base(container, r)
-        {           
-            Rect            = r;
-            PressTexture    = ResourceManager.Texture(pressPath);
-            HoverTexture    = ResourceManager.Texture(hoverPath);
-            ActiveTexture   = ResourceManager.Texture(activePath);
-            InactiveTexture = ResourceManager.Texture(inactivePath);                        
-            IconTexture     = ResourceManager.Texture(iconPath,"");
-            IconActive      = ResourceManager.Texture(string.Concat(iconPath, "_active"), "");
+        public ToggleButton(Vector2 pos, ToggleButtonStyle style, string iconPath = "", UIElementV2 container = null)
+            : base(container, new Rectangle((int)pos.X, (int)pos.Y, style.Width, style.Height))
+        {
+            PressTexture    = style.Press;
+            HoverTexture    = style.Hover;
+            ActiveTexture   = style.Active;
+            InactiveTexture = style.Inactive;      
             
+            if (iconPath.NotEmpty())
+            {
+                IconTexture = ResourceManager.Texture(iconPath, "");
+                IconActive  = ResourceManager.Texture(iconPath+"_active", "");
+            }
 
             if (IconTexture == null)
             {
                 IconPath = iconPath;
-                WordPos = new Vector2(Rect.X + 12 - Fonts.Arial12Bold.MeasureString(IconPath).X / 2f, Rect.Y + 12 - Fonts.Arial12Bold.LineSpacing / 2);             
+                WordPos = new Vector2(Rect.X + 12 - Fonts.Arial12Bold.MeasureString(IconPath).X / 2f,
+                    Rect.Y + 12 - Fonts.Arial12Bold.LineSpacing / 2);             
             }
             else
-                IconRect = new Rectangle(Rect.X + Rect.Width / 2 - IconTexture.Width / 2
-                    , Rect.Y + Rect.Height / 2 - IconTexture.Height / 2
-                    , IconTexture.Width, IconTexture.Height);
+            {
+                IconRect = new Rectangle(Rect.X + Rect.Width  / 2 - IconTexture.Width  / 2,
+                    Rect.Y + Rect.Height / 2 - IconTexture.Height / 2,
+                    IconTexture.Width, IconTexture.Height);
+            }
         }
 
         //hack... until this is all straightend out to allow override of base draw.
-        public void Draw(ScreenManager screenManager)            => Draw(screenManager.SpriteBatch);
-        public void DrawIconResized(ScreenManager screenManager) => Draw(screenManager.SpriteBatch, true);
-        public override void Draw(SpriteBatch spriteBatch)       => Draw(spriteBatch, false);
+        public void Draw(ScreenManager screenManager) => Draw(screenManager.SpriteBatch);
         
-
-        public void Draw(SpriteBatch spriteBatch, bool resizeIcon, ScreenManager screenManager = null)
+        public override void Draw(SpriteBatch spriteBatch)
         {
-            Rectangle iconRect = IconActive == null ? IconRect : Rect;
-            
             if (Pressed)
+            {
                 spriteBatch.Draw(PressTexture, Rect, Color.White);
+            }
             else if (Hover)
             {
                 spriteBatch.Draw(HoverTexture, Rect, Color.White);                
             }
             else if (Active)
+            {
                 spriteBatch.Draw(ActiveTexture, Rect, Color.White);
-            else if (!Active)
+            }
+            else
+            {
                 spriteBatch.Draw(InactiveTexture, Rect, Color.White);
+            }
+
             if (IconTexture == null)
             {
                 if (Active)
@@ -89,7 +192,10 @@ namespace Ship_Game.UI
                 spriteBatch.DrawString(Fonts.Arial12Bold, IconPath, WordPos, Color.Gray);
             }
             else
+            {
+                Rectangle iconRect = IconActive == null ? IconRect : Rect;
                 spriteBatch.Draw(IconActive ?? IconTexture, iconRect, Color.White);            
+            }
         }
 
         public override void PerformLegacyLayout(Vector2 pos)
