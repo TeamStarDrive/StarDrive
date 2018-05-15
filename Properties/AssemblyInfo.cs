d85854ea30cf297581144c30e7183e1a8bbe5118
crunchygremlin <crunchygremlin@gmail>
2018-05-15 03:07:27 -0700

    upload texas alpha 205
    --HG--
    branch : TexasAlpha1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b38aad3b-18b8-41a8-b758-0e5614dafc49")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
-[assembly: AssemblyInformationalVersion("Texas_Alpha_1_Release_Candidate_209")]
+[assembly: AssemblyInformationalVersion("Texas_Alpha_1_Release_Candidate_205")]
