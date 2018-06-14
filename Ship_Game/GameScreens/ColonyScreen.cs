3d9435a8d6958a0688531ac4884707d2751d139d
MadMortalMud <_jf@outlook.com>
2018-06-14 17:39:13 +0200

    Fix issue #1544, include optional ignoreLeftMouseReleased argument in ToggleButton.HandleInput Allow the caller of ToggleButton.HandleInput to pass a flag that, if true, will cause the LeftMouseReleased event to be ignored. Default is false.
    --HG--
    branch : fix_Colony_Screen

--- Ship_Game/GameScreens/ColonyScreen.cs
+++ Ship_Game/GameScreens/ColonyScreen.cs
@@ -2014,7 +2014,8 @@ namespace Ship_Game
             {
                 ToolTip.CreateTooltip(Localizer.Token(2280));
             }
-            if ((input.Right || RightColony.HandleInput(input)) && (Empire.Universe.Debug || this.p.Owner == EmpireManager.Player))
+            if ((input.Right || RightColony.HandleInput(input, ignoreLeftMouseReleased: true))
+                && (Empire.Universe.Debug || this.p.Owner == EmpireManager.Player))
             {
                 try
                 {
@@ -2037,13 +2038,18 @@ namespace Ship_Game
                 }
                 return true;
             }
-            if ((input.Left || this.LeftColony.HandleInput(input)) && (Empire.Universe.Debug || this.p.Owner == EmpireManager.Player))
+            if ((input.Left || this.LeftColony.HandleInput(input, ignoreLeftMouseReleased: true)) 
+                && (Empire.Universe.Debug || this.p.Owner == EmpireManager.Player))
             {
                 int thisindex = this.p.Owner.GetPlanets().IndexOf(this.p);
                 thisindex = (thisindex <= 0 ? this.p.Owner.GetPlanets().Count - 1 : thisindex - 1);
                 if (this.p.Owner.GetPlanets()[thisindex] != this.p)
                 {
-                    this.p = this.p.Owner.GetPlanets()[thisindex];
+                    //Console.Write("Switch Colony Screen");
+                    //Console.WriteLine(thisindex);
+                    //System.Threading.Thread.Sleep(1000);
+
+                    p = p.Owner.GetPlanets()[thisindex];
                     Empire.Universe.workersPanel = new ColonyScreen(Empire.Universe, p, eui);
                 }
                 if (input.MouseCurr.RightButton != ButtonState.Released || this.previousMouse.RightButton != ButtonState.Released)
