1e1f02cb423e74d4d9c524440971a77b2e5461f2
Fizban1978 <fizban34@yahoo.com>
2018-06-22 13:50:06 +0300

    Added ship maintenance class, but cant get it to work yet
    --HG--
    branch : SetupDynamicShipUpkeep

---
+++ Ship_Game/Ships/ShipMaintenance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Ship_Game.Gameplay;
+
+namespace Ship_Game.Ships
+{
+    public class ShipMaintenance
+    {
+        private const float BaseMaintModifier = 0.004f;
+        private ShipData shipData;
+
+        private bool IsFreeUpkeepShip(ShipData.RoleName role, Empire empire, Ship ship)
+        {
+            return shipData.ShipStyle == "Remnant"
+                   || empire?.data == null
+                   || ship.loyalty.data.PrototypeShip == ship.Name
+                   || (ship.Mothership != null && role >= ShipData.RoleName.fighter && role <= ShipData.RoleName.frigate);
+        }
+
+        public float GetMaintenanceCost(Empire empire, Ship ship, float shipCost, bool withModifiers = false, int numShipYards = 0)
+        {
+            ShipData.RoleName role = shipData.HullRole;
+            if (IsFreeUpkeepShip(role, empire, ship))
+                return 0;
+
+            float maint = shipCost * BaseMaintModifier;
+            if (role == ShipData.RoleName.freighter || role == ShipData.RoleName.platform)
+            {
+                maint *= empire.data.CivMaintMod;
+                if (empire.data.Privatization)
+                    maint *= 0.5f;
+            }
+            if (!withModifiers)
+                return  (float)Math.Round(maint, 2);
+
+
+            // Subspace Projectors do not get any more modifiers
+            if (ship.Name == "Subspace Projector")
+                return maint;
+            //added by gremlin shipyard exploit fix
+            if (ship.IsTethered())
+            {
+                if (role == ShipData.RoleName.platform)
+                    return maint * 0.5f;
+                if (shipData.IsShipyard)
+                {
+                    if (numShipYards > 3)
+                        maint *= numShipYards - 3;
+                }
+            }
+            float repairMaintModifier = 2 - ship.Health / ship.HealthMax;
+            maint *= repairMaintModifier;
+            return maint;
+        }
+    }
+}
