53d8c3a84b03b10e803c1d97f09cabb1ff17593e
Fizban1978 <fizban34@yahoo.com>
2018-07-05 00:36:22 +0300

    The auto load troop required a space port to fucntion (thats probably why people reported iit was not working). which is not logical , since you can launch any troops you want manually. I also added a minimum garrison (5 troops) so the colony wont be cleaned of troops. This really should be made a button in the colony screen where the player can set a the threshold himself.
    --HG--
    branch : A2_FixingFighterLaunchAndButtons

--- Ship_Game/Universe/SolarBodies/GeodeticManager.cs
+++ Ship_Game/Universe/SolarBodies/GeodeticManager.cs
@@ -298,8 +298,35 @@ namespace Ship_Game.Universe.SolarBodies
                     }
                 }
 
-                if (ship == null || ship.loyalty != Owner || !HasShipyard ||
-                    !ship.Position.InRadius(Center, 5000f)) continue;
+                if (ship == null || ship.loyalty != Owner || !ship.Position.InRadius(Center, 5000f))
+                    continue;
+
+                //auto load troop
+                using (TroopsHere.AcquireWriteLock())
+                {
+                    if ((ParentSystem.combatTimer > 0 && ship.InCombat) || TroopsHere.IsEmpty ||
+                        TroopsHere.Any(troop => troop.GetOwner() != Owner))
+                        continue;
+                    foreach (var pgs in TilesList)
+                    {
+                        // FB: I added here a minimum threshold of 5 troops to stay as garrison so the auto load wont clean the colony
+                        // But this should be made at a button for the player to decide how many troops he wants to leve as a garrison in ship colony screen
+                        if (ship.TroopCapacity == 0 || ship.TroopList.Count >= ship.TroopCapacity || TroopsHere.Count < 5)
+                            break;
+
+                        using (pgs.TroopsHere.AcquireWriteLock())
+                            if (pgs.TroopsHere.Count > 0 && pgs.TroopsHere[0].GetOwner() == Owner)
+                            {
+                                Troop troop = pgs.TroopsHere[0];
+                                ship.TroopList.Add(troop);
+                                pgs.TroopsHere.Clear();
+                                TroopsHere.Remove(troop);
+                            }
+                    }
+                }
+
+                if (!HasShipyard)
+                    continue;
 
                 if (!ship.InCombat)
                 {
@@ -325,27 +352,6 @@ namespace Ship_Game.Universe.SolarBodies
                     ship.AI.HasPriorityOrder = false;
                     ship.AI.State = AIState.AwaitingOrders;
 
-                }
-                //auto load troop
-                using (TroopsHere.AcquireWriteLock())
-                {
-                    if ((ParentSystem.combatTimer > 0 && ship.InCombat) || TroopsHere.IsEmpty ||
-                        TroopsHere.Any(troop => troop.GetOwner() != Owner))
-                        continue;
-                    foreach (var pgs in TilesList)
-                    {
-                        if (ship.TroopCapacity == 0 || ship.TroopList.Count >= ship.TroopCapacity)
-                            break;
-
-                        using (pgs.TroopsHere.AcquireWriteLock())
-                            if (pgs.TroopsHere.Count > 0 && pgs.TroopsHere[0].GetOwner() == Owner)
-                            {
-                                Troop troop = pgs.TroopsHere[0];
-                                ship.TroopList.Add(troop);
-                                pgs.TroopsHere.Clear();
-                                TroopsHere.Remove(troop);
-                            }
-                    }
                 }
             }
         }
