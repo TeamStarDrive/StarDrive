abd26e40f045b14ae80cb7ceabe3cfff05484b0b
crunchygremlin <crunchygremlin@gmail>
2018-07-07 22:14:49 -0700

    add file to repo...
    --HG--
    branch : ProjectorSize

---
+++ Ship_Game/Empires/SubSpaceProjectors.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Ship_Game.Empires
+{
+    public class SubSpaceProjectors
+    {
+        public static void SetProjectorSize(float universeWidth) => ProjectorRadius = universeWidth * .04f;
+        public static float ProjectorRadius { get; private set; } = 150000f;
+    }
+}
