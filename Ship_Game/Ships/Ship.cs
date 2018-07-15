aa37b801a721851a8250fa7b74ad71b2b02e5a57
Fizban1978 <fizban34@yahoo.com>
2018-07-15 21:12:59 +0300

    Moved struct to Power.cs made power calc static. Next step it to check shipyard.
    --HG--
    branch : A2_AlignWarpCalcsInGoodForGoal

--- Ship_Game/Ships/Ship.cs
+++ Ship_Game/Ships/Ship.cs
@@ -2270,7 +2270,7 @@ namespace Ship_Game.Ships
                 }
             }
 
-            NetPower = CalcPower(loyalty, ShieldsWarpBehavior.OnFullChargeAtWarpExit);
+            NetPower = Power.Calculate(ModuleSlotList, loyalty, ShieldsWarpBehavior.OnFullChargeAtWarpExit);
             NormalWarpThrust = WarpThrust;
             //Doctor: Add fixed tracking amount if using a mixed method in a mod or if only using the fixed method.
             TrackingPower += FixedTrackingPower;
@@ -2937,64 +2937,6 @@ namespace Ship_Game.Ships
         //it should count all but "NotApplicable"
         private const int ShipStatusCount = 5;
 
-        public struct Power
-        {
-            public float NetSubLightPowerDraw;
-            public float NetWarpPowerDraw;
-
-        }
-
-        private Power CalcPower(Empire empire, ShieldsWarpBehavior behavior)
-        {
-            float nonShieldPowerDraw = 0f;
-            float shieldPowerDraw    = 0f;
-            float warpPowerDrawBonus = 0f;
-
-            foreach(ShipModule module in ModuleSlotList)
-            {
-                if (!module.Active || (!module.Powered && module.PowerDraw > 0f)) 
-                    continue;
-
-                if (module.Is(ShipModuleType.Shield))
-                    shieldPowerDraw += module.PowerDraw;
-                if (behavior == ShieldsWarpBehavior.OnFullChargeAtWarpExit)
-                    warpPowerDrawBonus += module.PowerDrawAtWarp; // FB: include bonuses to warp if shields are on at warp
-                else
-                {
-                    nonShieldPowerDraw += module.PowerDraw;
-                    warpPowerDrawBonus += module.PowerDrawAtWarp;
-                }
-            }
-            float subLightPowerDraw      = shieldPowerDraw + nonShieldPowerDraw;
-            float warpPowerDrainModifier = empire?.data.FTLPowerDrainModifier ?? 1;
-            float warpPowerDraw          = 0f;
-            switch (behavior)
-            {
-                case ShieldsWarpBehavior.OnFullChargeAtWarpExit:
-                {
-                    warpPowerDraw = (shieldPowerDraw + nonShieldPowerDraw) * warpPowerDrainModifier - (warpPowerDrawBonus * warpPowerDrainModifier / 2);
-                    break;
-                }
-                case ShieldsWarpBehavior.TurnOnDelayAtWarpExit:
-                {
-                        warpPowerDraw = nonShieldPowerDraw * warpPowerDrainModifier + shieldPowerDraw;
-                    break;
-                }
-                case ShieldsWarpBehavior.OffNoChargeAtWarpExit:
-                {
-                        warpPowerDraw = nonShieldPowerDraw * warpPowerDrainModifier;
-                    break;
-                }
-            }
-
-            return new Power
-            {
-                NetSubLightPowerDraw = subLightPowerDraw,
-                NetWarpPowerDraw = warpPowerDraw
-            };
-        }
-
-
 
     }
     
@@ -3006,15 +2948,6 @@ namespace Ship_Game.Ships
         Good,
         Excellent,
         NotApplicable
-
-    }
-
-    public enum ShieldsWarpBehavior
-    {
-        OnFullChargeAtWarpExit,
-        TurnOnDelayAtWarpExit,
-        OffNoChargeAtWarpExit
-
     }
 }
 
