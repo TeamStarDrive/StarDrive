a4e62745a2d2ddff48f9f3d1b458f7e77ce36aac
Fizban1978 <fizban34@yahoo.com>
2018-07-17 00:00:15 +0300

    moved enum out of struct.
    --HG--
    branch : ImplementShieldsBehavior

--- Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
+++ Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
@@ -6,7 +6,7 @@ namespace Ship_Game
 {
     public sealed partial class ShipDesignScreen
     {
-        private class ShieldBehaviorDropDown : DropOptions<Power.ShieldsWarpBehavior>
+        private class ShieldBehaviorDropDown : DropOptions<ShieldsWarpBehavior>
         {
             public ShieldBehaviorDropDown(UIElementV2 parent, Rectangle dropdownRect) : base(parent, dropdownRect)
             {
@@ -17,10 +17,10 @@ namespace Ship_Game
                 {
                     switch (ActiveValue)
                     {
-                        case Power.ShieldsWarpBehavior.OnFullChargeAtWarpExit: ToolTip.CreateTooltip("Shields stay online during warp, they will draw more power"); break;
-                        case Power.ShieldsWarpBehavior.LowDischargeDownTo50Percent: ToolTip.CreateTooltip("Shields will not consume excess power at warp but will discharge slowly to a minimum of 50% of total shield power"); break;
-                        case Power.ShieldsWarpBehavior.MediumDischargeDownTo25Percent: ToolTip.CreateTooltip("Shields will consume less power at warp but will discharge to a minimum of 25% of total shield power"); break;
-                        case Power.ShieldsWarpBehavior.HighDischargeDownTo0Percent: ToolTip.CreateTooltip("Shields will not consume power at warp but will discharge quickly to zero"); break;
+                        case ShieldsWarpBehavior.OnFullChargeAtWarpExit: ToolTip.CreateTooltip("Shields stay online during warp, they will draw more power"); break;
+                        case ShieldsWarpBehavior.LowDischargeDownTo50Percent: ToolTip.CreateTooltip("Shields will not consume excess power at warp but will discharge slowly to a minimum of 50% of total shield power"); break;
+                        case ShieldsWarpBehavior.MediumDischargeDownTo25Percent: ToolTip.CreateTooltip("Shields will consume less power at warp but will discharge to a minimum of 25% of total shield power"); break;
+                        case ShieldsWarpBehavior.HighDischargeDownTo0Percent: ToolTip.CreateTooltip("Shields will not consume power at warp but will discharge quickly to zero"); break;
                     }
                 }
                 return base.HandleInput(input);
