91fd425ed83f925765e6ab8de0be71942f249291
Fizban1978 <fizban34@yahoo.com>
2018-07-16 10:01:36 +0300

    Uploaded RC2 A2 477. More Fleet fixes. Fixed ship scaling in ship design Power calculations are now done in a single place.
    --HG--
    branch : release/Texas_A2_RC2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b38aad3b-18b8-41a8-b758-0e5614dafc49")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
-[assembly: AssemblyInformationalVersion("Texas_A2_RC2_462")]
+[assembly: AssemblyInformationalVersion("Texas_A2_RC2_477")]
