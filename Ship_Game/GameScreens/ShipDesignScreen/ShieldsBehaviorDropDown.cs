ef52d5097c47fdc6edb7e3602f875dd39b0d11aa
Fizban1978 <fizban34@yahoo.com>
2018-07-17 17:29:26 +0300

    Changed shield behavior optinons to more generic. bug fixes. changes after review
    --HG--
    branch : ImplementShieldsBehavior

--- Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
+++ Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
@@ -13,21 +13,17 @@ namespace Ship_Game
             }
             public override bool HandleInput(InputState input)
             {
-                if (Rect.HitTest(input.CursorPosition)) //fbedard: add tooltip for CategoryList
+                if (Rect.HitTest(input.CursorPosition)) 
                 {
-                    switch (ActiveValue)
-                    {
-                        case ShieldsWarpBehavior.Fully_Powered: ToolTip.CreateTooltip("Shields stay online during warp and also recharge, " +
-                                                                                      "they will draw more power, like other modules"); break;
-                        case ShieldsWarpBehavior.Maintained_With_Acticvation: ToolTip.CreateTooltip("Shields will not consume excess power at warp but " +
-                                                                                                    "they will not recharge during warp and upon exit" +
-                                                                                                    ", they will need to be brought online. The chance " +
-                                                                                                    "to bring them online is affected by ship level and shield" +
-                                                                                                    " complexcity"); break;
-                        case ShieldsWarpBehavior.Discharged_With_Acticvation: ToolTip.CreateTooltip("Shields will not consume power at warp but will discharge" +
-                                                                                                    " slowly to zero. They will also need activation after" +
-                                                                                                    " warp exit and with less chances"); break;
-                    }
+                    var tooltips = new string[] {
+                        "Shields stay online during warp and also recharge, they will draw more power, like other modules",
+                        "Shields will not consume excess power at warp but they will not recharge during warp. Upon warp exit" +
+                        ", they will need to be actvicated. The chance activate them is affected by ship level and shield complexcity",
+                        "Shields will not consume power at warp but will discharge slowly to zero. They will also need activation after" +
+                        " warp exit and with less chances"
+                    };
+                    string tooltip = tooltips[(int)ActiveValue];
+                    ToolTip.CreateTooltip(tooltip);
                 }
                 return base.HandleInput(input);
             }
