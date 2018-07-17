479d78f372bb08e87891253c5a961ff20c706dc6
Fizban1978 <fizban34@yahoo.com>
2018-07-18 01:05:43 +0300

    Fixed stupid typos :(
    --HG--
    branch : ImplementShieldsBehavior

--- Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
+++ Ship_Game/GameScreens/ShipDesignScreen/ShieldsBehaviorDropDown.cs
@@ -18,9 +18,9 @@ namespace Ship_Game
                     var tooltips = new string[] {
                         "Shields stay online during warp and also recharge, they will draw more power, like other modules",
                         "Shields will not consume excess power at warp but they will not recharge during warp. Upon warp exit" +
-                        ", they will need to be actvicated. The chance activate them is affected by ship level and shield complexcity",
+                        ", they will need to be activcated. The chance to activate them is affected by ship level and shield complexity",
                         "Shields will not consume power at warp but will discharge slowly to zero. They will also need activation after" +
-                        " warp exit and with less chances"
+                        " warp exit and will have lower activation chance"
                     };
                     string tooltip = tooltips[(int)ActiveValue];
                     ToolTip.CreateTooltip(tooltip);
