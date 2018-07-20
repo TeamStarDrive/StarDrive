aba967899b2689a52bc0ea7d5ce33941fb2ff1cf
RedFox <jorma.rebane@gmail.com>
2018-07-20 03:07:56 +0300

    I ended up fixing a bunch of targeting bugs.... UGH. Lets merge this. Colonist transporting and Goods transporting is also ok.
    --HG--
    branch : trading_refacto

---
+++ Ship_Game/ExtensionMethods/Vector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Ship_Game
+{
+    public static class Vector
+    {
+        // +Y is downwards on the screen
+        public static Vector2 Down() => new Vector2(0f, +1f);
+
+        // -Y is downwards on the screen
+        public static Vector2 Up() => new Vector2(0f, -1f);
+    }
+}
