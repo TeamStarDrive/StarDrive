972332f084eef3fc8713a6a512aa98d447b262b5
crunchygremlin <crunchygremlin@gmail>
2018-07-21 22:44:45 -0700

    further declutter debug window. i moved most of the unclassified debug messages to the normal page of the debug screen. trade stuff to the trade screen. ill need to build some other debug screens.
    Fixed some floating point errors in the build ratio logic. It would never satisfy the fighter ratios.
    adjusted budgets.
    
    --HG--
    branch : investigateBuildIssues

--- Ship_Game/AI/EmpireAI/EmpireAI.RunMilitaryPlanner.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.RunMilitaryPlanner.cs
@@ -305,17 +305,16 @@ namespace Ship_Game.AI
                     , DesiredCarriers, DesiredBombers, DesiredCapitals, DesiredTroops, DesiredSupport);
             }
 
-            private float SetCounts(float roleCount, float roleUpkeep, float capacity, float ratio, float totalRatio)
+            private int SetCounts(float roleCount, float roleUpkeep, float capacity, float ratio, float totalRatio)
             {
 
                 if (ratio < .01f) return 0;
-                //float normalizedRatioed = ratio / totalRatio;
+                
                 float shipUpkeep = Math.Max(roleUpkeep, 1) / Math.Max(roleCount, 1);                
-                //float possible = capacity / shipUpkeep;
-                //return possible * normalizedRatioed;
                 float mainRatio = shipUpkeep * ratio / TotalUpkeep;
                 float possible = capacity * mainRatio / shipUpkeep;
-                return possible;
+
+                return (int)possible;
 
                 
             }
@@ -440,7 +439,7 @@ namespace Ship_Game.AI
 
             private static bool CheckRoleAndScrap(ref float numShips, float desiredShips, Ship ship, ShipData.RoleName role)
             {
-                if (numShips <= desiredShips || ship.DesignRole != role || ship.fleet?.IsCoreFleet == false)
+                if (numShips <= (int)desiredShips || ship.DesignRole != role || ship.fleet?.IsCoreFleet == false)
                     return false;
                 numShips--;
                 ship.AI.OrderScrapShip();
@@ -456,16 +455,16 @@ namespace Ship_Game.AI
             //Find ship to build
            
             var pickRoles = new Map<ShipData.RoleName, float>();
-
-            PickRoles(ref buildRatios.NumFighters , buildRatios.DesiredFighters, ShipData.RoleName.fighter, pickRoles);
-            PickRoles(ref buildRatios.NumCorvettes, buildRatios.DesiredCorvettes, ShipData.RoleName.corvette, pickRoles);            
+            PickRoles(ref buildRatios.NumCarriers, buildRatios.DesiredCarriers, ShipData.RoleName.carrier, pickRoles);
+            PickRoles(ref buildRatios.NumTroops, buildRatios.DesiredTroops, ShipData.RoleName.troopShip, pickRoles);
+            PickRoles(ref buildRatios.NumSupport, buildRatios.DesiredSupport, ShipData.RoleName.support, pickRoles);                        
             PickRoles(ref buildRatios.NumFrigates , buildRatios.DesiredFrigates, ShipData.RoleName.frigate, pickRoles);
             PickRoles(ref buildRatios.NumBombers  , buildRatios.DesiredBombers, ShipData.RoleName.bomber, pickRoles);
             PickRoles(ref buildRatios.NumCruisers , buildRatios.DesiredCruisers, ShipData.RoleName.cruiser, pickRoles);
             PickRoles(ref buildRatios.NumCapitals , buildRatios.DesiredCapitals, ShipData.RoleName.capital, pickRoles);
-            PickRoles(ref buildRatios.NumCarriers , buildRatios.DesiredCarriers, ShipData.RoleName.carrier, pickRoles);            
-            PickRoles(ref buildRatios.NumTroops   , buildRatios.DesiredTroops, ShipData.RoleName.troopShip, pickRoles);
-            PickRoles(ref buildRatios.NumSupport  , buildRatios.DesiredSupport, ShipData.RoleName.support,  pickRoles);
+            PickRoles(ref buildRatios.NumFighters, buildRatios.DesiredFighters, ShipData.RoleName.fighter, pickRoles);
+            PickRoles(ref buildRatios.NumCorvettes, buildRatios.DesiredCorvettes, ShipData.RoleName.corvette, pickRoles);
+
 
 
 
@@ -482,8 +481,9 @@ namespace Ship_Game.AI
     
         private static void PickRoles(ref float numShips, float desiredShips, ShipData.RoleName role, Map<ShipData.RoleName, float>
              rolesPicked)
-        {
-            if (numShips > desiredShips) return;            
+        {            
+            if (numShips >= (int)desiredShips)
+                return;            
             rolesPicked.Add(role,  numShips / desiredShips);
         }
         public string PickFromCandidates(ShipData.RoleName role) => PickFromCandidates(role, false, ShipModuleType.Dummy);
