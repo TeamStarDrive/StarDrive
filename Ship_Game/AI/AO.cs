1ee6eb1764799ebab879202f9c487998ba22319e
crunchygremlin <crunchygremlin@gmail>
2018-07-24 00:14:44 -0700

    make sure that auto freighters can work without further modification.
    --HG--
    branch : MinorChanges

--- Ship_Game/AI/AO.cs
+++ Ship_Game/AI/AO.cs
@@ -214,7 +214,7 @@ namespace Ship_Game.AI
 
         public void Update()
         {
-            Empire.Universe.DebugWin.DrawCircle(Debug.DebugModes.AO, Center, Radius);
+            Empire.Universe?.DebugWin?.DrawCircle(Debug.DebugModes.AO, Center, Radius);
             if (OurPlanetsInAo.Length == 0 && Owner != null && PlanetsInAo.Length > 0)
             {
                 OurPlanetsInAo = PlanetsInAo.FilterBy(p => p.Owner == Owner);
