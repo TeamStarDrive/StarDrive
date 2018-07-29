af597dfaf212c0fcc23b7865ff87e835128888dc
RedFox <jorma.rebane@gmail.com>
2018-07-30 02:18:49 +0300

    Added IEffectCache which reduces duplicate shader loading. Misc refactoring.
    --HG--
    branch : fix_out_of_memory_due_to_shadows

---
+++ SynapseGaming-SunBurn-Pro/SynapseGaming/LightingSystem/Processors/IEffectCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SynapseGaming.LightingSystem.Processors
+{
+    public interface IEffectCache
+    {
+        bool TryGetEffect<T>(string assetName, out T asset) where T : Effect;
+        void AddEffect(string assetName, Effect effect);
+    }
+}
