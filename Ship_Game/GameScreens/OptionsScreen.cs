22d96789439fc539ec6a7fb7fca5529e49e82f2e
Gretman <arcmage5@hotmail.com>
2018-08-02 21:36:06 -0700

    Added a `Disable Asteroids` checkbox.
    --HG--
    branch : Disable_Asteroids_Button

--- Ship_Game/GameScreens/OptionsScreen.cs
+++ Ship_Game/GameScreens/OptionsScreen.cs
@@ -246,7 +246,9 @@ namespace Ship_Game
                 Checkbox(() => GlobalStats.ZoomTracking,        title: 6185, tooltip: 7082);
                 Checkbox(() => GlobalStats.AutoErrorReport, "Automatic Error Report", 
                                 "Send automatic error reports to Blackbox developers");
-                if (Debugger.IsAttached)
+                Checkbox(() => GlobalStats.DisableAsteroids, "Disable Asteroids",           //Added by Gretman
+                                "Disable all asteroids. This can significantly boost performance late`ngame, but reduces emersion when closely viewing solar systems.");
+            if (Debugger.IsAttached)
                     Checkbox(() => GlobalStats.WarpBehaviorsSetting, "Warp Behaviors (experimental)",
                         "Experimental and untested feature for complex Shield behaviors during Warp");
             EndLayout();
