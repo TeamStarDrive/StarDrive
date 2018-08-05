89ddd34cffb0697b69475ad5530aef0cf3e97233
Fizban1978 <fizban34@yahoo.com>
2018-08-05 15:32:39 +0300

    eagle eyes / blind are relevant for cannon weapons.
    --HG--
    branch : Fat_Bastard/RacialTraitsBonusFixes

--- Ship_Game/Gameplay/Weapon.cs
+++ Ship_Game/Gameplay/Weapon.cs
@@ -463,7 +463,7 @@ namespace Ship_Game.Gameplay
 
             //reduce or increase jitter based on weapon and trait characteristics. 
             
-            if (isBeam || Tag_Energy) adjust *= (1f - (Owner?.loyalty?.data.Traits.EnergyDamageMod ?? 0));    
+            if (Tag_Cannon) adjust *= (1f - (Owner?.loyalty?.data.Traits.EnergyDamageMod ?? 0));    
             if (Tag_Kinetic) adjust          *= (1f - (Owner?.loyalty?.data.OrdnanceEffectivenessBonus ?? 0));
 
             adjust *= CalculateBaseAccuracy();
