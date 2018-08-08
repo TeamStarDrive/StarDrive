ee4585cd9782cede5ac7b2f1bea16de2acab180a
Fizban1978 <fizban34@yahoo.com>
2018-08-08 20:57:33 +0300

    Damage  threshold is now based on internal damage. works much better for ships. fixed wrong tool tip for symmetric buttons. created tool tip for Carrier ship.
    --HG--
    branch : Fat_Bastard/RacialTraitsBonusFixes

--- Ship_Game/GameScreens/ShipDesignScreen/ShipDesignScreen.CategoryDropDown.cs
+++ Ship_Game/GameScreens/ShipDesignScreen/ShipDesignScreen.CategoryDropDown.cs
@@ -18,12 +18,12 @@ namespace Ship_Game
                     switch (ActiveValue)
                     {
                         default:
-                        case ShipData.Category.Unclassified: ToolTip.CreateTooltip("Repair when damaged at 65%"); break;
-                        case ShipData.Category.Civilian: ToolTip.CreateTooltip("Can be used as Freighter.\nEvade when enemy.\nRepair when damaged at 15%"); break;
-                        case ShipData.Category.Recon:    ToolTip.CreateTooltip("Repair when damaged at 35%"); break;
-                        case ShipData.Category.Combat:   
-                        case ShipData.Category.Bomber:
-                        case ShipData.Category.Fighter:  ToolTip.CreateTooltip("Repair when damaged at 55%"); break;
+                        case ShipData.Category.Unclassified: ToolTip.CreateTooltip("Repair when internally damaged at 65%"); break;
+                        case ShipData.Category.Civilian: ToolTip.CreateTooltip("Can be used as Freighter.\nEvade when enemy.\nRepair when internally damaged at 15%"); break;
+                        case ShipData.Category.Recon:    ToolTip.CreateTooltip("Repair when internally damaged at 35%"); break;
+                        case ShipData.Category.Combat: ToolTip.CreateTooltip("Repair when internally damaged at 75%"); break;
+                        case ShipData.Category.Bomber: ToolTip.CreateTooltip("Repair when internally damaged at 60%"); break;
+                        case ShipData.Category.Fighter:  ToolTip.CreateTooltip("Repair when internally damaged at 80%"); break;
                         case ShipData.Category.Kamikaze: ToolTip.CreateTooltip("Never Repair! Never Rearm!\n(unless ordered)"); break;
                     }
                 }
