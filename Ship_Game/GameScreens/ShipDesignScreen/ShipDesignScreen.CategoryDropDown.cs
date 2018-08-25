a50e742d97be05b54a1500f36c1c42696983e3cf
Fizban1978 <fizban34@yahoo.com>
2018-08-25 16:16:41 +0300

    more calcs
    --HG--
    branch : Fat_Bastard/SmallVariousFixes

--- Ship_Game/GameScreens/ShipDesignScreen/ShipDesignScreen.CategoryDropDown.cs
+++ Ship_Game/GameScreens/ShipDesignScreen/ShipDesignScreen.CategoryDropDown.cs
@@ -22,7 +22,7 @@ namespace Ship_Game
                         case ShipData.Category.Civilian: ToolTip.CreateTooltip("Can be used as Freighter. Evade when enemies are near. " +
                                                                                "Repair when structural integrity is 85% or less"); break;
                         case ShipData.Category.Recon:    ToolTip.CreateTooltip("Repair when structural integrity is 65% or less"); break;
-                        case ShipData.Category.Combat: ToolTip.CreateTooltip("Repair when structural integrity is 25% or less"); break;
+                        case ShipData.Category.Combat: ToolTip.CreateTooltip("Repair when structural integrity is 35% or less"); break;
                         case ShipData.Category.Bomber: ToolTip.CreateTooltip("Repair when structural integrity is 35% or less. " +
                                                                              "Designate as Bomber/Anti Capital. Dynamic Bomber Hangars " +
                                                                              "will be able to pick the best from this category"); break;
