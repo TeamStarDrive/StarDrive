141eee1994a50f3e38b269e122d2a491f85f9aeb
crunchygremlin <crunchygremlin@gmail>
2018-08-28 01:56:46 -0700

    trying to scrap together a way to create a custom universe for sandbox.
    --HG--
    branch : SandBox/Experimental

--- Ship_Game/GameScreens/DeveloperSandbox.cs
+++ Ship_Game/GameScreens/DeveloperSandbox.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Ship_Game.Gameplay;
+using Ship_Game.Ships;
 
 namespace Ship_Game
 {
@@ -22,47 +24,85 @@ namespace Ship_Game
         public override void LoadContent()
         {
             Label(20, 20, "Developer Debug Sandbox (WIP, press ESC to quit)", Fonts.Arial20Bold);
+            EmpireManager.Clear();
             ResourceManager.LoadItAll();
             UniverseData universeSandBox = new UniverseData
             {
                 Size = new Vector2(1000000f)
             };
-            
-            foreach(var empireData in ResourceManager.Empires)
+            UniverseData.UniverseWidth = universeSandBox.Size.X * 2;
+            CurrentGame.StartNew(universeSandBox);
+            foreach (var empireData in ResourceManager.Empires)
             {
                 Empire empireFromEmpireData = EmpireManager.CreateEmpireFromEmpireData(empireData);
                 universeSandBox.EmpireList.Add(empireFromEmpireData);
                 EmpireManager.Add(empireFromEmpireData);
+                empireFromEmpireData.data.CurrentAutoScout = empireFromEmpireData.data.ScoutShip;
+                empireFromEmpireData.data.CurrentAutoColony = empireFromEmpireData.data.ColonyShip;
+                empireFromEmpireData.data.CurrentAutoFreighter = empireFromEmpireData.data.FreighterShip;
+                empireFromEmpireData.data.CurrentConstructor = empireFromEmpireData.data.ConstructorShip;
+
             }
 
-            UniverseScreen = new UniverseScreen(universeSandBox);
+            foreach (Empire empire in EmpireManager.Empires)
+            {
+                foreach (Empire e in EmpireManager.Empires)
+                {
+                    if (empire == e)
+                        continue;
+
+                    var r = new Relationship(e.data.Traits.Name);
+                    empire.AddRelationships(e, r);
+
+                }
+            }
 
 
-            //var playerEmpire = new Empire()
-            //{
-            //    EmpireColor = currentObjectColor,
-            //    data = SelectedData
-            //};
-            //playerEmpire.data.SpyModifier = RaceSummary.SpyMultiplier;
-            //playerEmpire.data.Traits.Spiritual = RaceSummary.Spiritual;
-            //RaceSummary.Adj1 = SelectedData.Traits.Adj1;
-            //RaceSummary.Adj2 = SelectedData.Traits.Adj2;
-            //playerEmpire.data.Traits = RaceSummary;
-            //playerEmpire.EmpireColor = currentObjectColor;
+            universeSandBox.EmpireList.First.isPlayer = true;
+            universeSandBox.playerShip = Ship.CreateShipAtPoint("Unarmed Scout", universeSandBox.EmpireList.First, new Vector2(0,0));
+            universeSandBox.MasterShipList.Add(universeSandBox.playerShip);
+            var solarSystem = new SolarSystem();
+            solarSystem.GenerateRandomSystem("sandbox", universeSandBox, 1, universeSandBox.EmpireList.First);
+            universeSandBox.SolarSystemsList.Add(solarSystem);
 
-            //empire.Initialize();
+            UniverseScreen = new UniverseScreen(universeSandBox)
+                { PlayerEmpire = universeSandBox.EmpireList.First};
+            UniverseScreen.player = UniverseScreen.PlayerEmpire;
+            SubmitSceneObjectsForRendering(universeSandBox);
+            UniverseScreen.ResetLighting();
+            ScreenManager.AddScreen(UniverseScreen);
 
-
-
-            //Universe = new UniverseScreen(Data)
-            //{
-            //    player = PlayerEmpire,
-            //    CamPos = new Vector3(-playerShip.Center.X, playerShip.Center.Y, 5000f),
-            //    ScreenManager = ScreenManager,
-            //    GameDifficulty = Difficulty,
-            //    GameScale = Scale
-            //};
         }
+
+        private void SubmitSceneObjectsForRendering(UniverseData data)
+        {
+            SolarSystem wipSystem = data.SolarSystemsList[0];
+
+            foreach (Planet planet in wipSystem.PlanetList)
+            {
+                planet.ParentSystem = wipSystem;
+                planet.Center += wipSystem.Position;
+                planet.InitializePlanetMesh(this);
+            }
+            foreach (Asteroid asteroid in wipSystem.AsteroidsList)
+            {
+                asteroid.Position3D.X += wipSystem.Position.X;
+                asteroid.Position3D.Y += wipSystem.Position.Y;
+                asteroid.Initialize();
+                AddObject(asteroid.So);
+            }
+            foreach (Moon moon in wipSystem.MoonList)
+            {
+                moon.Initialize();
+                AddObject(moon.So);
+            }
+            foreach (Ship ship in wipSystem.ShipList)
+            {
+                ship.Position = ship.loyalty.GetPlanets()[0].Center + new Vector2(6000f, 2000f);
+                ship.InitializeShip();
+            }
+        }
+
         public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
         {
             base.Update(gameTime, false, false);
