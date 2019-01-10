6f46c819e83e66418a9a437a6caae9c600201fc4
RedFox <jorma.rebane@gmail.com>
2019-01-10 21:01:53 +0200

    Removed a bunch of unnecessary Nuget packages
    --HG--
    branch : develop

--- SDUnitTests/TestSpatialManager.cs
+++ SDUnitTests/TestSpatialManager.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.Xna.Framework;
 using NUnit.Framework;
 using Ship_Game;
+using Ship_Game.Ships;
 
 namespace SDUnitTests
 {
     [TestFixture]
     public class TestSpatialManager
     {
-        
         public TestSpatialManager()
         {
 
