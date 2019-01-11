81f9995ba916ac7f641d52878bf2684b7ed5ce68
RedFox <jorma.rebane@gmail.com>
2019-01-11 04:07:59 +0200

    Moved atlas tests into unit tests
    --HG--
    branch : minor_bugfixes_and_improvements

---
+++ UnitTests/TestTextureAtlas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ship_Game;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestTextureAtlas : IDisposable
+    {
+        readonly GameDummy Game;
+
+        public TestTextureAtlas()
+        {
+            Directory.SetCurrentDirectory("/Projects/BlackBox/StarDrive");
+            Game = new GameDummy();
+            Game.Create();
+        }
+
+        public void Dispose()
+        {
+            Game?.Dispose();
+        }
+
+        [TestMethod]
+        public void TextureAtlasFromFolder()
+        {
+            TextureAtlas.FromFolder(Game.Content, "Textures", false);
+            TextureAtlas.FromFolder(Game.Content, "Textures/Modules", false);
+            TextureAtlas.FromFolder(Game.Content, "Textures/Conduits", false);
+        }
+
+        [TestMethod]
+        public void ContentManager()
+        {
+            var nonExistent = Game.Content.Load<SubTexture>("TestForAtlasDoesNotExist");
+            Assert.AreEqual(nonExistent, null, "nonExistent SubTexture must be null");
+
+            var reactor = Game.Content.Load<SubTexture>("Textures/Modules/AncientReactorMed");
+            Assert.AreNotEqual(reactor, null, "reactor != null");
+
+            reactor = Game.Content.Load<SubTexture>("Textures/Modules/AncientReactorMed");
+            Assert.AreNotEqual(reactor, null, "reactor != null");
+        }
+    }
+}
