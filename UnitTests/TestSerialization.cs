e6c5be0be54bfce2a7dab7434f4f7ecfd3a0f54b
RedFox <jorma.rebane@gmail.com>
2019-01-12 23:14:07 +0200

    Added a basic enum serialization test
    --HG--
    branch : develop

--- UnitTests/TestSerialization.cs
+++ UnitTests/TestSerialization.cs
@@ -4,12 +4,39 @@ using Microsoft.Xna.Framework;
 using Newtonsoft.Json;
 using Ship_Game;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ship_Game.Ships;
 
 namespace SDUnitTests
 {
     [TestClass]
     public class TestSerialization
     {
+        public class DataToSerialize
+        {
+            public ShipData.RoleName RoleName;
+        }
+
+        [TestMethod]
+        public void SerializeEnums()
+        {
+            var ser = new JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+            };
+
+            var data = new DataToSerialize { RoleName = ShipData.RoleName.carrier }; 
+
+            var writer = new StringWriter();
+            ser.Serialize(writer, data);
+            string serialized = writer.ToString();
+
+            var reader = new JsonTextReader(new StringReader(serialized));
+            var data2 = ser.Deserialize<DataToSerialize>(reader);
+
+            Assert.AreEqual(data2.RoleName, data.RoleName);
+        }
+
         [TestMethod]
         public void SerializeXnaRectangle()
         {
