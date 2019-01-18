2e891f1b1d01419c16798780d2f84ea70ed91d02
RedFox <jorma.rebane@gmail.com>
2019-01-18 02:29:58 +0200

    Fixed Ship building
    --HG--
    branch : develop

--- Ship_Game/ExtensionMethods/NumberExt.cs
+++ Ship_Game/ExtensionMethods/NumberExt.cs
@@ -9,11 +9,6 @@ namespace Ship_Game
             return number.ToString("0.#", CultureInfo.InvariantCulture);
         }
 
-        public static string SignString(this float number)
-        {
-            return number.ToString("0.0;-0.0;0.0", CultureInfo.InvariantCulture);
-        }
-
         public static string String(this float number, int numDecimals)
         {
             switch (numDecimals)
@@ -22,6 +17,22 @@ namespace Ship_Game
                 case 1:  return number.ToString("0.#",   CultureInfo.InvariantCulture);
                 case 2:  return number.ToString("0.##",  CultureInfo.InvariantCulture);
                 default: return number.ToString("0.###", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static string SignString(this float number)
+        {
+            return number.ToString("0.0;-0.0;0.0", CultureInfo.InvariantCulture);
+        }
+
+        public static string SignString(this float number, int numDecimals)
+        {
+            switch (numDecimals)
+            {
+                case 0:  return ((int)number).ToString();
+                case 1:  return number.ToString("+0.0;-0.0;0.0",   CultureInfo.InvariantCulture);
+                case 2:  return number.ToString("+0.00;-0.00;0.00",  CultureInfo.InvariantCulture);
+                default: return number.ToString("+0.000;-0.000;0.000", CultureInfo.InvariantCulture);
             }
         }
 
