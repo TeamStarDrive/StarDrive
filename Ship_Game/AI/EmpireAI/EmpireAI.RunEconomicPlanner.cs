df0b3e5cb94970bbee1df82d34898580ca76a86d
fizban1978 <fizban34@yahoo.com>
2019-01-22 19:56:01 +0200

    small fixes
    --HG--
    branch : Fat_Bastard/PlatformsAndStations

--- Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
@@ -85,7 +85,7 @@ namespace Ship_Game.AI
             float taxRateModifer = 0;
             if (treasuryGoalRatio > 1)
                 taxRateModifer = -(float)Math.Round((treasuryGoalRatio -1) / 10, 2); // this will decrease tax based on ratio
-            if (treasuryGoal < 1)
+            if (treasuryGoalRatio < 1)
                 taxRateModifer = (float)Math.Round((1 / treasuryGoalRatio - 1) / 10, 2); // this will decrease tax based on oppsite ratio
 
             OwnerEmpire.data.TaxRate  = (normalTaxRate + taxRateModifer).Clamped(0.05f,0.95f);
