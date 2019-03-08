f5539cf6810cd9301b9894f8ba33b49e5e5497be
fizban1978 <fizban34@yahoo.com>
2019-03-08 11:24:50 +0200

    Fixed manual trade preferences not being saved. Fixed starting ships being recteared on game load.
    --HG--
    branch : Fat_Bastard/SmallManualTradeRefactor

--- Ship_Game/Ships/Ship_Initialize.cs
+++ Ship_Game/Ships/Ship_Initialize.cs
@@ -191,6 +191,10 @@ namespace Ship_Game.Ships
             TroopsLaunched   = save.TroopsLaunched;
             FightersLaunched = save.FightersLaunched;
 
+            TransportingFood       = save.TransportingFood;
+            TransportingProduction = save.TransportingProduction;
+            TransportingPassengers = save.TransportingPassengers;
+
             VanityName = shipData.Role == ShipData.RoleName.troop && save.TroopList.NotEmpty
                             ? save.TroopList[0].Name : save.Name;
 
