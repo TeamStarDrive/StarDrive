94520e831c550db6634096c6a5ba556197c6daea
fizban1978 <fizban34@yahoo.com>
2019-03-08 10:27:04 +0200

    Seperated manual food and production  trade buttons. The players can now set their freighters to transport a specifict type of goods or several of them.
    --HG--
    branch : Fat_Bastard/SmallManualTradeRefactor

--- Ship_Game/Ships/ShipInfoUIElement.cs
+++ Ship_Game/Ships/ShipInfoUIElement.cs
@@ -661,13 +661,13 @@ namespace Ship_Game.Ships
             {
                 OrdersButton tf = new OrdersButton(Ship, Vector2.Zero, OrderType.TradeFood, 16)
                 {
-                    ValueToModify = new Ref<bool>(() => Ship.DoingFoodTransport, x => Ship.DoingTransport = x),
+                    ValueToModify = new Ref<bool>(() => Ship.DoingFoodTransport, x => Ship.DoingFoodTransport = x),
                     RightClickValueToModify = new Ref<bool>(() => Ship.TransportingFood, x => Ship.TransportingFood = x)
                 };
                 Orders.Add(tf);
                 OrdersButton tp = new OrdersButton(Ship, Vector2.Zero, OrderType.TradeProduction, 17)
                 {
-                    ValueToModify = new Ref<bool>(() => Ship.DoingProdTransport, x => Ship.DoingTransport = x),
+                    ValueToModify = new Ref<bool>(() => Ship.DoingProdTransport, x => Ship.DoingProdTransport = x),
                     RightClickValueToModify = new Ref<bool>(() => Ship.TransportingProduction, x => Ship.TransportingProduction = x)
                 };
                 Orders.Add(tp);
