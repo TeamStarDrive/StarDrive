a8264c2fc6c853beee1fcbe2d6e0d67f6ca13e21
fizban1978 <fizban34@yahoo.com>
2019-03-14 18:13:15 +0200

    added oppurtunist freighter extention to trade. If the closest freighter to the import planet has the cargo it needs, the ship will be selected for the trade.
    --HG--
    branch : Fat_Bastard/Venus_Bug_Fixes

--- Ship_Game/AI/ShipAI/ShipAI.cs
+++ Ship_Game/AI/ShipAI/ShipAI.cs
@@ -608,6 +608,12 @@ namespace Ship_Game.AI
             SetTradePlan(plan, exportPlanet, importPlanet, goods);
         }
 
+        public void SetupFreighterPlan(Planet importPlanet, Goods goods)
+        {
+            Plan plan = Plan.DropOffGoods; // found close freighter with the goods we need so we don't need an export planet
+            SetTradePlan(plan, importPlanet, importPlanet, goods); // this planet takes care of itself this trade
+        }
+
         public bool ClearOrderIfCombat() => ClearOrdersConditional(Plan.DoCombat);
         public bool ClearOrdersConditional(Plan plan)
         {
