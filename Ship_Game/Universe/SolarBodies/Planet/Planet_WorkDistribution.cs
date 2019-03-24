5c82147d472c4e87bfb1fc8a4dd78c5d993cf712
crunchygremlin <crunchygremlin@gmail>
2019-03-24 04:58:52 -0700

    initial change to AI research production. Change the default amount of workers used on the production queue to be based on what is being built and what personality is building.
    --HG--
    branch : AI/ResearchProduction

--- Ship_Game/Universe/SolarBodies/Planet/Planet_WorkDistribution.cs
+++ Ship_Game/Universe/SolarBodies/Planet/Planet_WorkDistribution.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Ship_Game.Ships;
+using Ship_Game.Universe.SolarBodies;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ship_Game.Universe.SolarBodies;
 
 namespace Ship_Game
 {
@@ -39,7 +40,7 @@ namespace Ship_Game
             if (percent <= 0f || Food.YieldPerColonist <= 0.5f || IsCybernetic)
                 return 0; // No farming here, so never mind
 
-            float farmers = Food.EstPercentForNetIncome(+1f);          
+            float farmers = Food.EstPercentForNetIncome(+1f);
 
             // modify nominal farmers by overage or underage
             farmers += CalculateMod(percent, Storage.FoodRatio).Clamped(-0.35f, 0.50f);
@@ -50,7 +51,7 @@ namespace Ship_Game
         {
             if (percent <= 0f) return 0;
 
-            float workers = Prod.EstPercentForNetIncome(+1f);
+            float workers = Prod.EstPercentForNetIncome(+.5f);
 
             workers += CalculateMod(percent, Storage.ProdRatio).Clamped(-0.35f, 1.00f);
             return workers.Clamped(0f, 1f);
@@ -64,7 +65,7 @@ namespace Ship_Game
                 AssignCoreWorldProduction(1f);
             else // Strategy for Flesh-bags:
             {
-                AssignCoreWorldFarmers(1f); 
+                AssignCoreWorldFarmers(1f);
                 AssignCoreWorldProduction(1f - Food.Percent); // then we optimize production
             }
 
@@ -77,7 +78,7 @@ namespace Ship_Game
             Food.Percent = FarmToPercentage(percentFood);
             Prod.Percent = Math.Min(1 - Food.Percent, WorkToPercentage(percentProd));
             if (ConstructionQueue.Count > 0)
-                Prod.Percent = Math.Max(Prod.Percent, (1 - Food.Percent) * 0.5f);
+                Prod.Percent = Math.Max(Prod.Percent, (1 - Food.Percent) * EvaluateProductionQueue());
 
             Res.AutoBalanceWorkers(); // rest goes to research
         }
@@ -100,7 +101,7 @@ namespace Ship_Game
         // Core World aims for +1 NetIncome
         void AssignCoreWorldFarmers(float labor)
         {
-            float minPerTurn = MinIncomePerTurn(Storage.Food, Food);
+            float minPerTurn = MinIncomePerTurn(Storage.Food, Food) * (1-Owner.ResearchStrategy.ResearchRatio);
             float farmers = Food.EstPercentForNetIncome(minPerTurn);
 
             if (farmers > 0 && farmers < 0.1f)
@@ -116,11 +117,9 @@ namespace Ship_Game
             float minPerTurn = MinIncomePerTurn(Storage.Prod, Prod);
             float workers = Prod.EstPercentForNetIncome(minPerTurn);
 
-            if (workers > 0 && workers < 0.1f)
-                workers = 0.1f; // avoid crazy small percentage of labor
-
-            if (ConstructionQueue.Count > 1 && workers < 0.75f)
-                workers = 0.75f; // minimum value if construction is going on
+            workers += EvaluateProductionQueue();
+            workers = workers.Clamped(.1f, 1f);
+            //    workers = 0.75f; // minimum value if construction is going on
 
             Prod.Percent = workers * labor;
         }
@@ -134,6 +133,77 @@ namespace Ship_Game
         float LeftoverWorkerBillions()
         {
             return LeftoverWorkers() * MaxPopulationBillion;
+        }
+
+        float EvaluateProductionQueue()
+        {
+            float priority = 0;
+            var item = ConstructionQueue.FirstOrDefault();
+            if (item != null)
+            {
+                priority = 0;
+                if (item.isBuilding)
+                {
+                    //set base pri to industry ratio;
+                    switch (item.Building.Category)
+                    {
+                        case BuildingCategory.General:
+                        case BuildingCategory.Storage:
+                        case BuildingCategory.Shipyard:
+                            priority = Owner.ResearchStrategy.IndustryRatio;
+                            break;
+                        case BuildingCategory.Production:
+                        case BuildingCategory.Finance:
+                        case BuildingCategory.Food:
+                            priority += Owner.ResearchStrategy.IndustryRatio;
+                            priority += Owner.ResearchStrategy.ExpansionRatio;
+                            break;
+                        case BuildingCategory.Defense:
+                        case BuildingCategory.Military:
+                        case BuildingCategory.Sensor:
+                            priority = Owner.ResearchStrategy.MilitaryPriority;
+                            break;
+                        case BuildingCategory.Science:
+                            priority += Owner.ResearchStrategy.ResearchRatio;
+                            priority += Owner.ResearchStrategy.ExpansionRatio;
+                            break;
+                        case BuildingCategory.Population:
+                        case BuildingCategory.Terraforming:
+                        case BuildingCategory.Growth:
+                        case BuildingCategory.Biosphere:
+                            priority += Owner.ResearchStrategy.ExpansionRatio;
+                            break;
+                        case BuildingCategory.Victory:
+                            priority = .75f;
+                            break;
+                        default:
+                            priority += .2f;
+                            break;
+                    }
+                }
+                if (item.isShip)
+                {
+                    switch(item.sData.Role)
+                    {
+                        case ShipData.RoleName.colony:
+                            priority = .75f;
+                            break;
+                        case ShipData.RoleName.freighter:
+                            priority = Owner.ResearchStrategy.ExpansionRatio + Owner.ResearchStrategy.IndustryRatio;
+                            break;
+                        default:
+                            priority = Owner.ResearchStrategy.MilitaryRatio;
+                            break;
+
+                    }
+                }
+                if (item.isTroop)
+                    priority = Owner.ResearchStrategy.MilitaryRatio + Owner.ResearchStrategy.ExpansionRatio;
+                if (item.isOrbital)
+                    priority = .1f;
+            }
+
+            return priority;
         }
     }
 }
