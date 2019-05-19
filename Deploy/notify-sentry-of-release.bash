266545b88063af4ebb332513491de61e57377d7e
crunchygremlin <crunchygremlin@gmail>
2019-05-19 01:09:28 -0700

    one more time
    --HG--
    branch : develop

--- Deploy/notify-sentry-of-release.bash
+++ Deploy/notify-sentry-of-release.bash
@@ -11,7 +11,6 @@
 #                           If not provided, this script assumes Sentry matches Bitbucket repository.
 #                           Example: "ZeroDivisionError"
 # BITBUCKET_COMMIT       (Provided) Commit SHA that triggered the build.
-# {"dateReleased": null, "commitCount": 0, "url": null, "data": {}, "lastDeploy": null, "deployCount": 0, "dateCreated": #"2019-05-19T06:59:26.124Z", "lastEvent": null, "version": "${BITBUCKET_COMMIT}", "firstEvent": null, "lastCommit": null, "shortVersion": #"${BITBUCKET_COMMIT}", "authors": [], "owner": null, "newGroups": 0, "ref": null, "projects": [{"slug": "blackbox", "name": "BlackBox"}]}
 
 if [ -z "${SENTRY_ORGANIZATION}" ]; then
     SENTRY_ORGANIZATION=${BITBUCKET_REPO_OWNER}
@@ -26,11 +25,11 @@ curl https://app.getsentry.com/api/0/projects/${SENTRY_ORGANIZATION}/${SENTRY_PR
 -H "Content-Type:application/json" \
 -d '
   {
-    "version": \"${BITBUCKET_COMMIT}\",
+    "version": "\"${BITBUCKET_COMMIT}\"",
     "refs": [{
-        "repository":"sd-blackbox",
-        "commit":\"${BITBUCKET_COMMIT}\"
+        repository":"sd-blackbox",
+        "commit": "\"${BITBUCKET_COMMIT}\""
     }],
-    "projects": [\"${SENTRY_PROJECT}\"]
+    "projects\": ["\"${SENTRY_PROJECT}\""]
 	}
 '
