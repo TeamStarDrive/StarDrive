a4f380fa6547c02f1c2955fac3bc694ffe6f3eed
RedFox <jorma.rebane@gmail.com>
2019-06-03 05:24:36 +0300

    First steps in getting animation clip exports to work.
    --HG--
    branch : implement_mesh_exporter

--- SDNative/SdMesh/SdMesh.cpp
+++ SDNative/SdMesh/SdMesh.cpp
@@ -6,11 +6,11 @@ namespace SdMesh
     
     SDMesh::SDMesh() = default;
 
-    SDMesh::SDMesh(strview path) : Data{ path }
+    SDMesh::SDMesh(strview path) : TheMesh{ path }
     {
-        Name = Data.Name;
-        Groups.resize(Data.NumGroups());
-        for (int i = 0; i < Data.NumGroups(); ++i)
+        Name = TheMesh.Name;
+        Groups.resize(TheMesh.NumGroups());
+        for (int i = 0; i < TheMesh.NumGroups(); ++i)
         {
             Groups[i] = std::make_unique<SDMeshGroup>(*this, i);
         }
@@ -27,14 +27,15 @@ namespace SdMesh
                 return mapping.get();
 
         Materials.push_back(std::make_unique<SDMaterial>(mat)); // add new
+        NumMaterials = (int)Materials.size();
         return Materials.back().get();
     }
 
     void SDMesh::SyncStats()
     {
-        Name      = Data.Name;
-        NumGroups = Data.NumGroups();
-        NumFaces  = Data.TotalTris();
+        Name      = TheMesh.Name;
+        NumGroups = TheMesh.NumGroups();
+        NumFaces  = TheMesh.TotalTris();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
@@ -42,7 +43,7 @@ namespace SdMesh
     DLLAPI(SDMesh*) SDMeshOpen(const wchar_t* fileName)
     {
         auto sdm = new SDMesh{ toString(fileName) };
-        if (!sdm->Data) {
+        if (!sdm->TheMesh) {
             SDMeshClose(sdm);
             return nullptr;
         }
@@ -57,19 +58,19 @@ namespace SdMesh
     DLLAPI(SDMesh*) SDMeshCreateEmpty(const wchar_t* meshName)
     {
         auto* mesh = new SDMesh{};
-        mesh->Data.Name = toString(meshName);
-        mesh->Name = mesh->Data.Name;
+        mesh->TheMesh.Name = toString(meshName);
+        mesh->Name = mesh->TheMesh.Name;
         return mesh;
     }
 
     DLLAPI(bool) SDMeshSave(SDMesh* mesh, const wchar_t* fileName)
     {
-        return mesh->Data.SaveAs(toString(fileName));
+        return mesh->TheMesh.SaveAs(toString(fileName));
     }
 
     DLLAPI(SDMeshGroup*) SDMeshGetGroup(SDMesh* mesh, int groupId)
     {
-        if (mesh && mesh->Data.IsValidGroup(groupId))
+        if (mesh && mesh->TheMesh.IsValidGroup(groupId))
         {
             return mesh->Groups[groupId].get();
         }
@@ -78,7 +79,7 @@ namespace SdMesh
 
     DLLAPI(SDMeshGroup*) SDMeshNewGroup(SDMesh* mesh, const wchar_t* groupName, Matrix4* transform)
     {
-        Nano::MeshGroup& group = mesh->Data.CreateGroup(toString(groupName));
+        Nano::MeshGroup& group = mesh->TheMesh.CreateGroup(toString(groupName));
         auto* g = mesh->Groups.emplace_back(std::make_unique<SDMeshGroup>(*mesh, group.GroupId)).get();
         mesh->NumGroups++;
         if (transform) g->Transform = *transform;
