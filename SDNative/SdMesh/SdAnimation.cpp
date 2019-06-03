2c792b32224634146ec96b17a25e176664dae7c9
RedFox <jorma.rebane@gmail.com>
2019-06-03 05:24:36 +0300

    First steps in getting animation clip exports to work.
    --HG--
    branch : implement_mesh_exporter

--- SDNative/SdMesh/SdAnimation.cpp
+++ SDNative/SdMesh/SdAnimation.cpp
@@ -1,4 +1,6 @@
 #include "SdAnimation.h"
+#include "SdMesh.h"
+#include <cassert>
 
 namespace SdMesh
 {
@@ -7,18 +9,68 @@ namespace SdMesh
     DLLAPI(void) SDMeshAddBone(SDMesh* mesh, const wchar_t* name, int boneIndex, int parentBone, 
                                const Matrix4& transform)
     {
+        assert(mesh != nullptr && "SDMeshAddBone mesh cannot be null");
 
+        mesh->TheMesh.Bones.emplace_back(Nano::MeshBone {
+            boneIndex, parentBone, toString(name),
+            Nano::BonePose {
+                transform.getTranslation(),
+                transform.getScale(),
+                transform.getRotationAngles()
+            }
+        });
     }
 
     DLLAPI(void) SDMeshAddSkinnedBone(SDMesh* mesh, const wchar_t* name, int boneIndex, int parentBone,
-                                      const SDBonePose& bindPose, const Matrix4& inverseBindPoseTransform)
+                                      const Nano::BonePose& bindPose, const Matrix4& inverseBindPoseTransform)
     {
+        assert(mesh != nullptr && "SDMeshAddSkinnedBone mesh cannot be null");
 
+        mesh->TheMesh.SkinnedBones.emplace_back(Nano::SkinnedBone {
+            boneIndex, parentBone, toString(name), 
+            bindPose, inverseBindPoseTransform
+        });
+        mesh->NumSkinnedBones = (int)mesh->TheMesh.SkinnedBones.size();
     }
 
     DLLAPI(SDAnimationClip*) SDMeshCreateAnimationClip(SDMesh* mesh, const wchar_t* name, float duration)
     {
-        return nullptr;
+        assert(mesh != nullptr && "SDMeshCreateAnimationClip mesh cannot be null");
+
+        string cName = toString(name);
+        Nano::AnimationClip& theClip = mesh->TheMesh.AnimationClips[cName];
+        theClip.Name = cName;
+        theClip.Duration = duration;
+
+        SDAnimationClip& clip = *mesh->Clips.emplace_back(new SDAnimationClip{ theClip });
+        clip.Name = theClip.Name;
+        clip.Duration = duration;
+
+        mesh->NumAnimClips = (int)mesh->Clips.size();
+
+        return &clip;
+    }
+
+    DLLAPI(SDBoneAnimation*) SDMeshAddBoneAnimation(SDAnimationClip* clip, int skinnedBoneIndex)
+    {
+        assert(clip != nullptr && "SDMeshAddBoneAnimation clip cannot be null");
+
+        Nano::BoneAnimation& theAnim = clip->TheClip.Animations.emplace_back();
+        theAnim.SkinnedBoneIndex = skinnedBoneIndex;
+
+        SDBoneAnimation& anim = *clip->TheAnims.emplace_back(new SDBoneAnimation{ theAnim });
+        anim.BoneIndex = theAnim.SkinnedBoneIndex;
+
+        return &anim;
+    }
+
+    DLLAPI(void) SDMeshAddAnimationKeyFrame(SDBoneAnimation* anim, const Nano::AnimationKeyFrame& keyFrame)
+    {
+        assert(anim != nullptr && "SDMeshAddAnimationKeyFrame anim cannot be null");
+
+        anim->TheAnim.Frames.push_back(keyFrame);
+        anim->NumKeyFrames = (int)anim->TheAnim.Frames.size();
+        anim->KeyFrames = anim->TheAnim.Frames.data();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
--- SDNative/SdMesh/SdMesh.h
+++ SDNative/SdMesh/SdMesh.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Nano/Mesh.h>
 #include "SdMeshGroup.h"
+#include "SdAnimation.h"
 
 namespace SdMesh
 {
@@ -20,9 +21,15 @@ namespace SdMesh
         strview Name  = "";
         int NumGroups = 0;
         int NumFaces  = 0;
+        int NumMaterials = 0;
+
+        int NumModelBones = 0;
+        int NumSkinnedBones = 0;
+        int NumAnimClips = 0;
 
         // not mapped to C#
-        Nano::Mesh Data;
+        Nano::Mesh TheMesh;
+        vector<unique_ptr<SDAnimationClip>> Clips;
         vector<unique_ptr<SDMeshGroup>> Groups;
         vector<unique_ptr<SDMaterial>> Materials;
 
