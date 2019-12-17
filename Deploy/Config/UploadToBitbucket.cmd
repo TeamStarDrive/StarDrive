c5ea600261a09f6bc1e0f0490da15a38a5acd589
Jorma <jorma.rebane@gmail.com>
2019-12-17 05:54:30 +0200

    Check if file has already been uploaded to BitBucket

--- Deploy/Config/UploadToBitbucket.cmd
+++ Deploy/Config/UploadToBitbucket.cmd
@@ -5,17 +5,20 @@ if not defined BB_UPLOAD_PASS ( echo envvar BB_UPLOAD_PASS undefined && goto err
 
 echo APPVEYOR_REPO_BRANCH=%APPVEYOR_REPO_BRANCH%
 echo APPVEYOR_REPO_COMMIT=%APPVEYOR_REPO_COMMIT%
-echo APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH=%APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH%
 git name-rev --name-only %APPVEYOR_REPO_COMMIT%
-git name-rev --name-only HEAD
-
 ::for /f %%b in ('git name-rev --name-only HEAD') do set BRANCH_NAME=%%b
 echo BRANCH_NAME=%APPVEYOR_REPO_BRANCH%
 if "%BRANCH_NAME%" NEQ "develop" ( echo Auto-Deploy is only enabled for develop branch && goto :eof )
 
-for /f %%r in ('dir /B /O-D C:\Projects\BlackBox\Deploy\upload') do set file=%%r
-set file=C:/Projects/BlackBox/Deploy/upload/%file%
+for /f %%r in ('dir /B /O-D C:\Projects\BlackBox\Deploy\upload') do set file_name=%%r
+set file=C:/Projects/BlackBox/Deploy/upload/%file_name%
 echo Bitbucket Upload: %file%
+
+curl --silent --head --fail "https://bitbucket.org/CrunchyGremlin/stardrive-blackbox/downloads/%file_name%"
+if %ERRORLEVEL% EQ 0 (
+  echo "File has already been uploaded to BitBucket?"
+  goto :eof
+)
 
 curl -X POST -u "%BB_UPLOAD_USER%:%BB_UPLOAD_PASS%" -F files=@"%file%" "https://api.bitbucket.org/2.0/repositories/CrunchyGremlin/stardrive-blackbox/downloads"
 goto :eof
