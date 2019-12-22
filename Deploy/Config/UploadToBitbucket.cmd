f17f082510a59ad747806e94c04c771fe40fd2a1
Jorma <jorma.rebane@gmail.com>
2019-12-22 15:07:54 +0200

    Fix upload script

--- Deploy/Config/UploadToBitbucket.cmd
+++ Deploy/Config/UploadToBitbucket.cmd
@@ -5,10 +5,9 @@ if not defined BB_UPLOAD_PASS ( echo envvar BB_UPLOAD_PASS undefined && goto err
 
 echo APPVEYOR_REPO_BRANCH=%APPVEYOR_REPO_BRANCH%
 echo APPVEYOR_REPO_COMMIT=%APPVEYOR_REPO_COMMIT%
-git name-rev --name-only %APPVEYOR_REPO_COMMIT%
+::git name-rev --name-only %APPVEYOR_REPO_COMMIT%
 ::for /f %%b in ('git name-rev --name-only HEAD') do set BRANCH_NAME=%%b
-echo BRANCH_NAME=%APPVEYOR_REPO_BRANCH%
-if "%BRANCH_NAME%" NEQ "develop" ( echo Auto-Deploy is only enabled for develop branch && goto :eof )
+if "%APPVEYOR_REPO_BRANCH%" NEQ "develop" ( echo Auto-Deploy is only enabled for develop branch && goto :eof )
 
 for /f %%r in ('dir /B /O-D C:\Projects\BlackBox\Deploy\upload') do set file_name=%%r
 set file=C:/Projects/BlackBox/Deploy/upload/%file_name%
