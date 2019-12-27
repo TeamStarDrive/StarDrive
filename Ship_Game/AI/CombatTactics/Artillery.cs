8499638dd3ad73652a3ccc8c55ef7a5687163d9f
Jorma <jorma.rebane@gmail.com>
2019-12-27 02:32:12 +0200

    Reducing unused code. Fixed FormationWarp speed limit.

--- Ship_Game/AI/CombatTactics/Artillery.cs
+++ Ship_Game/AI/CombatTactics/Artillery.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
 using Ship_Game.Ships;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Ship_Game.AI.CombatTactics
 {
