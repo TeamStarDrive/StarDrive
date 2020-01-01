d2eddfd6d61f8bd77088fea3716a64e7720a56b1
Fizban1978 <gkapulis@gmail.com>
2020-01-01 18:57:19 +0200

    Reverted change to Colony budget and changed the Defense budget.

--- Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
@@ -38,7 +38,7 @@ namespace Ship_Game.AI
         {
             EconomicResearchStrategy strat = OwnerEmpire.Research.Strategy;
             float adjustor = risk + strat.MilitaryRatio;
-            return SetBudgetForeArea(0.0025f, adjustor, money);
+            return SetBudgetForeArea(0.005f, adjustor, money);
         }
         float DetermineSSPBudget(float money)
         {
@@ -59,8 +59,9 @@ namespace Ship_Game.AI
         float DetermineColonyBudget(float money)
         {
             EconomicResearchStrategy strat = OwnerEmpire.Research.Strategy;
-            return SetBudgetForeArea(0.01f, strat.IndustryRatio + strat.ExpansionRatio, money);
+            return SetBudgetForeArea(0.0075f, strat.IndustryRatio + strat.ExpansionRatio, money);
         }
+
         float DetermineSpyBudget(float risk, float money)
         {
             EconomicResearchStrategy strat = OwnerEmpire.Research.Strategy;
