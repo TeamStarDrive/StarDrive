702a9f51c7b9b2253af493cca639a9baea4b7085
Jorma <jorma.rebane@gmail.com>
2020-01-01 19:26:31 +0200

    Split up ToolTip and ToolTipText

--- Ship_Game/ToolTip.cs
+++ Ship_Game/ToolTip.cs
@@ -5,44 +5,6 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Ship_Game
 {
-    public struct ToolTipText
-    {
-        public int Id; // Tooltip ID
-        public string Text; // custom text
-        public static readonly ToolTipText None = new ToolTipText();
-        public bool IsValid => Id > 0 || !string.IsNullOrEmpty(Text);
-
-        public static implicit operator ToolTipText(int id)
-        {
-            return new ToolTipText{ Id = id };
-        }
-        public static implicit operator ToolTipText(string text)
-        {
-            return new ToolTipText{ Text = text };
-        }
-
-        // intentional lazy lookup for tooltips
-        public string LocalizedText
-        {
-            get
-            {
-                if (Id > 0)
-                {
-                    ToolTip tooltip = ResourceManager.GetToolTip(Id);
-                    if (tooltip != null)
-                    {
-                        return Localizer.Token(tooltip.Data);
-                    }
-                    if (Text.IsEmpty()) // try to recover.. somehow
-                    {
-                        return Localizer.Token(Id);
-                    }
-                }
-                return Text;
-            }
-        }
-    }
-
     public sealed class ToolTip
     {
         public int TIP_ID; // Serialized from: Tooltips.xml
