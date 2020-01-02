97fa17bf8fd931a4918b327a8c10dad28d30d303
Jorma <jorma.rebane@gmail.com>
2020-01-02 21:21:32 +0200

    Fix annoying merge issues

--- Ship_Game/UI/UIElementContainer.cs
+++ Ship_Game/UI/UIElementContainer.cs
@@ -293,10 +293,6 @@ namespace Ship_Game
         public UIButton ButtonMedium(float x, float y, LocalizedText text, Action<UIButton> click)
             => Button(ButtonStyle.Medium, new Vector2(x, y), text, click);
 
-        public UIButton ButtonMedium(float x, float y, LocalizedText title, Action<UIButton> click)
-            => Button(ButtonStyle.Medium, new Vector2(x, y), title, click);
-
-
         public UIButton Button(ButtonStyle style, LocalizedText text, Action<UIButton> click, string clickSfx = null)
             => Button(new UIButton(style, text), click, clickSfx);
 
