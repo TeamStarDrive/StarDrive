11678670cb24855e2a82d57bce25d88c158a29e3
Jorma <jorma.rebane@gmail.com>
2020-01-06 10:37:17 +0200

    Include short commit hash in Informational Version. Will help us track the actual commits more easily.

--- Deploy/Config/Deploy.cmd
+++ Deploy/Config/Deploy.cmd
@@ -6,12 +6,12 @@ set year=%date:~-2%
 set month=%date:~4,2%
 set day=%date:~7,2%
 
-REM Get the branch name and replace release/ with empty and / with _
+:: Get the branch name and replace release/ with empty and / with _
 for /f "delims=" %%b in ('git name-rev --name-only HEAD') do set name=%%b
 set name=%name:release/=%
 set name=%name:/=_%
 
-REM Get current revision `7912`
+:: Get current revision `7912`
 for /f %%r in ('git rev-list --count HEAD') do set revision=%%r
 echo Name=%name%
 echo Revision=%revision%
@@ -25,11 +25,11 @@ echo Packaging %installer_filename% to sd.7z
 echo ..\7-Zip\7z A sd.7z @"%1deploy\Config\include_%name%"
 echo copy /b "..\7-Zip\7ZSD.sfx" + "%1deploy\Config\config_%name%" + sd.7z "%installer_filename%.exe"
 
-REM Always remove sd.7z otherwise we'll get nasty bugs / redundant files
+:: Always remove sd.7z otherwise we'll get nasty bugs / redundant files
 echo Creating SFX %installer_filename%.exe
 del /f sd.7z
 if not exist ..\upload mkdir ..\upload
 ..\7-Zip\7z a sd.7z @"%1deploy\Config\include_%name%" > NUL
 copy /b "..\7-Zip\7ZSD.sfx" + "%1deploy\Config\config_%name%" + sd.7z "../upload/%installer_filename%.exe"
 echo Created %installer_filename%.exe
-REM ..\7-Zip\7z a -sfx7zSD.sfx "%installer_filename%.exe" @"%1deploy\Config\include_%name%"
+:: ..\7-Zip\7z a -sfx7zSD.sfx "%installer_filename%.exe" @"%1deploy\Config\include_%name%"
