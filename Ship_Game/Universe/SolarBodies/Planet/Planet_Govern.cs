fd8729bfeb1d6838d0489b5669a18e2887fc8ecb
Fizban1978 <gkapulis@gmail.com>
2020-01-13 21:23:45 +0200

    Incoming colonists slots bug fix. LimitedProductionExpenditure tweaks Fixed Budget debt Tolerance bug

--- Ship_Game/Universe/SolarBodies/Planet/Planet_Govern.cs
+++ Ship_Game/Universe/SolarBodies/Planet/Planet_Govern.cs
@@ -133,12 +133,12 @@ namespace Ship_Game
                 {
                     default: // Importing
                         if (IncomingProdFreighters > 0)
-                            prodToSpend = ProdHere * 0.5f; // We have incoming prod, so we can spend more now
+                            prodToSpend = ProdHere + Prod.NetIncome; // We have incoming prod, so we can spend more now
                         else
-                            prodToSpend = Prod.NetIncome * Storage.ProdRatio; // Spend less since nothing is coming
+                            prodToSpend = (ProdHere + Prod.NetIncome) * 0.5f; // Spend less since nothing is coming
                         break;
                     case GoodState.STORE:
-                            prodToSpend = ProdHere * 0.5f; // Spend some of our store since we are storing for building stuff
+                        prodToSpend = ProdHere * 0.5f; // Spend some of our store since we are storing for building stuff
                         break;
                     case GoodState.EXPORT:
                         if (OutgoingProdFreighters > 0 && ConstructionQueue.Count < 6)
