eebcbc3b275b6482d339228f304bf3522da4af83
crunchyGremlin <crunchygremlin@gmail.com>
2020-01-19 00:32:07 -0800

    add some directories to hold fleet stuff. make Group leader fleet center. smal adjustment to target prorities.

--- Ship_Game/AI/Fleet.cs
+++ Ship_Game/AI/Fleet.cs
@@ -5,6 +5,7 @@ using Ship_Game.Debug;
 using Ship_Game.Ships;
 using System;
 using System.Linq;
+using Ship_Game.Fleets.FleetTactics;
 
 namespace Ship_Game.AI
 {
@@ -1596,75 +1597,22 @@ namespace Ship_Game.AI
 
         public void OffensiveTactic()
         {
-            SetTacticDefense(ScreenShips);
-            SetTacticIntercept(LeftShips);
-            SetTacticIntercept(RightShips);
-            SetTacticIntercept(RearShips);
-            SetTacticAttack(CenterShips, CommandShip?.SensorRange ?? 150000);
+            var fleetTactics = new CombatPreferences();
+            fleetTactics.SetTacticDefense(ScreenShips);
+            fleetTactics.SetTacticIntercept(LeftShips);
+            fleetTactics.SetTacticIntercept(RightShips);
+            fleetTactics.SetTacticIntercept(RearShips);
+            fleetTactics.SetTacticAttack(CenterShips, CommandShip?.SensorRange ?? 150000);
         }
 
         public void DefensiveTactic()
         {
-            SetTacticDefense(ScreenShips);
-            SetTacticIntercept(LeftShips);
-            SetTacticIntercept(RightShips);
-            SetTacticDefense(RearShips);
-            SetTacticDefense(CenterShips, CommandShip?.SensorRange ?? 150000);
-        }
-
-        void SetTacticDefense(Array<Ship> flankShips, float aoRadius = 7500f)
-        {
-            foreach (var ship in flankShips)
-            {
-                var node = ship.AI.FleetNode;
-                ChangeNodeWeight(ref node.VultureWeight, 0f);
-                ChangeNodeWeight(ref node.AttackShieldedWeight, 0f);
-                ChangeNodeWeight(ref node.AssistWeight, 0.25f);
-                ChangeNodeWeight(ref node.DefenderWeight, 1f);
-                ChangeNodeWeight(ref node.SizeWeight, 0.5f);
-                ChangeNodeWeight(ref node.ArmoredWeight, 0f);
-                ChangeNodeWeight(ref node.DPSWeight, 0.25f);
-                ChangeNodeWeight(ref node.OrdersRadius, aoRadius);
-                ;
-            }
-        }
-
-        void SetTacticAttack(Array<Ship> flankShips, float aoRadius = 7500f)
-        {
-            foreach (var ship in flankShips)
-            {
-                var node = ship.AI.FleetNode;
-                ChangeNodeWeight(ref node.VultureWeight, 0.0f);
-                ChangeNodeWeight(ref node.AttackShieldedWeight, 0.0f);
-                ChangeNodeWeight(ref node.DPSWeight, 1f);
-                ChangeNodeWeight(ref node.ArmoredWeight, 0.0f);
-                ChangeNodeWeight(ref node.AssistWeight, 1f);
-                ChangeNodeWeight(ref node.DefenderWeight, 0f);
-                ChangeNodeWeight(ref node.SizeWeight, 1f);
-                ChangeNodeWeight(ref node.OrdersRadius, aoRadius);
-            }
-        }
-
-        void SetTacticIntercept(Array<Ship> flankShips, float aoRadius = 7500f)
-        {
-            foreach (var ship in flankShips)
-            {
-                var node = ship.AI.FleetNode;
-                ChangeNodeWeight(ref node.VultureWeight, 0.0f);
-                ChangeNodeWeight(ref node.AttackShieldedWeight, 0.0f);
-                ChangeNodeWeight(ref node.AssistWeight, 1f);
-                ChangeNodeWeight(ref node.DefenderWeight, 0.5f);
-                ChangeNodeWeight(ref node.SizeWeight, 0.0f);
-                ChangeNodeWeight(ref node.ArmoredWeight, 0.0f);
-                ChangeNodeWeight(ref node.DPSWeight, 1.0f);
-                ChangeNodeWeight(ref node.OrdersRadius, aoRadius);
-            }
-        }
-
-        void ChangeNodeWeight(ref float currentValue, float newValue)
-        {
-            if (!currentValue.AlmostEqual(0.5f));
-            currentValue = newValue;
+            var fleetTactics = new CombatPreferences();
+            fleetTactics.SetTacticDefense(ScreenShips);
+            fleetTactics.SetTacticIntercept(LeftShips);
+            fleetTactics.SetTacticIntercept(RightShips);
+            fleetTactics.SetTacticDefense(RearShips);
+            fleetTactics.SetTacticDefense(CenterShips, CommandShip?.SensorRange ?? 150000);
         }
 
         public enum FleetCombatStatus
