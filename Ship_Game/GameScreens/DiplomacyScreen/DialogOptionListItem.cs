21008e3efbad35bec00aeb5cc9af6f7751e8d234
crunchyGremlin <crunchygremlin@gmail.com>
2020-01-22 02:34:49 -0800

    consolidate diplomacy pieces.

--- Ship_Game/GameScreens/DiplomacyScreen/DialogOptionListItem.cs
+++ Ship_Game/GameScreens/DiplomacyScreen/DialogOptionListItem.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Ship_Game.Gameplay;
 
-namespace Ship_Game.Gameplay
+namespace Ship_Game.GameScreens.DiplomacyScreen
 {
     public class DialogOptionListItem : ScrollListItem<DialogOptionListItem>
     {
         public readonly DialogOption Option;
         public DialogOptionListItem(DialogOption option)
         {
+            Height = 15;
             Option = option;
         }
         public override void Draw(SpriteBatch batch)
         {
             Color color = (Hovered ? Color.White : new Color(255, 255, 255, 220));
             batch.DrawDropShadowText(Option.Number+". "+Option.Words, Pos, Fonts.Consolas18, color);
+            
         }
+        
     }
 }
