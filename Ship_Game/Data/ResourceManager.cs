a797edeccf117adf54950657c7bbe6942f3eff30
Fizban1978 <gkapulis@gmail.com>
2020-01-25 14:43:08 +0200

    Added Unitests. Created a specific unitest folder for xml of ships used in testing

--- Ship_Game/Data/ResourceManager.cs
+++ Ship_Game/Data/ResourceManager.cs
@@ -1337,7 +1337,7 @@ namespace Ship_Game
             LoadBasicContentForTesting();
 
             FileInfo[] ships = shipsList != null
-                ? shipsList.Select(ship => GetModOrVanillaFile($"StarterShips/{ship}.xml"))
+                ? shipsList.Select(ship => GetModOrVanillaFile($"StarterShips/UnitTest/{ship}.xml"))
                 : GatherFilesModOrVanilla("StarterShips", "xml");
 
             ShipsDict.Clear();
