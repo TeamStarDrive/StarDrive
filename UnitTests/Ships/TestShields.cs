3519a25a7664ed8848aa654d35f666b317d5509a
Fizban1978 <gkapulis@gmail.com>
2020-01-25 14:43:08 +0200

    Added Unitests. Created a specific unitest folder for xml of ships used in testing

---
+++ UnitTests/Ships/TestShields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Ship_Game.Gameplay;
+using Ship_Game.Ships;
+
+namespace UnitTests.Ships
+{
+    [TestClass]
+    public class TestShields : StarDriveTest
+    {
+        public TestShields()
+        {
+            LoadStarterShips("TEST-ShipShield");
+            CreateGameInstance();
+            CreateUniverseAndPlayerEmpire(out _);
+        }
+
+        [TestMethod]
+        public void AmplifierDestroyed()
+        {
+            Ship ship = SpawnShip("TEST-ShipShield", Player, Vector2.Zero);
+            Assert.That.Equal(ship.shield_max, 1400);
+            ShipModule amplifier = ship.TestGetModule("TEST-ModuleAmplifier");
+            Assert.IsNotNull(amplifier);
+            Assert.That.Equal(amplifier.AmplifyShields, 200);
+            amplifier.Active = false;
+            ship.ShipStatusChange();
+            Assert.That.Equal(ship.shield_max, 1200);
+            amplifier.Active = true;
+            ship.ShipStatusChange();
+            Assert.That.Equal(ship.shield_max, 1400);
+        }
+    }
+}
