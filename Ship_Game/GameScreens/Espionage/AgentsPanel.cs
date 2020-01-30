98b33678779f8194bcaa985b91fe16b3f2ebb2d1
crunchyGremlin <crunchygremlin@gmail.com>
2020-01-30 01:33:32 -0800

    fix spy screen

---
+++ Ship_Game/GameScreens/Espionage/AgentsPanel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Ship_Game.GameScreens.Espionage
+{
+    public class AgentsPanel : UIPanel
+    {
+        public AgentsPanel(EspionageScreen screen, in Rectangle rect) : base(rect, EspionageScreen.PanelBackground)
+        {
+            Label(rect.X + 20, rect.Y + 10, 6090, Fonts.Arial20Bold);
+        }
+    }
+}
---
+++ Ship_Game/GameScreens/Espionage/OperationsPanel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Ship_Game.GameScreens.Espionage
+{
+    class OperationsPanel : UIPanel
+    {
+        readonly EspionageScreen Screen;
+        readonly UILabel AgentName;
+        readonly UILabel AgentLevel;
+        public OperationsPanel(EspionageScreen screen, in Rectangle rect) : base(rect, EspionageScreen.PanelBackground)
+        {
+            Screen = screen;
+            AgentName = Label(rect.X + 20, rect.Y + 10, "", Fonts.Arial20Bold);
+            AgentLevel = Label(AgentName.X, AgentName.Y + Fonts.Arial20Bold.LineSpacing + 2, "", Fonts.Arial12Bold);
+            AgentName.DropShadow = true;
+            AgentLevel.Color = Color.Gray;
+        }
+        public override void Draw(SpriteBatch batch)
+        {
+            Agent agent = Screen.Agents.SelectedAgent;
+            AgentName.Visible = agent != null;
+            AgentLevel.Visible = agent != null;
+            if (agent != null)
+            {
+                AgentName.Text = agent.Name;
+                AgentLevel.Text = $"Level {agent.Level} Agent";
+            }
+            base.Draw(batch);
+        }
+    }
+}
