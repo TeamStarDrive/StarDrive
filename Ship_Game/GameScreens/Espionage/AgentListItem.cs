d3f8459336de1a45e5a149c8ba320fe0b734f580
crunchyGremlin <crunchygremlin@gmail.com>
2020-01-30 01:33:32 -0800

    fix spy screen

--- Ship_Game/GameScreens/Espionage/AgentListItem.cs
+++ Ship_Game/GameScreens/Espionage/AgentListItem.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Ship_Game
+namespace Ship_Game.GameScreens.Espionage
 {
     public class AgentListItem : ScrollListItem<AgentListItem>
     {
