5bfac2ed1f4259f1f54e8bf88181c97138a5aac5
crunchyGremlin <crunchygremlin@gmail.com>
2020-02-03 00:55:24 -0800

    updates to ship count contol and forcepool control. remove ship maintenance from treasury goal

--- Ship_Game/Debug/DebugInfoScreen.cs
+++ Ship_Game/Debug/DebugInfoScreen.cs
@@ -623,7 +623,7 @@ namespace Ship_Game.Debug
                 DrawString($"Money: {e.Money.String()} A:({e.GetActualNetLastTurn().String()}) T:({e.GrossIncome.String()})");
                 float taxRate = e.data.TaxRate * 100f;
                 DrawString("Tax Rate:      "+taxRate.ToString("#.0")+"%");
-                DrawString("Ship Maint:    "+e.TotalShipMaintenance);
+                DrawString($"Ship Maint:    {(int)e.TotalShipMaintenance} - War:{(int)e.TotalWarShipMaintenance} - Civ:{(int)e.TotalCivShipMaintenance}");
 
                 Array<Ship> ships = e.GetShips();
                 DrawString($"Ship Count:    {ships.Count}" +
