221d95ac3f7b7fafaceb20ab8534c82447ed6e5b
Fizban1978 <gkapulis@gmail.com>
2020-02-04 23:44:06 +0200

    Fixed debug tech unlocking not updating best orbitals, freighters or scrap code. (Different logic for debug tech unlock)

--- Ship_Game/Empire.cs
+++ Ship_Game/Empire.cs
@@ -981,7 +981,7 @@ namespace Ship_Game
             }
         }
 
-        void UpdateForNewTech()
+        public void UpdateForNewTech()
         {
             UpdateShipsWeCanBuild();
             EmpireAI.TriggerRefit();
