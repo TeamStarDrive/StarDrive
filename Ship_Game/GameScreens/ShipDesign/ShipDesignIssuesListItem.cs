059712a5545b63492d335241cf35049f15f00d67
Fizban1978 <gkapulis@gmail.com>
2020-02-20 01:03:37 +0200

    Changes after review

--- Ship_Game/GameScreens/ShipDesign/ShipDesignIssuesListItem.cs
+++ Ship_Game/GameScreens/ShipDesign/ShipDesignIssuesListItem.cs
@@ -22,25 +22,24 @@ namespace Ship_Game.GameScreens.ShipDesign
             IssueDetails = details;
 
             IssueTexture = Add(new UIPanel(Pos, IssueDetails.Texture));
-            IssueTexture.SetRelPos(100, 80);
             //IssueTexture.Pos = new Vector2(580, 320);
             IssueTexture.Size      = new Vector2(60, 60);
+            IssueTexture.SetRelPos(100, 0);
             IssueTexture.DebugDraw = true;
-            //IssueTexture.RelPos = new Vector2(80, 80);
-            //IssueTexture.ZOrder = 1;
 
-            AddIssueLabel(TitleLabel, IssueDetails.Title, 100, 0, SmallFont, TextAlign.Center, IssueDetails.Color);
-            AddIssueLabel(ProblemLabel, IssueDetails.Problem, 380, 200, SmallFont, TextAlign.VerticalCenter, Color.MintCream);
-            AddIssueLabel(RemediationLabel, IssueDetails.Remediation, 380, 580, SmallFont, TextAlign.VerticalCenter, Color.White);
+            TitleLabel       = AddIssueLabel(IssueDetails.Title, 100, 0, SmallFont, TextAlign.Center, IssueDetails.Color);
+            ProblemLabel     = AddIssueLabel(IssueDetails.Problem, 380, 200, SmallFont, TextAlign.VerticalCenter, Color.MintCream);
+            RemediationLabel = AddIssueLabel(IssueDetails.Remediation, 380, 580, SmallFont, TextAlign.VerticalCenter, Color.White);
         }
 
-        void AddIssueLabel(UILabel label, string text, float sizeX, float relativeX, SpriteFont font, TextAlign align, Color color)
+        UILabel AddIssueLabel(string text, float sizeX, float relativeX, SpriteFont font, TextAlign align, Color color)
         {
             string parsedText = font.ParseText(text, sizeX-20);
-            label             = Add(new UILabel(parsedText, font, color));
+            UILabel label     = Add(new UILabel(parsedText, font, color));
             label.Size        = new Vector2(sizeX, 80);
             label.Align       = align;
             label.SetRelPos(relativeX, 0);
+            return label;
         }
 
         public override void Draw(SpriteBatch batch)
