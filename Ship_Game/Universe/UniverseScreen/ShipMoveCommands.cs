6f69e76efedc48643628392f28735b6f89be9aca
Fizban1978 <gkapulis@gmail.com>
2020-02-20 20:27:28 +0200

    small update

--- Ship_Game/Universe/UniverseScreen/ShipMoveCommands.cs
+++ Ship_Game/Universe/UniverseScreen/ShipMoveCommands.cs
@@ -257,8 +257,6 @@ namespace Ship_Game.Universe
                 ship.AI.OrderMoveTo(pos, direction, true, null, AI.AIState.MoveTo);
                 ship.AI.OrderHoldPosition(pos, direction);
             }
-
-            ship.AI.ChangePriorityOrder(true);
         }
     }
 }
