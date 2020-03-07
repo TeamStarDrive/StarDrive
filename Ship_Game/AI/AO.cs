857cbc04bcc8ca0721bc3b706fed508a47f6db05
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-07 11:25:11 -0800

    Economy updates refactory of lower bound.
    added a concept to allow the AI to spend more when its near its treasury goal.
    this may effect players.
    here is the formula.
    https://mathnotepad.com/#contents=N4IgzgLghhCmIC5RlgG1gYwgSwPYDtFRt8ATWAD0QAYAacaAJwhvtjJoF96wNHdUqACq4ADlzYVRjWGDB58YRAG1QANyioArvAQgAtgVgBPALwACAIzUbAHXwQZUMFsZmrN6vfszSWjLAAFBAAlOYWEAD0AEzm9gBmgRS0xmEWFOYAVObmSQC05r7%2BQalhkbnGBUUBgaX25omG%2BCa0jrDOrqkg9Pqw0ESc3OqaOoggoqi4EIGJydY2ISHdBn1QRCBq2LAA7rCMSgiqIBQAsiSIeQAcAGzRAHTU1%2FSnUFQIlwCcAOx3l5f0xjOhAQeWilwALHcvh8ASdXogAKwfBF3Sz0bbYUgQAAWiIAzHQQNjYNgAObYlgIAmEyiiKBkWCkRCOHQ8bG4bYAZT6OHwpIO8U0KDZHIAMrBSewmQgWbB6PEtIJeDJ2IhBagUJwALqDWjDbS6EDLXr9JCDLX0NR7eQERB4%2BhaUSkGCMsbRajuvLUPFer5CSwfBDRPFB%2B7UZEALRAnCAA%3D%3D%3D
    
    It will use that or risk as appropriate.
    may need to put in a clamp based on current research output as well.
    
    Fixed a couple of crashes when empire is defeated.

--- Ship_Game/AI/AO.cs
+++ Ship_Game/AI/AO.cs
@@ -100,7 +100,7 @@ namespace Ship_Game.AI
             foreach (Ship ship in ShipsWaitingForCoreFleet)
                 str += ship.GetStrength();
 
-            return ThreatLevel + str.ClampMin(100) < CoreFleet.GetStrength();
+            return ThreatLevel + str.LowerBound(100) < CoreFleet.GetStrength();
         }
 
         private void CoreFleetAddShip(Ship ship)
@@ -337,9 +337,9 @@ namespace Ship_Game.AI
             if (CoreFleet != null)
                 Owner?.AddShipNextFrame(CoreFleet.Ships);
             if (OffensiveForcePool?.NotEmpty == true)
-                Owner.AddShipNextFrame(OffensiveForcePool);
+                Owner?.AddShipNextFrame(OffensiveForcePool);
             if (ShipsWaitingForCoreFleet?.NotEmpty == true)
-                Owner.AddShipNextFrame(ShipsWaitingForCoreFleet);
+                Owner?.AddShipNextFrame(ShipsWaitingForCoreFleet);
 
             OffensiveForcePool?.Clear();
             ShipsWaitingForCoreFleet?.Clear();
