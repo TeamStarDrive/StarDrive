c45d0f8b9397d2e5f7a40e1f8f1afc0b1dd39c2a
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-15 03:45:52 -0700

    revert test change to sensor refresh. fix another defense ship bug.

--- Ship_Game/AI/ShipAI/ShipAI.cs
+++ Ship_Game/AI/ShipAI/ShipAI.cs
@@ -627,7 +627,7 @@ namespace Ship_Game.AI
             if (ScanForThreatTimer <= 0f)
             {
                 SetCombatStatus();
-                ScanForThreatTimer = 0.032f; //2f;
+                ScanForThreatTimer = 2f;
             }
         }
 
