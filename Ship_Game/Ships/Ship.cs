9c74ffcdf9362e50f4692b4a1985bb11f77721e9
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-17 03:19:53 -0700

    more performance tweaks. fix a bad loop in threatmatrix update. add precalculation for warp powerduration. put it into the Power class. add a delay for planet target scans so that if they dont find a target they will wait a couple of seconds before scanning again. allow buildings to decrement fire delay when no targets available. use the defense ships count to not scan if all ships are out. fix an issue in fleet where the information warp bool was not getting unset. remove debug mode show all ships. add an option to normal known ship method to not consider range. use this instead. This will make all races known when switching to debug.

--- Ship_Game/Ships/Ship.cs
+++ Ship_Game/Ships/Ship.cs
@@ -1361,14 +1361,14 @@ namespace Ship_Game.Ships
                     OrdAddedPerSecond   += module.OrdnanceAddedPerSecond;
                     HealPerTurn         += module.HealPerTurn;
                     ECMValue             = 1f.Clamped(0f, Math.Max(ECMValue, module.ECM)); // 0-1 using greatest value.
-                    PowerStoreMax       += module.ActualPowerStoreMax;
-                    PowerFlowMax        += module.ActualPowerFlowMax;
                     module.AddModuleTypeToList(module.ModuleType, isTrue: module.InstalledWeapon?.isRepairBeam == true, addToList: RepairBeams);
                 }
             }
 
-            shield_max = ShipUtils.UpdateShieldAmplification(Amplifiers, Shields);
-            NetPower   = Power.Calculate(ModuleSlotList, loyalty);
+            shield_max    = ShipUtils.UpdateShieldAmplification(Amplifiers, Shields);
+            NetPower      = Power.Calculate(ModuleSlotList, loyalty);
+            PowerStoreMax = NetPower.PowerStoreMax;
+            PowerFlowMax  = NetPower.PowerFlowMax;
 
             //Doctor: Add fixed tracking amount if using a mixed method in a mod or if only using the fixed method.
             TrackingPower += FixedTrackingPower;
