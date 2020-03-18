53a255e619168bfe71a58c2d78caa8bcb0578561
Fizban1978 <gkapulis@gmail.com>
2020-03-18 20:04:43 +0200

    refacto - update sapce combat buldings

--- Ship_Game/AI/ShipAI/ShipAI.DoAction.cs
+++ Ship_Game/AI/ShipAI/ShipAI.DoAction.cs
@@ -538,11 +538,11 @@ namespace Ship_Game.AI
 
         void DoReturnHome(float elapsedTime)
         {
-            if (Owner.HomePlanet?.Owner != Owner.loyalty)
+            if (Owner.HomePlanet == null || Owner.HomePlanet.Owner != Owner.loyalty)
             {
                 // find another friendly planet to land at
                 Owner.UpdateHomePlanet(Owner.loyalty.RallyShipYardNearestTo(Owner.Center));
-                if (Owner.HomePlanet?.Owner != Owner.loyalty)
+                if (Owner.HomePlanet == null || Owner.HomePlanet.Owner != Owner.loyalty)
                 {
                     // Nowhere to land, bye bye.
                     Owner.ScuttleTimer = 1;
