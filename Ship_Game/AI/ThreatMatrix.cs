9cef5b4b9917c1ecc1623169f7a998ef42dea515
Fizban1978 <gkapulis@gmail.com>
2020-03-24 21:57:19 +0200

    Refacto Research Priority

--- Ship_Game/AI/ThreatMatrix.cs
+++ Ship_Game/AI/ThreatMatrix.cs
@@ -91,23 +91,18 @@ namespace Ship_Game.AI
         {
             float highestStr = 0;
             Map<Empire, float> empireStrTable = new Map<Empire, float>();
-            using (PinsMutex.AcquireReadLock())
+            foreach (Pin pin in Pins.Values)
             {
-                foreach (Pin pin in Pins.Values)
+                Empire pinEmpire = pin.GetEmpire();
+                if (pinEmpire != null && !pinEmpire.isFaction)
                 {
-                    Empire pinEmpire = pin.GetEmpire();
-                    if (pinEmpire != null && !pinEmpire.isFaction)
-                    {
-                        float str = empire.IsEmpireAttackable(pinEmpire) ? pin.Strength : pin.Strength / 2;
-                        if (!empireStrTable.ContainsKey(empire))
-                            empireStrTable.Add(empire, str);
-                        else
-                            empireStrTable[empire] += str;
-                    }
+                    float str = empire.IsEmpireAttackable(pinEmpire) ? pin.Strength : pin.Strength / 2;
+                    if (!empireStrTable.TryGetValue(pinEmpire, out _))
+                        empireStrTable.Add(pinEmpire, str);
+                    else
+                        empireStrTable[pinEmpire] += str;
                 }
             }
-            if (empireStrTable.Count > 1)
-                Log.Info("lala");
 
             if (empireStrTable.Count > 0)
                 highestStr = empireStrTable.FindMaxValue(v => v);
