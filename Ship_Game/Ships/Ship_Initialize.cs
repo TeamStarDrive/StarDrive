35cca490a9e4a502104154b0439f7e20f5d0afea
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-26 12:12:23 -0700

    dont add ships to force pools if there isnt an empire AI initialized. fixes unit test failures

--- Ship_Game/Ships/Ship_Initialize.cs
+++ Ship_Game/Ships/Ship_Initialize.cs
@@ -85,7 +85,8 @@ namespace Ship_Game.Ships
                 Log.Warning($"Warning: Ship base warp is false: {this}");
             owner.AddShip(this);
             Empire.Universe?.MasterShipList.Add(this);
-            owner.Pool.ForcePoolAdd(this);
+            if (owner.GetEmpireAI() != null)
+                owner.Pool.ForcePoolAdd(this);
         }
 
         protected Ship(string shipName, Empire owner, Vector2 position)
