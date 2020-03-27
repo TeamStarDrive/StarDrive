ce19f978d2387420bbb9af5eaba5bcfd79bac2dc
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-27 04:03:49 -0700

    change rush to change production not the quireitem

---
+++ Ship_Game/UI/UIInputQueue/UIQueueConstruction.cs
@@ -0,0 +1,18 @@
+﻿namespace Ship_Game.UI.UIInputQueue
+{
+    public class UIQueueConstruction : UIQueue
+    {
+        QueueItem Item;
+
+
+        public UIQueueConstruction(QueueItem item)
+        {
+
+        }
+
+        public override void Action()
+        {
+
+        }
+    }
+}
