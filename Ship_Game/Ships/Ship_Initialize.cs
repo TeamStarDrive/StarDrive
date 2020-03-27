1a4de45a89869833806a45ddd292cca329820693
crunchyGremlin <crunchygremlin@gmail.com>
2020-03-27 04:36:34 -0700

    Do not add player ships to the force pool on creation

--- Ship_Game/Ships/Ship_Initialize.cs
+++ Ship_Game/Ships/Ship_Initialize.cs
@@ -85,7 +85,7 @@ namespace Ship_Game.Ships
                 Log.Warning($"Warning: Ship base warp is false: {this}");
             owner.AddShip(this);
             Empire.Universe?.MasterShipList.Add(this);
-            if (owner.GetEmpireAI() != null)
+            if (owner.GetEmpireAI() != null && !owner.isPlayer)
                 owner.Pool.ForcePoolAdd(this);
         }
 
