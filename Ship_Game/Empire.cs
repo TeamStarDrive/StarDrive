e0fc33aa5075447c09ef8a31f90e9490212d628f
Fizban1978 <gkapulis@gmail.com>
2020-06-21 20:44:58 +0300

    currnet maxpop

--- Ship_Game/Empire.cs
+++ Ship_Game/Empire.cs
@@ -1874,18 +1874,22 @@ namespace Ship_Game
         /// <summary>
         /// Gets the total population in billions
         /// </summary>
-        public float GetTotalPop()
+        public float GetTotalPop(out float maxPop)
         {
-            float num = 0.0f;
+            float num = 0f;
+            maxPop    = 0f;
             using (OwnedPlanets.AcquireReadLock())
                 for (int i = 0; i < OwnedPlanets.Count; i++)
-                    num += OwnedPlanets[i].PopulationBillion;
+                {
+                    num    += OwnedPlanets[i].PopulationBillion;
+                    maxPop += OwnedPlanets[i].MaxPopulationBillion;
+                }
 
             return num;
         }
 
         /// <summary>
-        /// Gets the total potential population in billions
+        /// Gets the total potential population in billions (with biospheres/Terraformers if researched)
         /// </summary>
         public float GetTotalPopPotential()
         {
