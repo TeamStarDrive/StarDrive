e33038ec6f98a82e71af7411b8644903f391d60d
Fizban1978 <gkapulis@gmail.com>
2020-07-10 01:12:49 +0300

    moving stuff which needs to be in relationship.cs to relationship.cs

--- Ship_Game/AI/EmpireAI/EmpireAI.DiplomacyAnger.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.DiplomacyAnger.cs
@@ -9,52 +9,6 @@ namespace Ship_Game.AI {
         private readonly int FirstDemand  = 20;
         private readonly int SecondDemand = 75;
 
-        private void AssessDiplomaticAnger(Relationship usToThem, Empire them)
-        {
-            if (!usToThem.Known) 
-                return;
-
-            if (usToThem.Anger_MilitaryConflict >= 5 && !usToThem.AtWar && !usToThem.Treaty_Peace)
-            {
-                DeclareWarOn(them, WarType.DefensiveWar);
-                return;
-            }
-                
-            if (usToThem.Anger_TerritorialConflict + usToThem.Anger_FromShipsInOurBorders >= OwnerEmpire.data.DiplomaticPersonality.Territorialism 
-                && !usToThem.AtWar && !usToThem.Treaty_OpenBorders && !usToThem.Treaty_Peace)
-            {
-                usToThem.PreparingForWar = true;
-                usToThem.PreparingForWarType = WarType.BorderConflict;
-                return;
-            }
-
-            WarnAboutShips(usToThem, them);
-        }
-
-        void WarnAboutShips(Relationship usToThem, Empire them)
-        {
-            if (usToThem.Anger_FromShipsInOurBorders > OwnerEmpire.data.DiplomaticPersonality.Territorialism / 4f
-                && !usToThem.AtWar && !usToThem.WarnedAboutShips)
-            {
-                if (them.isPlayer && usToThem.turnsSinceLastContact > FirstDemand)
-                    if (!usToThem.WarnedAboutColonizing)
-                        DiplomacyScreen.Show(OwnerEmpire, them, "Warning Ships");
-                    else if (usToThem.GetContestedSystem(out SolarSystem contested))
-                        DiplomacyScreen.Show(OwnerEmpire, them, "Warning Colonized then Ships", contested);
-
-                usToThem.turnsSinceLastContact = 0;
-                usToThem.WarnedAboutShips      = true;
-            }
-
-            if (!usToThem.WarnedAboutShips || usToThem.AtWar || !OwnerEmpire.IsEmpireAttackable(them))
-                return;
-
-            int territorialism = OwnerEmpire.data.DiplomaticPersonality.Territorialism;
-            if (them.CurrentMilitaryStrength < OwnerEmpire.CurrentMilitaryStrength * (1f - territorialism * 0.01f))
-                DeclareWarOn(them, WarType.ImperialistWar);
-        }
-        
-
         void ChangeToFriendlyIfPossible(Relationship usToThem, Empire them)
         {
             switch (OwnerEmpire.Personality)
