3c7c755af0758e86e6b5c764d9567a98c49a9779
Fizban1978 <gkapulis@gmail.com>
2020-07-14 19:00:53 +0300

    Small fix. Added infra to Xeno and Nano mines

--- Ship_Game/Commands/Goals/BuildOffensiveShips.cs
+++ Ship_Game/Commands/Goals/BuildOffensiveShips.cs
@@ -42,7 +42,7 @@ namespace Ship_Game.Commands.Goals
 
             if (PlanetBuildingAt.ConstructionQueue[0].Goal == this) 
             {
-                if (PlanetBuildingAt.Storage.ProdRatio > 0.5f && empire.data.TaxRate < 0.5f)
+                if (PlanetBuildingAt.Storage.ProdRatio > 0.5f && empire.data.TaxRate < 0.25f)
                 {
                     float rush = 10;
                     PlanetBuildingAt.Construction.RushProduction(0, rush.UpperBound(PlanetBuildingAt.ProdHere));
