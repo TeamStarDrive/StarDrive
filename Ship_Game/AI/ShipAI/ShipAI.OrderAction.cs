8eae043ed05aff7bef51e75133fb018b856913ae
Fizban1978 <gkapulis@gmail.com>
2020-07-27 21:35:29 +0300

    comment the remove ships from fleet code if it goes to ressuply for now.

--- Ship_Game/AI/ShipAI/ShipAI.OrderAction.cs
+++ Ship_Game/AI/ShipAI/ShipAI.OrderAction.cs
@@ -443,8 +443,8 @@ namespace Ship_Game.AI
             HadPO = clearOrders;
             ClearWayPoints();
 
-            if (!Owner.loyalty.isPlayer)
-                Owner.fleet?.RemoveShip(Owner); // Avoid lingering fleets for the AI
+            //if (!Owner.loyalty.isPlayer) TODO: we cant do this since removeship does not remove the ship from the Ships list. Should Investigate
+            //    Owner.fleet?.RemoveShip(Owner); // Avoid lingering fleets for the AI
 
             Target       = null;
             OrbitTarget  = toOrbit;
