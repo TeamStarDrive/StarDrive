931ae1a237c8d8131298e46b4efc8334cfc0ad72
Fizban1978 <gkapulis@gmail.com>
2020-08-08 16:59:41 +0300

    small fix for tech research actual cost calc

--- Ship_Game/Technology.cs
+++ Ship_Game/Technology.cs
@@ -132,7 +132,9 @@ namespace Ship_Game
             int idealNumPlayers     = (int)(CurrentGame.GalaxySize) + 3;
             float galSizeModifier   = ((int)CurrentGame.GalaxySize / 2f).LowerBound(0.25f);
             int techDepth           = Parents.Length.LowerBound(1);
-            float techDepthModifier = (techDepth * galSizeModifier) / techDepth;
+            float techDepthModifier = CurrentGame.GalaxySize == GalSize.Medium ? (techDepth * galSizeModifier) / techDepth
+                                                                               : (techDepth*techDepth/4).LowerBound(1);
+
             float extraPlanetsMod   = 1 + GlobalStats.ExtraPlanets * 0.1f;
             float playerRatio       = idealNumPlayers / (float)EmpireManager.MajorEmpires.Length; // Cheaper for more empires if bigger than medium
 
