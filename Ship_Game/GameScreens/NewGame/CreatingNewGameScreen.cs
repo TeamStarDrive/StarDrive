bcdd2041ef5de33acf85849641f1d6a6a8e69c77
Fizban1978 <gkapulis@gmail.com>
2020-08-09 22:01:17 +0300

    fixed save stars and extra planets modifier

--- Ship_Game/GameScreens/NewGame/CreatingNewGameScreen.cs
+++ Ship_Game/GameScreens/NewGame/CreatingNewGameScreen.cs
@@ -58,11 +58,11 @@ namespace Ship_Game
                 GalaxySize            = universeSize
             };
 
-            CurrentGame.StartNew(Data, pace, starNumModifier);
+            CurrentGame.StartNew(Data, pace, starNumModifier, GlobalStats.ExtraPlanets);
             Player = player;
             player.isPlayer = true;
 
-            CurrentGame.StartNew(Data, pace, starNumModifier);
+            CurrentGame.StartNew(Data, pace, starNumModifier, GlobalStats.ExtraPlanets);
             player.Initialize();
             player.data.CurrentAutoScout     = player.data.ScoutShip;
             player.data.CurrentAutoColony    = player.data.ColonyShip;
