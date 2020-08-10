dde971cdf4928c28e0d227273432302a0f86c846
Jorma <jorma.rebane@gmail.com>
2020-08-10 23:22:20 +0300

    Added an alternative base content zip

--- appveyor.ps1
+++ appveyor.ps1
@@ -7,11 +7,11 @@ if ( [System.IO.File]::Exists($stardrive) )
 }
 else
 {
-    $url = "https://cdn-33.anonfiles.com/xfG3K5Lfo0/9ff48d58-1597089408/StarDrive.zip"
+    $url = "https://filebin.net/jikeykt2f2njtbdd/StarDrive.zip?t=4gj9qmk6"
     $zip = "C:\Projects\BlackBox\StarDrive.zip"
     $out = "C:\Projects\BlackBox\StarDrive"
     $start_time = Get-Date
-    Write-Output "Downloading StarDrive.zip ... 2-3 minutes for 710MB"
+    Write-Output "Downloading StarDrive.zip ... 2-3 minutes for 707MB"
     #Start-FileDownload $url
     Start-BitsTransfer -Source $url -Destination $zip
     Write-Output "Time taken: $((Get-Date).Subtract($start_time).TotalSeconds) second(s)"
