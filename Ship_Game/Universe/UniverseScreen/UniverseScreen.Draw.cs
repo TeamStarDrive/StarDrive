42426880980d6bd20faaa260be973892112f086b
crunchyGremlin <crunchygremlin@gmail.com>
2020-08-29 20:24:14 -0700

    remove some restrictions on attacking systems. let the AI attack more planets in a system. fix bug preventing empire defensive from launching invasions. empty systems to attack list when rebuilding it.
    add a few more random locking to prevent destruction of random number generator.
    
    add some try catching to try and get the correct error from some threaded loops.
    
    remove fleet completeness dificulty modifiers. The fleet completeness is very sensitive. too much and it wont create any fleets. too little and it wont create useful fleets. best to leave it alone and let difficulty create more fleets rather than rarer bigger ones.

--- Ship_Game/Universe/UniverseScreen/UniverseScreen.Draw.cs
+++ Ship_Game/Universe/UniverseScreen/UniverseScreen.Draw.cs
@@ -417,8 +417,8 @@ namespace Ship_Game
             if (viewState >= UnivScreenState.SectorView) // draw colored empire borders only if zoomed out
             {
                 // set the alpha value depending on camera height
-                int alpha = (int) (70.0f * CamHeight / 1800000.0f);
-                if (alpha > 70) alpha = 70;
+                int alpha = (int) (90.0f * CamHeight / 1800000.0f);
+                if (alpha > 90) alpha = 90;
                 else if (alpha < 10) alpha = 0;
                 var color = new Color(255, 255, 255, (byte) alpha);
 
