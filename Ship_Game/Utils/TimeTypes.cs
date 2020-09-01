9ef111e91c67b334364f5ddd3c758856e4c99e50
Jorma <jorma.rebane@gmail.com>
2020-09-01 22:33:59 +0300

    add: base time types FixedSimTime and VariableFrameTime

---
+++ Ship_Game/Utils/TimeTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game.Utils
+{
+    /// <summary>
+    /// Time type which describes FIXED tick time used in simulations.
+    /// This is a CONSTANT time step for our physics simulations
+    /// </summary>
+    public struct FixedSimTime
+    {
+        public double Time;
+        public FixedSimTime(double time)
+        {
+            Time = time;
+        }
+    }
+
+    /// <summary>
+    /// Time type which contains variable frame delta time used for drawing.
+    /// This elapsed time can have huge differences and is NOT suitable for simulations
+    /// </summary>
+    public struct VariableFrameTime
+    {
+        public double Time;
+        public VariableFrameTime(double time)
+        {
+            Time = time;
+        }
+    }
+}
