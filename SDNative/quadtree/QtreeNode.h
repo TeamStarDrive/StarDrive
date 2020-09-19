957f1b775ca51dbf1ef99ee52d7f4c4259c10fb7
Jorma <jorma.rebane@gmail.com>
2020-09-20 00:31:52 +0300

    Store Node cx,cy,radius inside the node itself, this gives a significant +40% performance improvement

--- SDNative/quadtree/QtreeNode.h
+++ SDNative/quadtree/QtreeNode.h
@@ -5,14 +5,12 @@
 
 namespace tree
 {
-    class QtreeAllocator;
-
     struct QtreeNode
     {
-        // Our node can be two things:
+        // Our node can be in 2 states:
         // BRANCH: It has no objects and has 4 sub nodes
         // LEAF:   It has only objects and NO sub nodes
-        // This is done to save one pointer storage and fit this QtreeNode into 8-byte boundary on x86
+        // This is done to save one pointer storage
         union
         {
             QtreeNode* nodes = nullptr;
@@ -26,20 +24,39 @@ namespace tree
         // by default start as a LEAF
         int size = 0;
 
+        // node center-x, center-y, radius
+        int cx = 0;
+        int cy = 0;
+        int radius = 0;
+
         bool isBranch() const { return size == BRANCH_ID; }
         bool isLeaf()   const { return size != BRANCH_ID; }
 
-        QtreeNode* nw() const { return &nodes[0]; }
-        QtreeNode* ne() const { return &nodes[1]; }
-        QtreeNode* se() const { return &nodes[2]; }
-        QtreeNode* sw() const { return &nodes[3]; }
+        TREE_FINLINE QtreeNode* nw() const { return &nodes[0]; }
+        TREE_FINLINE QtreeNode* ne() const { return &nodes[1]; }
+        TREE_FINLINE QtreeNode* se() const { return &nodes[2]; }
+        TREE_FINLINE QtreeNode* sw() const { return &nodes[3]; }
+
+        TREE_FINLINE int height() const { return (radius<<1); }
+        TREE_FINLINE int width()  const { return (radius<<1); }
+        TREE_FINLINE int left()   const { return cx - radius; }
+        TREE_FINLINE int right()  const { return cx + radius; }
+        TREE_FINLINE int top()    const { return cy - radius; }
+        TREE_FINLINE int bottom() const { return cy + radius; }
+
+        TREE_FINLINE void setCoords(int centerX, int centerY, int nodeRadius)
+        {
+            cx = centerX;
+            cy = centerY;
+            radius = nodeRadius;
+        }
 
         // fast adding of an object
-        __forceinline void addObject(QtreeAllocator& allocator, const QtreeObject& item)
+        TREE_FINLINE void addObject(QtreeAllocator& allocator, const QtreeObject& item)
         {
             if (size == 0)
             {
-                objects = allocator.allocArray<QtreeObject>(QuadCellThreshold);
+                objects = allocator.allocArray<QtreeObject>(QuadDefaultLeafSplitThreshold);
             }
             objects[size++] = item;
         }
@@ -58,11 +75,11 @@ namespace tree
         }
 
         // adds another object, growth is only limited by QuadLinearAllocatorSlabSize
-        __forceinline void addObjectUnbounded(QtreeAllocator& allocator, const QtreeObject& item)
+        TREE_FINLINE void addObjectUnbounded(QtreeAllocator& allocator, const QtreeObject& item)
         {
-            if (size == QuadCellThreshold)
+            if (size == QuadDefaultLeafSplitThreshold)
             {
-                constexpr int capacity = upperPowerOf2(QuadCellThreshold+1);
+                constexpr int capacity = upperPowerOf2(QuadDefaultLeafSplitThreshold+1);
                 objects = allocator.allocArray(objects, size, capacity);
             }
             else
@@ -78,58 +95,17 @@ namespace tree
 
 
         // Converts a LEAF node into a BRANCH node which contains sub-QtreeNode's
-        __forceinline void convertToBranch(QtreeAllocator& allocator)
+        TREE_FINLINE void convertToBranch(QtreeAllocator& allocator)
         {
             // create 4 LEAF nodes
             nodes = allocator.allocArrayZeroed<QtreeNode>(4);
             size = BRANCH_ID;
+            
+            const int r2 = radius >> 1;
+            nodes[0].setCoords(cx-r2, cy-r2, r2); // NW
+            nodes[1].setCoords(cx+r2, cy-r2, r2); // NE
+            nodes[2].setCoords(cx+r2, cy+r2, r2); // SE
+            nodes[3].setCoords(cx-r2, cy+r2, r2); // SW
         }
-    };
-
-    // Simple helper: Node pointer, with Center XY and Bounds AABB
-    // We don't store these in the tree itself, in order to save space and improve cache locality
-    struct QtreeBoundedNode
-    {
-        QtreeNode* node;
-        int cx, cy, r; // node center-x, center-y, radius
-
-        /** @return TRUE if this is LEAF node with no child nodes */
-        __forceinline bool isLeaf() const { return node->isLeaf(); }
-
-        /** @return TRUE if this is a branch with child nodes */
-        __forceinline bool isBranch() const { return node->isBranch(); }
-
-        __forceinline int height() const { return r*2; }
-        __forceinline int width()  const { return r*2; }
-
-        __forceinline QtreeBoundedNode nw() const
-        {
-            int r2 = r >> 1;
-            return QtreeBoundedNode{ &node->nodes[0], cx-r2, cy-r2, r2 };
-        }
-
-        __forceinline QtreeBoundedNode ne() const
-        {
-            int r2 = r >> 1;
-            return QtreeBoundedNode{ &node->nodes[1], cx+r2, cy-r2, r2 };
-        }
-
-        __forceinline QtreeBoundedNode se() const
-        {
-            int r2 = r >> 1;
-            return QtreeBoundedNode{ &node->nodes[2], cx+r2, cy+r2, r2 };
-        }
-        
-        __forceinline QtreeBoundedNode sw() const
-        {
-            int r2 = r >> 1;
-            return QtreeBoundedNode{ &node->nodes[3], cx-r2, cy+r2, r2 };
-        }
-
-        //__forceinline bool overlaps(const QtreeRect& r) const
-        //{
-        //    return left <= r.right  && right  > r.left
-        //        && top  <= r.bottom && bottom > r.top;
-        //}
     };
 }
