6a51acac548400586c44b451b89a7bf83634f8bc
Fizban1978 <gkapulis@gmail.com>
2020-10-16 02:14:44 +0300

    Remnant Tweaks

--- Ship_Game/Commands/Goals/RemnantPortal.cs
+++ Ship_Game/Commands/Goals/RemnantPortal.cs
@@ -48,6 +48,7 @@ namespace Ship_Game.Commands.Goals
             Remnants.OrderEscortPortal(Portal);
             float production = Empire.Universe.StarDate - 1000; // Stardate 1100 yields 100, 1200 yields 200, etc.
             production      *= empire.DifficultyModifiers.RemnantResourceMod;
+            production      *= (int)(CurrentGame.GalaxySize + 1) * 2f / EmpireManager.MajorEmpires.Length;
             Remnants.TryGenerateProduction(production);
             return GoalStep.TryAgain;
         }
