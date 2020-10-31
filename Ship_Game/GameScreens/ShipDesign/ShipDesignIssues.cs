8846e3239bc4b7941e9eaef487b111a6e1ead561
Fizban1978 <gkapulis@gmail.com>
2020-10-31 02:47:01 +0200

    image for low target tracking issue made by Moonster

--- Ship_Game/GameScreens/ShipDesign/ShipDesignIssues.cs
+++ Ship_Game/GameScreens/ShipDesign/ShipDesignIssues.cs
@@ -601,7 +601,7 @@ namespace Ship_Game.ShipDesignIssues
                     Title       = new LocalizedText(GameText.LowTracking).Text;
                     Problem     = new LocalizedText(GameText.TrackingTargets).Text;
                     Remediation = new LocalizedText(GameText.ImproveTracking).Text;
-                    Texture     = ResourceManager.Texture("NewUI/IssuesLowAccuracy");
+                    Texture     = ResourceManager.Texture("NewUI/IssueLowTracking");
                     break;
             }
 
