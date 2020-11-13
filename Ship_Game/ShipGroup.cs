a0f82cfc5864114211b77fa102198fe7456e4e94
Fizban1978 <gkapulis@gmail.com>
2020-11-13 11:40:29 +0200

    Revert "Merged in feature/war (pull request #400)"
    This reverts commit c317ef60a662db03d2b0d942e9edb51c04bfcb60, reversing
    changes made to fcce01be03358c379f254ae5a98e75a2457db437.

--- Ship_Game/ShipGroup.cs
+++ Ship_Game/ShipGroup.cs
@@ -481,8 +481,7 @@ namespace Ship_Game
                 if (ship.AI.State == AIState.Orbit || !ship.Center.InRadius(position, radius))
                     continue;
 
-                if (planet != null)
-                    ship.OrderToOrbit(planet);
+                ship.OrderToOrbit(planet);
             }
         }
 
@@ -586,7 +585,7 @@ namespace Ship_Game
         {
             float combatRadius = Math.Min(radius, ship.AI.FleetNode.OrdersRadius);
             if (!ship.CanTakeFleetOrders || ship.Center.OutsideRadius(position + ship.FleetOffset, combatRadius))
-                return CombatStatus.NotApplicable;
+                return CombatStatus.ClearSpace;
 
             if (ship.InCombat) return CombatStatus.InCombat;
             if (ship.AI.BadGuysNear) return CombatStatus.EnemiesNear;
