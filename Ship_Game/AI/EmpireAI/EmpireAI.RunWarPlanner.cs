eea9410d4acc92b5f12042b01f57e57689c5b8a7
Crunchy Gremlin <crunchygremlin@gmail.com>
2020-11-19 19:55:35 -0800

    add failover when construction ship is not found. try the current constructor, log a warning instead of an error if not found, then fail over to the default constructor and throw an error if that isnt found. useful for save games given to other people that dont have the wanted construction ship

--- Ship_Game/AI/EmpireAI/EmpireAI.RunWarPlanner.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.RunWarPlanner.cs
@@ -398,5 +398,19 @@ namespace Ship_Game.AI
             }
             WarTasks.Update();
         }
+
+        //bool ShouldGoToWar(Relationship rel, Empire them)
+        //{
+        //    if (!rel.PreparingForWar) return false;
+        //    float anger = (rel.TotalAnger - rel.Trust);
+        //    if (anger < 0) return false;
+
+        //    int averageWarStatus = rel.WarHistory
+
+
+        //    if (anger > 99) return true;
+        //    float risk = rel.Risk.Risk;
+
+        //}
     }
 }
