0310a892ee50f9d12407aff476ebb4836c2d26e3
Fizban1978 <gkapulis@gmail.com>
2020-12-03 16:16:15 +0200

    Ai will have some standby colony ships. this will reduce scout fleet waiting times and free resources to other tasks.

--- Ship_Game/Fleets/Fleet.cs
+++ Ship_Game/Fleets/Fleet.cs
@@ -1092,6 +1092,9 @@ namespace Ship_Game.Fleets
 
         void DoClearAreaOfEnemies(MilitaryTask task)
         {
+            if (task.TargetEmpire == null)
+                task.TargetEmpire = Owner.GetEmpireAI().ThreatMatrix.GetDominantEmpireInSystem(FleetTask.TargetSystem);
+
             float enemyStrength = Owner.GetEmpireAI().ThreatMatrix.PingHostileStr(task.AO, task.AORadius, Owner);
             bool threatIncoming = Owner.SystemWithThreat.Any(t=> !t.ThreatTimedOut && t.TargetSystem == FleetTask.TargetSystem);
             bool stillThreats = threatIncoming || (enemyStrength > 1 || TaskStep < 5);
