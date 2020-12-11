5cb5461bd719ac8717e461e2cbed3316c6d8433c
Fizban1978 <gkapulis@gmail.com>
2020-12-11 15:08:05 +0200

    Refit to use astrogation

--- Ship_Game/Commands/Goals/RefitOrbital.cs
+++ Ship_Game/Commands/Goals/RefitOrbital.cs
@@ -44,8 +44,8 @@ namespace Ship_Game.Commands.Goals  // Created by Fat Bastard
             if (OldShip.AI.State == AIState.Refit)
                 RemoveOldRefitGoal();
 
-            PlanetBuildingAt = empire.FindPlanetToBuildAt(empire.SafeSpacePorts, OldShip.RefitCost(newOrbital));
-            if (PlanetBuildingAt == null)
+            if (!empire.FindPlanetToRefitAt(empire.SafeSpacePorts, OldShip.RefitCost(newOrbital),
+                OldShip, true, out PlanetBuildingAt))
             {
                 OldShip.AI.ClearOrders();
                 return GoalStep.GoalFailed;  // No planet to refit
