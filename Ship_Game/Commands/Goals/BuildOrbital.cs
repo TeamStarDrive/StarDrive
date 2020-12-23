4106d11a6c4570d0e052e20c247400cdf7a3110c
Fizban1978 <gkapulis@gmail.com>
2020-12-23 16:44:22 +0200

    better checks for construction shipscompleted in build orbital and deep space construction goals.

--- Ship_Game/Commands/Goals/BuildOrbital.cs
+++ Ship_Game/Commands/Goals/BuildOrbital.cs
@@ -18,7 +18,7 @@ namespace Ship_Game.Commands.Goals  // Created by Fat Bastard
             Steps = new Func<GoalStep>[]
             {
                 BuildConstructor,
-                WaitMainGoalCompletion,
+                WaitForConstructorBuilt,
                 OrderDeployOrbital,
                 WaitForDeployment
             };
@@ -56,6 +56,14 @@ namespace Ship_Game.Commands.Goals  // Created by Fat Bastard
 
             PlanetBuildingAt.Construction.Enqueue(orbital, ShipToBuild, this);
             return GoalStep.GoToNextStep;
+        }
+
+        GoalStep WaitForConstructorBuilt() // When the Ship is finished, the goal is moved externally to next step (ReportShipComplete).
+        {
+            if (PlanetBuildingAt.ConstructionQueue.Filter(q => q.Goal == this).Length == 0 && FinishedShip == null)
+                return GoalStep.GoalFailed;
+
+            return GoalStep.TryAgain;
         }
 
         GoalStep OrderDeployOrbital()
