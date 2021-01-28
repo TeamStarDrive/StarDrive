c3220018ebbf70cd17d5a46aa6235d95dd2547fc
Fizban1978 <gkapulis@gmail.com>
2021-01-28 21:10:59 +0200

    cleanup

--- Ship_Game/Data/ResourceManager.cs
+++ Ship_Game/Data/ResourceManager.cs
@@ -1244,14 +1244,11 @@ namespace Ship_Game
                 if (data.IsCommandModule && data.TargetTracking == 0)  data.TargetTracking = (sbyte) (int)(data.XSIZE * data.YSIZE * 1.25f );
                 if (data.IsCommandModule && data.TargetAccuracy == 0)  data.TargetAccuracy = data.TargetTracking;
 
-
-                {
-                    data.DisableRotation = data.DisableRotation || data.XSIZE == data.YSIZE
-                        || (data.WeaponType.NotEmpty() && data.ModuleType == ShipModuleType.Turret);
-                }
+                data.DisableRotation = data.DisableRotation 
+                                       || data.XSIZE == data.YSIZE 
+                                       || (data.WeaponType.NotEmpty() && data.ModuleType == ShipModuleType.Turret);
 
                 ModuleTemplates[data.UID] = ShipModule.CreateTemplate(data);
-
             }
 
             //Log.Info("Num ShipModuleFlyweight: {0}", ShipModuleFlyweight.TotalNumModules);
