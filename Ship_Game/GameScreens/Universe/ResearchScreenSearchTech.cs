9d7fb0fbf8b68589c585856b02623a9ea39a2418
Fizban1978 <gkapulis@gmail.com>
2021-02-05 02:21:11 +0200

    implementing Search function for research tech

---
+++ Ship_Game/GameScreens/Universe/ResearchScreenSearchTech.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Ship_Game.Audio;
+
+namespace Ship_Game
+{
+    public sealed class ResearchScreenSearchTech : GameScreen
+    {
+        private readonly Menu2 Window;
+        private readonly Color Cream = Colors.Cream;
+        private readonly SpriteFont LargeFont = Fonts.Arial20Bold;
+        readonly ScrollList2<SearchTechItem> TechList;
+        readonly UITextEntry SearchTech;
+        readonly ResearchScreenNew Screen;
+
+        public ResearchScreenSearchTech(ResearchScreenNew screen, Array<TreeNode> allTreeNodes) : base(screen)
+        {
+            IsPopup           = true;
+            TransitionOnTime  = 0.25f;
+            TransitionOffTime = 0.25f;
+            Screen            = screen;
+            int height        = (int)(ScreenHeight * 0.8f);
+            Window = Add(new Menu2(new Rectangle(ScreenWidth / 2 - 125, ScreenHeight / 2 - (height/2), 250, height)));
+
+            var panelRect   = new Rectangle((int)Window.X + 20, (int)Window.Y + 80, (int)Window.Width - 40, (int)Window.Height - 110);
+            var panel       = new Submenu(panelRect, SubmenuStyle.Blue);
+            TechList        = Add(new ScrollList2<SearchTechItem>(panel, 125, ListStyle.Blue));
+            SearchTech      = Add(new UITextEntry(new Vector2(Window.X + 30, Window.Y + 65), ""));
+            SearchTech.Font = Fonts.Arial14Bold;
+            SearchTech.ClickableArea = new Rectangle((int)Window.X + 35, (int)Window.Y + 70, (int)Window.Width - 70, 36);
+            SearchTech.MaxCharacters = 14;
+            Add(new Submenu(Window.X + 30, Window.Y + 40, Window.Y + 60, 50, SubmenuStyle.Blue));
+            PerformLayout();
+        }
+
+        void PopulateTechs()
+        {
+            TechList.Reset();
+            var items = new Array<SearchTechItem>();
+
+            foreach (var entry in EmpireManager.Player.TechnologyDict)
+            {
+                string lower = entry.Key.ToLower();
+                if (entry.Value.Discovered && !entry.Value.IsRoot &&
+                    (SearchTech.Text.IsEmpty() || lower.Contains(SearchTech.Text.ToLower())))
+                {
+                    TreeNode node = new TreeNode(Vector2.Zero, entry.Value, Screen);
+                    items.Add(CreateQueueItem(node));
+                }
+            }
+
+            TechList.SetItems(items);
+            TechList.RequiresLayout = true;
+    }
+
+        SearchTechItem CreateQueueItem(TreeNode node)
+        {
+            var defaultPos = new Vector2(Window.X + 5, Window.Y);
+            return new SearchTechItem(Screen, node, defaultPos) { List = TechList };
+        }
+
+        public override void LoadContent()
+        {
+            CloseButton(Window.Menu.Right - 40, Window.Menu.Y + 20);
+            //Screen Title
+            string title    = "Search Technology";
+            Vector2 menuPos = new Vector2(Window.Menu.CenterTextX(title, LargeFont), Window.Menu.Y + 35);
+            Label(menuPos, title, LargeFont, Cream);
+            PopulateTechs();
+            base.LoadContent();
+        }
+
+        public override void Draw(SpriteBatch batch, DrawTimes elapsed)
+        {
+            ScreenManager.FadeBackBufferToBlack(TransitionAlpha * 2 / 3);
+            batch.Begin();
+            SearchTech.Draw(batch, elapsed);
+            base.Draw(batch, elapsed);
+            batch.End();
+        }
+
+        public override bool HandleInput(InputState input)
+        {
+            if (SearchTech.HandlingInput && input.Escaped)
+                return true;
+            
+            if (SearchTech.HitTest(input.CursorPosition) && !SearchTech.HandlingInput)
+                SearchTech.HandlingInput = true;
+            
+            if (!SearchTech.HitTest(input.CursorPosition) && (input.RightMouseClick || input.LeftMouseClick))
+                SearchTech.HandlingInput = false;
+            
+            if (SearchTech.HandleInput(input) && SearchTech.HandlingInput)
+            {
+                PopulateTechs();
+                return true;
+            }
+
+            if (input.RightMouseClick)
+            {
+                foreach (SearchTechItem item in TechList.AllEntries)
+                {
+                    if (item.HandleInput(input))
+                        return true;
+                }
+            }
+
+            if (!SearchTech.HandlingInput && (input.Escaped || input.RightMouseClick))
+            {
+                ExitScreen();
+                return true;
+            }
+
+            return base.HandleInput(input);
+        }
+    }
+}
---
+++ Ship_Game/SearchTechItem.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Ship_Game
+{
+    public sealed class SearchTechItem : ScrollListItem<SearchTechItem>
+    {
+        readonly ResearchScreenNew Screen;
+        public readonly TechEntry Tech;
+        private TreeNode Node;
+
+        public override string ToString() => $"SearchTechItem \"{Tech.UID}\" {ElementDescr}";
+
+        public SearchTechItem(ResearchScreenNew screen, TreeNode node, Vector2 pos)
+        {
+            Screen = screen;
+            Tech    = node.Entry;
+            Pos     = pos;
+            PerformLayout();
+        }
+
+        public override void PerformLayout()
+        {
+            Size = new Vector2(160, 110);
+            Node = new TreeNode(Pos + new Vector2(20f, 50f), Tech, Screen);
+            base.PerformLayout();
+        }
+
+        public override bool HandleInput(InputState input)
+        {
+            if (Node.HandleInput(input, Screen.ScreenManager, Screen.camera))
+                return true;
+
+            return false;
+        }
+
+        public override void Draw(SpriteBatch batch, DrawTimes elapsed)
+        {
+            base.Draw(batch, elapsed);
+            Node.Draw(batch);
+        }
+    }
+}
