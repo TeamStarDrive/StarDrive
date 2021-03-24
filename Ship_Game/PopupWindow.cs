4ef82effbadd4d3b1f817c47483af4ed29187719
Jorma <jorma.rebane@gmail.com>
2021-03-24 02:20:37 +0200

    make MiddleText optional in PopupWindow

--- Ship_Game/PopupWindow.cs
+++ Ship_Game/PopupWindow.cs
@@ -69,9 +69,17 @@ namespace Ship_Game
             batch.Draw(ResourceManager.Texture("Popup/popup_horiz_B_gradient"), BotSep, Color.White);
             batch.Draw(ResourceManager.Texture("Popup/popup_filler_lower"), BottomFill, Color.White);
             batch.Draw(ResourceManager.Texture("Popup/popup_filler_lower"), BottomBigFill, Color.White);
-            batch.Draw(ResourceManager.Texture("Popup/popup_filler_mid"), MidContainer, Color.White);
-            batch.Draw(ResourceManager.Texture("Popup/popup_separator"), MidSepTop, Color.White);
-            batch.Draw(ResourceManager.Texture("Popup/popup_separator"), MidSepBot, Color.White);
+
+            if (MiddleText != null)
+            {
+                batch.Draw(ResourceManager.Texture("Popup/popup_filler_lower"), MidContainer, Color.White);
+                batch.Draw(ResourceManager.Texture("Popup/popup_separator"), MidSepTop, Color.White);
+                batch.Draw(ResourceManager.Texture("Popup/popup_separator"), MidSepBot, Color.White);
+            }
+            else
+            {
+                batch.Draw(ResourceManager.Texture("Popup/popup_separator"), MidSepTop, Color.White);
+            }
 
             batch.Draw(ResourceManager.Texture("Popup/popup_filler_title"), TitleRect, Color.White);
             batch.Draw(ResourceManager.Texture("Popup/popup_filler_title"), TitleLeft, Color.White);
@@ -99,50 +107,57 @@ namespace Ship_Game
             TLc.Y      = TLc.Y + 3;
             TLc.Width  = 30;
             TLc.Height = 27;
-            TR = new Rectangle(Rect.X + Rect.Width - 28, Rect.Y, 28, 30);
+            TR = new Rectangle(Rect.Right - 28, Rect.Y, 28, 30);
             TRc        = TR;
             TRc.Y      = TRc.Y + 3;
             TRc.Width  = 28;
             TRc.Height = 27;
             int distance = Rect.Width - 60 - 433;
-            TopSep     = new Rectangle(TL.X + TL.Width + distance / 2, TL.Y + 3, 433, 4);
-            TopHoriz   = new Rectangle(TL.X + TL.Width - 2, TopSep.Y, Rect.Width - 54, 4);
-            BL         = new Rectangle(Rect.X, Rect.Y + Rect.Height - 30, 28, 30);
-            BR         = new Rectangle(Rect.X + Rect.Width - 28, Rect.Y + Rect.Height - 30, 28, 30);
-            BotSep     = new Rectangle(BL.X + BL.Width + distance / 2, BL.Y + 18, 433, 12);
-            BotHoriz   = new Rectangle(BL.X + BL.Width - 2, BotSep.Y, Rect.Width - 54, 12);
+            TopSep     = new Rectangle(TL.Right + distance / 2, TL.Y + 3, 433, 4);
+            TopHoriz   = new Rectangle(TL.Right - 2, TopSep.Y, Rect.Width - 54, 4);
+            BL         = new Rectangle(Rect.X, Rect.Bottom - 30, 28, 30);
+            BR         = new Rectangle(Rect.Right - 28, Rect.Bottom - 30, 28, 30);
+            BotSep     = new Rectangle(BL.Right + distance / 2, BL.Y + 18, 433, 12);
+            BotHoriz   = new Rectangle(BL.Right - 2, BotSep.Y, Rect.Width - 54, 12);
             TitleRect  = new Rectangle(Rect.X + 28, Rect.Y + 7, Rect.Width - 56, 46);
             TitleLeft  = new Rectangle(TitleRect.X - 25, TitleRect.Y + 23, 25, TitleRect.Height - 23);
-            TitleRight = new Rectangle(TitleRect.X + TitleRect.Width, TitleRect.Y + 23, 17, TitleRect.Height - 23);
-            LeftVert   = new Rectangle(TL.X + 1, TL.Y + TL.Height, 2, Rect.Height - 60);
-            RightVert  = new Rectangle(Rect.X + Rect.Width - 11, TL.Y + TL.Height, 11, Rect.Height - 60);
-            BLc        = new Rectangle(Rect.X - 2, Rect.Y + Rect.Height - 30, 28, 30);
-            BRc        = new Rectangle(BR.X, Rect.Y + Rect.Height - 30, 28, 30);
-            BottomFill = new Rectangle(BL.X + BL.Width, BL.Y, Rect.Width - BL.Width - BR.Width, BL.Height - 12);
-            MidContainer = new Rectangle(TitleLeft.X, TitleRect.Y + TitleRect.Height, TitleRect.Width + TitleLeft.Width + TitleRight.Width, 88);
-            MidSepTop    = new Rectangle(MidContainer.X, MidContainer.Y, MidContainer.Width, 2);
-            MidSepBot    = new Rectangle(MidContainer.X, MidContainer.Y + MidContainer.Height - 2, MidContainer.Width, 2);
-            BottomBigFill = new Rectangle(MidContainer.X, MidContainer.Y + MidContainer.Height, 
-                MidContainer.Width, BottomFill.Y - (MidContainer.Y + MidContainer.Height));
+            TitleRight = new Rectangle(TitleRect.Right, TitleRect.Y + 23, 17, TitleRect.Height - 23);
+            LeftVert   = new Rectangle(TL.X + 1, TL.Bottom, 2, Rect.Height - 60);
+            RightVert  = new Rectangle(Rect.Right - 11, TL.Bottom, 11, Rect.Height - 60);
+            BLc        = new Rectangle(Rect.X - 2, Rect.Bottom - 30, 28, 30);
+            BRc        = new Rectangle(BR.X, Rect.Bottom - 30, 28, 30);
+            BottomFill = new Rectangle(BL.Right, BL.Y, Rect.Width - BL.Width - BR.Width, BL.Height - 12);
+
 
             EmpireFlagRect = new Rectangle(TitleRight.X-75, TitleRight.Y-22, 45, 45);
 
-            Close = CloseButton(Rect.X + Rect.Width - 38, Rect.Y + 15);
+            Close = CloseButton(Rect.Right - 44, Rect.Y + 19);
 
             if (TitleText != null)
             {
-                var pos = new Vector2(TitleRect.X, TitleRect.Y + TitleRect.Height / 2 - Fonts.Arial20Bold.LineSpacing / 2);
+                var pos = new Vector2(TitleRect.X, TitleRect.CenterY() - Fonts.Arial20Bold.LineSpacing / 2);
                 TitleLabel = Label(pos.Rounded(), TitleText, Fonts.Arial20Bold);
             }
+
             if (MiddleText != null)
             {
+                MidContainer = new Rectangle(TitleLeft.X, TitleRect.Bottom, TitleRect.Width + TitleLeft.Width + TitleRight.Width, 88);
                 MiddleText = Fonts.Arial12Bold.ParseText(MiddleText, MidContainer.Width - 50);
 
                 var textSize = Fonts.Arial12Bold.MeasureString(MiddleText);
-                var pos = new Vector2(MidContainer.X + MidContainer.Width  / 2 - textSize.X / 2f, 
-                                      MidContainer.Y + MidContainer.Height / 2 - textSize.Y / 2f);
+                var pos = new Vector2(MidContainer.CenterX() - textSize.X / 2f, 
+                                      MidContainer.CenterY() - textSize.Y / 2f);
                 MiddleLabel = Label(pos.Rounded(), MiddleText, Fonts.Arial12Bold);
             }
+            else
+            {
+                MidContainer = new Rectangle(TitleLeft.X, TitleRect.Bottom, TitleRect.Width + TitleLeft.Width + TitleRight.Width, 0);
+            }
+
+            MidSepTop    = new Rectangle(MidContainer.X, MidContainer.Y, MidContainer.Width, 2);
+            MidSepBot    = new Rectangle(MidContainer.X, MidContainer.Bottom - 2, MidContainer.Width, 2);
+            BottomBigFill = new Rectangle(MidContainer.X, MidContainer.Bottom, MidContainer.Width, BottomFill.Y - MidContainer.Bottom);
+
             BodyTextStart = new Vector2(BottomBigFill.Left +12, BottomBigFill.Top + 12);
 
             base.LoadContent();
