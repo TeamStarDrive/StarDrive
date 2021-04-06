486f5acbb02c6eb533878399e5e066b4fe3b0f9f
RedFox <jorma.rebane@gmail.com>
2021-04-06 22:02:08 +0300

    Refactoring Localization DB

---
+++ SDGameTextToEnum/LocalizationUsage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDGameTextToEnum
+{
+    public enum Usage
+    {
+        Unknown,    //
+        Building,   // prefix=Bldg
+        Technology, // prefix=Tech
+        Tooltip,    // prefix=Tips
+        Ship,       // prefix=Ship
+        Weapon,     // prefix=Weps
+    }
+
+    public class LocalizationUsage
+    {
+        public int Id;
+        public Usage Usage;
+        public string Tag;
+        public string File;
+
+        public string UsagePrefix
+        {
+            get
+            {
+                switch (Usage)
+                {
+                    default:
+                    case Usage.Unknown: throw new Exception($"Unknown usage Tag={Tag} File={File}");
+                    case Usage.Building:   return "Bldg";
+                    case Usage.Technology: return "Tech";
+                    case Usage.Tooltip:    return "Tips";
+                    case Usage.Ship:       return "Ship";
+                    case Usage.Weapon:     return "Weps";
+                }
+            }
+        }
+
+        public string CreateNameId(string prefix)
+        {
+            string nameId = prefix + "_" + ;
+            return null;
+        }
+    }
+}
