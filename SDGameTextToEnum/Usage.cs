a652ba58826864992a794d2c9fa821b1c034b65c
RedFox <jorma.rebane@gmail.com>
2021-04-07 03:51:33 +0300

    Generate NameId's based on usage

---
+++ SDGameTextToEnum/Usage.cs
@@ -0,0 +1,15 @@
+﻿namespace SDGameTextToEnum
+{
+    public enum Usage
+    {
+        Unknown,    //
+        Building,   // prefix=Bldg
+        Technology, // prefix=Tech
+        Tooltip,    // prefix=Tips
+        Weapon,     // prefix=Weps
+        Module,     // prefix=Modu
+        Artifact,   // prefix=Arti
+        Troop,      // prefix=Troop
+        Races,      // prefix=Race
+    }
+}
