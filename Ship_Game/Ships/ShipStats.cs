cca9716af5a88353fe81b5cf83bd66194f6c6b64
RedFox <jorma.rebane@gmail.com>
2021-04-10 07:43:12 +0300

    Refactored ShipDesignScreen to use a main Ship instead of doing duplicate calculations

--- Ship_Game/Ships/ShipStats.cs
+++ Ship_Game/Ships/ShipStats.cs
@@ -11,7 +11,6 @@ namespace Ship_Game.Ships
     {
         readonly Ship S;
         ShipData Hull;
-        public float Cost;
         public float Mass;
         
         public float Thrust;
@@ -26,6 +25,10 @@ namespace Ship_Game.Ships
 
         public float FTLSpoolTime;
 
+        public float TotalShieldAmplification; // total sum of applied amplifications
+        public float ShieldAmplifyPerShield;
+        public float ShieldMax;
+
         public bool IsStationary => Hull.HullRole == ShipData.RoleName.station
                                  || Hull.HullRole == ShipData.RoleName.platform;
 
@@ -35,15 +38,90 @@ namespace Ship_Game.Ships
             Hull = theShip.shipData;
         }
 
-        public void Update(ShipModule[] modules, Empire e, int level, int surfaceArea, float ordnancePercent)
+        public void UpdateCoreStats()
         {
+            Empire e = S.loyalty;
+            ShipModule[] modules = S.Modules;
             Hull = S.shipData;
-            Cost = GetCost(GetBaseCost(modules), e, IsStationary);
-            Mass = GetMass(modules, e, surfaceArea, ordnancePercent);
 
-            (Thrust,WarpThrust,TurnThrust) = GetThrust(modules);
-            VelocityMax    = GetVelocityMax(Thrust, Mass);
-            TurnRadsPerSec = GetTurnRadsPerSec(TurnThrust, Mass, level);
+            float maxSensorBonus = 0f;
+            S.ActiveInternalSlotCount = 0;
+            S.BonusEMP_Protection     = 0f;
+            S.PowerStoreMax           = 0f;
+            S.PowerFlowMax            = 0f;
+            S.OrdinanceMax            = 0f;
+            S.RepairRate              = 0f;
+            S.CargoSpaceMax           = 0f;
+            S.SensorRange             = 1000f;
+            S.InhibitionRadius        = 0f;
+            S.OrdAddedPerSecond       = 0f;
+            S.HealPerTurn             = 0;
+            S.ECMValue                = 0f;
+            S.hasCommand              = S.IsPlatform;
+            S.TrackingPower           = 0;
+            S.TargetingAccuracy       = 0;
+
+            for (int i = 0; i < modules.Length; i++)
+            {
+                ShipModule module = modules[i];
+                // active internal slots
+                if (module.HasInternalRestrictions && module.Active)
+                    S.ActiveInternalSlotCount += module.XSIZE * module.YSIZE;
+
+                S.RepairRate += module.Active ? module.ActualBonusRepairRate : module.ActualBonusRepairRate / 10; // FB - so destroyed modules with repair wont have full repair rate
+
+                if (module.Active && (module.Powered || module.PowerDraw <= 0f))
+                {
+                    S.hasCommand |= module.IsCommandModule;
+                    S.OrdinanceMax        += module.OrdinanceCapacity;
+                    S.CargoSpaceMax       += module.Cargo_Capacity;
+                    S.BonusEMP_Protection += module.EMP_Protection;
+                    S.OrdAddedPerSecond   += module.OrdnanceAddedPerSecond;
+                    S.HealPerTurn         += module.HealPerTurn;
+                    S.InhibitionRadius  = Math.Max(module.InhibitionRadius, S.InhibitionRadius);
+                    S.SensorRange       = Math.Max(module.SensorRange, S.SensorRange);
+                    maxSensorBonus      = Math.Max(module.SensorBonus, maxSensorBonus);
+                    S.TargetingAccuracy = Math.Max(module.TargetingAccuracy, S.TargetingAccuracy);
+                    S.TrackingPower    += module.TargetTracking;
+                    S.ECMValue = Math.Max(S.ECMValue, module.ECM).Clamped(0f, 1f);
+                    module.AddModuleTypeToList(module.ModuleType, isTrue: module.InstalledWeapon?.isRepairBeam == true, addToList: S.RepairBeams);
+                }
+            }
+            
+            UpdateShieldAmplification();
+            ShieldMax = UpdateShieldPowerMax(ShieldAmplifyPerShield);
+
+            S.shield_max     = ShieldMax;
+            S.NetPower       = Power.Calculate(modules, e);
+            S.PowerStoreMax  = S.NetPower.PowerStoreMax;
+            S.PowerFlowMax   = S.NetPower.PowerFlowMax;
+            S.shield_percent = (100.0 * S.shield_power / S.shield_max.LowerBound(0.1f)).LowerBound(0);
+            S.SensorRange   += maxSensorBonus;
+
+            // Apply modifiers to stats
+            if (S.IsPlatform) S.SensorRange = S.SensorRange.LowerBound(10000);
+            S.SensorRange   *= e.data.SensorModifier;
+            S.SensorRange   *= Hull.Bonuses.SensorModifier;
+            S.CargoSpaceMax *= Hull.Bonuses.CargoModifier;
+            S.RepairRate    += (float)(S.RepairRate * S.Level * 0.05);
+
+            // TODO: are these used? (legacy?)
+            //S.TrackingPower += 1 + e.data.Traits.Militaristic + (S.IsPlatform ? 3 : 0);
+            //S.TargetingAccuracy += 1 + e.data.Traits.Militaristic + (S.IsPlatform ? 3 : 0);
+
+            UpdateMassRelated();
+        }
+
+        public void UpdateMassRelated()
+        {
+            Empire e = S.loyalty;
+            ShipModule[] modules = S.Modules;
+
+            Mass = GetMass(modules, e, S.SurfaceArea, S.OrdnancePercent);
+
+            (Thrust, WarpThrust, TurnThrust) = GetThrust(modules);
+            UpdateVelocityMax();
+            TurnRadsPerSec = GetTurnRadsPerSec(TurnThrust, Mass, S.Level);
 
             MaxFTLSpeed  = GetFTLSpeed(WarpThrust, Mass, e);
             MaxSTLSpeed  = GetSTLSpeed(Thrust, Mass, e);
@@ -55,7 +133,6 @@ namespace Ship_Game.Ships
             float baseCost = 0f;
             for (int i = 0; i < modules.Length; i++)
                 baseCost += modules[i].Cost;
-
             return baseCost;
         }
 
@@ -118,10 +195,12 @@ namespace Ship_Game.Ships
             return radsPerSec.UpperBound(Ship.MaxTurnRadians);
         }
 
-        public float GetVelocityMax(float thrust, float mass)
+        public float UpdateVelocityMax()
         {
-            return thrust / mass;
+            VelocityMax = S.Thrust / S.Mass;
+            return VelocityMax;
         }
+
 
         public float GetFTLSpeed(float warpThrust, float mass, Empire e)
         {
@@ -149,5 +228,52 @@ namespace Ship_Game.Ships
                 spoolTime = 3f;
             return spoolTime;
         }
+
+        // This will also update shield max power of modules if there are amplifiers
+        float UpdateShieldPowerMax(float shieldAmplify)
+        {
+            ShipModule[] amplifiers = S.Amplifiers;
+            ShipModule[] shields = S.Shields;
+            var mainShields = shields.Filter(s => s.ModuleType == ShipModuleType.Shield);
+            if (mainShields.Length == 0)
+                return 0;
+
+            float shieldMax = 0;
+            for (int i = 0; i < shields.Length; i++)
+            {
+                ShipModule shield = shields[i];
+                if (shield.Active && shield.Powered)
+                {
+                    shield.UpdateShieldPowerMax(shieldAmplify);
+                    shieldMax += shield.ActualShieldPowerMax;
+                }
+            }
+            return shieldMax;
+        }
+
+        public void UpdateShieldAmplification()
+        {
+            TotalShieldAmplification = ShieldAmplifyPerShield = 0;
+
+            ShipModule[] amplifiers = S.Amplifiers;
+            if (amplifiers.Length == 0)
+                return;
+
+            var mainShields = S.Shields.Filter(s => s.ModuleType == ShipModuleType.Shield);
+            int numShields = mainShields.Length;
+            if (numShields == 0)
+                return;
+
+            for (int i = 0; i < amplifiers.Length; i++)
+            {
+                ShipModule amplifier = amplifiers[i];
+                if (amplifier.Active && amplifier.Powered)
+                    TotalShieldAmplification += amplifier.AmplifyShields;
+            }
+
+            ShieldAmplifyPerShield = TotalShieldAmplification / numShields;
+        }
+
+        public bool HasMainShields => S.Shields.Any(s => s.ModuleType == ShipModuleType.Shield);
     }
 }
