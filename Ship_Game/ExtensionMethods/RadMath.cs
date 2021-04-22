5351abce90908f3280c93e3e0b4dba651158e4ee
RedFox <jorma.rebane@gmail.com>
2021-04-22 21:47:30 +0300

    Last few corrections

--- Ship_Game/ExtensionMethods/RadMath.cs
+++ Ship_Game/ExtensionMethods/RadMath.cs
@@ -25,6 +25,8 @@ namespace Ship_Game
         public const float RadianToDegree = (float)RadianToDegreeD;
         public const float DegreeToRadian = (float)DegreeToRadianD;
 
+        public const float Deg10AsRads = PI / 18; // 10 degrees, expressed as radians
+        public const float Deg20AsRads = Deg10AsRads*2; // 20 degrees, expressed as radians
 
         const int TableSize = 2000;
         const double TableSizeD = TableSize;
