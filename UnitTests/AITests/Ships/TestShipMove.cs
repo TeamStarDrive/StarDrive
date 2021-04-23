19b3b7606fa695bb6a6de6671864c685e49d4cdf
RedFox <jorma.rebane@gmail.com>
2021-04-23 22:46:44 +0300

    fix: freezing unit tests

--- UnitTests/AITests/Ships/TestShipMove.cs
+++ UnitTests/AITests/Ships/TestShipMove.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xna.Framework;
@@ -34,6 +35,17 @@ namespace UnitTests.AITests.Ships
             return ship;
         }
 
+        void WaitForEngineChangeTo(Ship.MoveState state, Ship ship, Action update)
+        {
+            var sw = Stopwatch.StartNew();
+            while (ship.engineState != state)
+            {
+                update();
+                if (sw.Elapsed.TotalSeconds > 5.0)
+                    throw new TimeoutException($"Timed out while waiting for engine state change");
+            }
+        }
+
         [TestMethod]
         public void MoveShip()
         {
@@ -50,47 +62,47 @@ namespace UnitTests.AITests.Ships
                                         , Ship_Game.AI.AIState.AwaitingOrders, 0, false);
 
             // wait for ship to enter warp
-            while (ship.engineState != Ship.MoveState.Warp)
+            WaitForEngineChangeTo(Ship.MoveState.Warp, ship, () =>
             {
                 ship.AI.DoManualSensorScan(new FixedSimTime(10f));
-                ship.Update(TestSimStep);                
-            }
+                ship.Update(TestSimStep);
+            });
 
             bool sawEnemyShip = false;
 
             // wait for ship to exit warp
-            while (ship.engineState == Ship.MoveState.Warp)
+            WaitForEngineChangeTo(Ship.MoveState.Sublight, ship, () =>
             {
                 Universe.Objects.Update(TestSimStep); // update ships
                 ship.AI.DoManualSensorScan(new FixedSimTime(10f));
                 enemy.AI.DoManualSensorScan(new FixedSimTime(10f));
                 sawEnemyShip |= ship.AI.BadGuysNear;
-            }
+            });
             Assert.IsTrue(sawEnemyShip, "Did not see an enemy while at warp");
             Assert.IsTrue(ship.AI.BadGuysNear, "Bad guys near was not set");
             Assert.IsTrue(ship.Center.InRadius(movePosition, 6000), "final move failed");
 
             // fly back with a combat move. 
             movePosition = Vector2.Zero;
-            ship.AI.OrderMoveDirectlyTo(movePosition, new Vector2(1, 0), true
-                            , Ship_Game.AI.AIState.AwaitingOrders, 0, true);
+            ship.AI.OrderMoveDirectlyTo(movePosition, new Vector2(1, 0), true,
+                                        Ship_Game.AI.AIState.AwaitingOrders, 0, true);
 
             sawEnemyShip = false;
 
             // wait for ship to enter warp
-            while (ship.engineState != Ship.MoveState.Warp)
+            WaitForEngineChangeTo(Ship.MoveState.Warp, ship, () =>
             {
                 ship.Update(TestSimStep);
-            }
+            });
 
             // wait for ship to exit warp
-            while (ship.engineState == Ship.MoveState.Warp)
+            WaitForEngineChangeTo(Ship.MoveState.Sublight, ship, () =>
             {
                 Universe.Objects.Update(TestSimStep); // update ships           
                 ship.AI.StartSensorScan(new FixedSimTime(10f));
                 enemy.AI.StartSensorScan(new FixedSimTime(10f));
                 sawEnemyShip |= ship.AI.BadGuysNear;
-            }
+            });
 
             Assert.IsTrue(ship.Center.InRadius(enemySpawnLocation, 7500), "combat move failed");
         }
