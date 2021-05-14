3915608747cd8538c42b578efc673376039006d2
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-05-13 21:28:42 -0700

    add lock back. clean up some old issue sin pool addeding.

--- Ship_Game/AI/AO.cs
+++ Ship_Game/AI/AO.cs
@@ -398,7 +398,7 @@ namespace Ship_Game.AI
         {
             foreach(var ship in ships)
             {
-                ship.loyalty.EmpireShips.AddToForcePoolNextFame(ship);
+                ship.loyalty.AddShipToManagedPools(ship);
             }
         }
 
