0016dadf4f2447a66eaa3f6ef3befb8e78039177
RedFox <jorma.rebane@gmail.com>
2021-05-20 06:47:00 +0300

    Preparing the new Hull and ShipData Parsers, ran into a major bug with ShipData Grid calculations, ended up being a bug in ShipGridInfo calculation and apparently some broken designs in CA

---
+++ Ship_Game/Ships/ShipData_HullParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game.Ships
+{
+    // NOTE: public variables are SERIALIZED
+    public partial class ShipData
+    {
+        const int CurrentHullVersion = 1;
+
+        static ShipData ParseHull(FileInfo info)
+        {
+            return null;
+        }
+
+        static void ConvertXMLToHull(FileInfo xml, FileInfo outFile)
+        {
+            ShipData hull = ParseXML(xml, isHullDefinition: true);
+        }
+    }
+}
