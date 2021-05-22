650608c47cb78d454966ce9288646b01ddbba88a
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-05-22 00:05:48 -0700

    documentation

--- Ship_Game/Utils/BufferedList.cs
+++ Ship_Game/Utils/BufferedList.cs
@@ -2,12 +2,23 @@
 
 namespace Ship_Game.Utils
 {
+    /// <summary>
+    /// Used to provide a list of items that will not change by adding or removing items from the buffered list.
+    /// </summary>
     public class BufferedList<T> where T : class
     {
         Array<T> PublicList;
         Array<T> VolatileList;
         bool VolatileListChanged;
 
+        /// <summary>
+        /// Gets a snapshot reference of the items in the buffered list.
+        /// The list will never change unless changed outside of the buffered list.
+        /// When iterating this list make sure to use the reference list by using:
+        /// var refList = instance.GetRef()
+        /// foreach(var item in refList)
+        /// It is thread safe with above conditions. 
+        /// </summary>
         public IReadOnlyList<T> GetRef() =>  PublicList;
 
         public BufferedList()
@@ -16,6 +27,10 @@ namespace Ship_Game.Utils
             VolatileList = new Array<T>();
         }
 
+        /// <summary>
+        /// Apply pending modifications and create a new list reference.
+        /// the update is atomic 
+        /// </summary>
         public void Update()
         {
             if (VolatileListChanged)
@@ -26,6 +41,9 @@ namespace Ship_Game.Utils
             }
         }
 
+        /// <summary>
+        /// Not thread safe. all adds must be done on same thread.
+        /// </summary>
         public bool Add(T item)
         {
             bool added   = VolatileList.AddUniqueRef(item);
@@ -33,6 +51,9 @@ namespace Ship_Game.Utils
             return added;
         }
 
+        /// <summary>
+        /// Not thread safe. all removes must be done on same thread.
+        /// </summary>
         public bool Remove(T item)
         {
             bool removed = VolatileList.RemoveRef(item);
