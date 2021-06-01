3fc4da76453a03460d9d89c650443c5c66bfe492
Fizban1978 <gkapulis@gmail.com>
2021-06-01 04:02:19 +0300

    Allow CanBuildAnywhere buildings to prefer non habitable tiles

--- Ship_Game/Universe/SolarBodies/Planet/Planet_Generate.cs
+++ Ship_Game/Universe/SolarBodies/Planet/Planet_Generate.cs
@@ -213,8 +213,8 @@ namespace Ship_Game
 
         private void CreateHomeWorldBuildings()
         {
-            ResourceManager.CreateBuilding(Building.CapitalId).AssignBuildingToTile(this);
-            ResourceManager.CreateBuilding(Building.SpacePortId).AssignBuildingToTile(this);
+            ResourceManager.CreateBuilding(Building.CapitalId).AssignBuildingToTilePlanetCreation(this, out _);
+            ResourceManager.CreateBuilding(Building.SpacePortId).AssignBuildingToTilePlanetCreation(this, out _);
             Storage.Max = BuildingList.Sum(b => b.StorageAdded);
             FoodHere    = Storage.Max;
             ProdHere    = Storage.Max / 2;
