f404cfff69a68879f9a4915fa4be89ed72e6f020
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-06-01 02:07:08 -0700

    broadside rework Take 1. ships will move to keep a certain position to the target. this will result in something like an orbit but if it has the desired position it will only move as the targets position changes where the attackers position should be. a stationary tarker will not cause movement. movement will be in relation to the targets movement. broadside wont change if in range and the target is weak.  it will rotate.

--- Ship_Game/AI/CombatTactics/BroadSides.cs
+++ Ship_Game/AI/CombatTactics/BroadSides.cs
@@ -10,27 +10,40 @@ namespace Ship_Game.AI.CombatTactics
         public BroadSides(ShipAI ai, OrbitPlan.OrbitDirection direction) : base(ai, direction)
         {
         }
-
+        Vector2 InitialMovePoint(float desiredTargetRange) => OwnerTarget.Center + SetInitialOrbitEntryPoint(desiredTargetRange);
+       
         protected override void OverrideCombatValues(FixedSimTime timeStep)
         {
+            DesiredCombatRange = AI.Owner.DesiredCombatRange;//  - AI.Owner.Radius - AI.Target.Radius;
         }
 
         protected override CombatMoveState ExecuteAttack(FixedSimTime timeStep)
         {
-            
-            if (DistanceToTarget > DesiredCombatRange)
+            CombatMoveState moveState = CombatMoveState.Error;
+
+            float maxCombatRange = DesiredCombatRange + Owner.Radius;
+
+            if (DistanceToTarget > maxCombatRange)
             {
-                AI.SubLightMoveTowardsPosition(AI.Target.Center, timeStep);
-                return CombatMoveState.Approach; // we're still way far away from target
+                AI.SubLightMoveTowardsPosition(InitialMovePoint(DesiredCombatRange), timeStep);
+                moveState = CombatMoveState.Approach;
+            }
+            else if (TargetIsMighty(0.5f))
+            {
+                Vector2 initialMovePoint = InitialMovePoint(DesiredCombatRange * 0.75f);
+                AI.SubLightMoveTowardsPosition(initialMovePoint, timeStep);
+                moveState = CombatMoveState.Approach;
+            }
+            else
+            {
+                moveState  = ExecuteBroadside(timeStep);
             }
 
-            if (DistanceToTarget < (DesiredCombatRange * 0.70f)) // within suitable range
-            {
-                UpdateOrbitPos(AI.Target.Center, DesiredCombatRange * 0.95f, timeStep);
-                AI.SubLightMoveTowardsPosition(OrbitPos, timeStep);
-                return CombatMoveState.OrbitInjection;
-            }
+            return moveState;
+        }
 
+        CombatMoveState ExecuteBroadside(FixedSimTime timeStep)
+        {
             Vector2 dir = Owner.Center.DirectionToTarget(AI.Target.Center);
             // when doing broadside to Right, wanted forward dir is 90 degrees left
             // when doing broadside to Left, wanted forward dir is 90 degrees right
@@ -38,6 +51,27 @@ namespace Ship_Game.AI.CombatTactics
             AI.ReverseThrustUntilStopped(timeStep);
             AI.RotateTowardsPosition(Owner.Center + dir, timeStep, 0.02f);
             return CombatMoveState.Maintain;
+        }
+        /// <summary>
+        /// Creates a point that allows a turn friendly orbit injection point. 
+        /// </summary>
+        Vector2 SetInitialOrbitEntryPoint(float wantedRangeToTarget)
+        {
+            Vector2 initialOffset = Vectors.Up * wantedRangeToTarget;
+            if (OwnerTarget != null)
+            {
+                Vector2 initialDirection = OwnerTarget.Center.DirectionToTarget(Owner.Center);
+                initialOffset = initialDirection * DesiredCombatRange;
+
+                // desiredCombatRange * 0.25... need a turn friendly value here. 
+                if (Direction == OrbitDirection.Left)
+                    initialOffset += initialDirection.LeftVector() * (wantedRangeToTarget * 0.25f);
+                else
+                    initialOffset += initialDirection.RightVector() * (wantedRangeToTarget * 0.25f);
+
+                ForceOrbitOffSet(initialOffset);
+            }
+            return initialOffset;
         }
     }
 }
