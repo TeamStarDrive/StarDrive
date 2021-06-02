ad09a699aaa9712b9e8228cc53d3d5d74584149b
RedFox <jorma.rebane@gmail.com>
2021-06-02 15:46:40 +0300

    fix: unfortunate error in file copy

--- CopyGameContent.ps1
+++ CopyGameContent.ps1
@@ -1,7 +1,7 @@
 $targetdir=$Args[0] # "C:\Projects\BlackBox\UnitTests\bin\Release\"
 $solutiondir=$Args[1] # "C:\Projects\BlackBox\"
 
-$contentSrc = $targetdir + "Content"
+$contentSrc = $solutiondir + "Content"
 $contentDst = $solutiondir + "StarDrive\Content"
 echo "Copying StarDrive/Content from $contentSrc to $contentDst"
 # RoboCopy
@@ -11,7 +11,7 @@ echo "Copying StarDrive/Content from $contentSrc to $contentDst"
 # /MT:16=multi-threaded-copy,16-threads
 robocopy "$contentSrc" "$contentDst" /e /xo  /NFL /NDL /NJH /nc /ns /NP /MT:16
 
-$modsSrc = $targetdir + "Mods"
+$modsSrc = $solutiondir + "Mods"
 $modsDst = $solutiondir + "StarDrive\Mods"
 echo "Copying StarDrive/Mods from $modsSrc to $modsDst"
 robocopy "$modsSrc" "$modsDst" /e /xo  /NFL /NDL /NJH /nc /ns /np /MT:16
