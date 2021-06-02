be8a5209fbf4718ce94dae361c4060bc705696ff
RedFox <jorma.rebane@gmail.com>
2021-06-02 15:40:22 +0300

    content refactor: automatically copy all files using CopyGameContent.ps1 script

---
+++ CopyGameContent.ps1
@@ -0,0 +1,17 @@
+$targetdir=$Args[0] # "C:\Projects\BlackBox\UnitTests\bin\Release\"
+$solutiondir=$Args[1] # "C:\Projects\BlackBox\"
+
+$contentSrc = $targetdir + "Content"
+$contentDst = $solutiondir + "StarDrive\Content"
+echo "Copying StarDrive/Content from $contentSrc to $contentDst"
+# RoboCopy
+# /e=recursive /xo=eclude-older-files(copy-if-newer)
+# /NFL=no-filename-logs /NDL=no-dirname-logs /NJH=no-job-header
+# /nc=no-fileclass-logs /ns=no-filesize-logs /np=no-progress
+# /MT:16=multi-threaded-copy,16-threads
+robocopy "$contentSrc" "$contentDst" /e /xo  /NFL /NDL /NJH /nc /ns /NP /MT:16
+
+$modsSrc = $targetdir + "Mods"
+$modsDst = $solutiondir + "StarDrive\Mods"
+echo "Copying StarDrive/Mods from $modsSrc to $modsDst"
+robocopy "$modsSrc" "$modsDst" /e /xo  /NFL /NDL /NJH /nc /ns /np /MT:16
