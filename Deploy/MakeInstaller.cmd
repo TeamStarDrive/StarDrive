9d869f3149ed4f548367490ed31d386074f6b12c
RedFox <jorma.rebane@gmail.com>
2021-06-03 01:24:34 +0300

    Attempting to figure out why AppVeyor hates me

--- Deploy/MakeInstaller.cmd
+++ Deploy/MakeInstaller.cmd
@@ -4,13 +4,13 @@ echo APPVEYOR_BUILD_VERSION=%APPVEYOR_BUILD_VERSION%
 
 :: cd to given parameter if it exists
 if not [%1]==[] (
-    echo "Changing dir to: %1"
+    echo CurrentDir=%cd% ChangeDirTo=%1
     cd %1
 )
 
 :: Assume MakeInstaller.cmd is called from C:\Projects\BlackBox folder
 if not exist "Deploy/NSIS/makensis.exe" (
-    echo "MakeInstaller.cmd must be executed with WorkingDir=Projects\BlackBox\"
+    echo MakeInstaller.cmd must be executed with WorkingDir=Projects\BlackBox\
     exit /b %errorlevel%
 )
 
@@ -21,4 +21,5 @@ if not exist Deploy\upload mkdir Deploy\upload
 ::del /f Deploy\upload\sd.7z
 ::Deploy\7-Zip\7z.exe A Deploy\upload\sd.7z @"Deploy\include.txt"
 
+echo "MakeNSIS Deploy/BlackBox-Mars.nsi"
 "Deploy/NSIS/makensis.exe" /V3 /DVERSION=%APPVEYOR_BUILD_VERSION% /DSOURCE_DIR=%source% Deploy/BlackBox-Mars.nsi
