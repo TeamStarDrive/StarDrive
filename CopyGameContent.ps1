5eddd0a65c69480587dddcb170b08d97ad57af41
RedFox <jorma.rebane@gmail.com>
2021-06-02 18:12:20 +0300

    fix: CopyGameContent not working correctly in CI

--- CopyGameContent.ps1
+++ CopyGameContent.ps1
@@ -1,17 +1,24 @@
-$targetdir=$Args[0] # "C:\Projects\BlackBox\UnitTests\bin\Release\"
-$solutiondir=$Args[1] # "C:\Projects\BlackBox\"
+$solutiondir=$Args[0] # "C:\Projects\BlackBox\"
+$targetdir=$Args[1] # "C:\Projects\BlackBox\UnitTests\bin\Release\"
+$copymods=$Args[2]
 
 $contentSrc = $solutiondir + "Content"
-$contentDst = $solutiondir + "StarDrive\Content"
-echo "Copying StarDrive/Content from $contentSrc to $contentDst"
-# RoboCopy
+$contentDst = $targetdir + "Content"
+echo "Copying $contentSrc to $contentDst"
+# RoboCopy "source" "destination" /options...
 # /e=recursive /xo=eclude-older-files(copy-if-newer)
 # /NFL=no-filename-logs /NDL=no-dirname-logs /NJH=no-job-header
 # /nc=no-fileclass-logs /ns=no-filesize-logs /np=no-progress
 # /MT:16=multi-threaded-copy,16-threads
 robocopy "$contentSrc" "$contentDst" /e /xo  /NFL /NDL /NJH /nc /ns /NP /MT:16
 
-$modsSrc = $solutiondir + "Mods"
-$modsDst = $solutiondir + "StarDrive\Mods"
-echo "Copying StarDrive/Mods from $modsSrc to $modsDst"
-robocopy "$modsSrc" "$modsDst" /e /xo  /NFL /NDL /NJH /nc /ns /np /MT:16
+if ($copymods -eq "COPYMODS")
+{
+    $modsSrc = $solutiondir + "Mods"
+    $modsDst = $targetdir + "Mods"
+    if (Test-Path -Path "$modsSrc")
+    {
+        echo "Copying $modsSrc to $modsDst"
+        robocopy "$modsSrc" "$modsDst" /e /xo  /NFL /NDL /NJH /nc /ns /np /MT:16
+    }
+}
