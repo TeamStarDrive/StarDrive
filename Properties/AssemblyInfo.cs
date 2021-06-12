914e1ab69eea14b0fa639e4a99a304980406a9e4
RedFox <jorma.rebane@gmail.com>
2021-06-12 13:08:17 +0300

    Set base version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b38aad3b-18b8-41a8-b758-0e5614dafc49")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
-[assembly: AssemblyInformationalVersion("develop_9221 ec9d3f1f0")]
+[assembly: AssemblyInformationalVersion("1.20.12000 develop-latest")]
