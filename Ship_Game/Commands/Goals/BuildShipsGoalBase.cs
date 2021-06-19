06f56a87c3badbb2e75a95943b301d6a10aa94b6
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-06-19 16:39:38 -0700

    add warship rush based on threat level. add warship queue priority based on the threat level.
    create some variables for threatlevel, economic stability, available freight capacity, and a few others.

--- Ship_Game/Commands/Goals/BuildShipsGoalBase.cs
+++ Ship_Game/Commands/Goals/BuildShipsGoalBase.cs
@@ -55,7 +55,7 @@ namespace Ship_Game.Commands.Goals
             return false;
         }
 
-        protected GoalStep FindPlanetToBuildAt(SpacePortType portType)
+        protected GoalStep TryBuildShip(SpacePortType portType)
         {
             if (!GetShipTemplate(ToBuildUID, out Ship template))
                 return GoalStep.GoalFailed;
@@ -64,6 +64,12 @@ namespace Ship_Game.Commands.Goals
                 return GoalStep.TryAgain;
 
             planet.Construction.Enqueue(template, this);
+
+            float threatLevel = empire.GetEmpireAI().ThreatLevel.UpperBound(1);
+            int atPeace = (int)(20 - 20 * threatLevel);
+            int atWar   = (int)(5 - 5 * threatLevel);
+
+            planet.Construction.PrioritizeShip(template, atPeace, atWar);
             return GoalStep.GoToNextStep;
         }
     }
