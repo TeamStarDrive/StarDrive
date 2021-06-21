b8c57786f88603c3a3f8b4beef2a46677a4dea86
Fizban1978 <gkapulis@gmail.com>
2021-06-21 10:50:01 +0300

    do not prioritize offensive ships

--- Ship_Game/Commands/Goals/BuildShipsGoalBase.cs
+++ Ship_Game/Commands/Goals/BuildShipsGoalBase.cs
@@ -65,11 +65,12 @@ namespace Ship_Game.Commands.Goals
 
             planet.Construction.Enqueue(template, this);
 
+            /* FB: never prioritize ships. It keeps piling up on top of everything else needed
+            // like troops, buildings
             float threatLevel = empire.GetEmpireAI().ThreatLevel.UpperBound(1);
             int atPeace = (int)(20 - 20 * threatLevel);
             int atWar   = (int)(5 - 5 * threatLevel);
-
-            planet.Construction.PrioritizeShip(template, atPeace, atWar);
+            planet.Construction.PrioritizeShip(template, atPeace, atWar);*/
             return GoalStep.GoToNextStep;
         }
     }
