c107f15c4c14f58d81928c07c2529033e3ec65d6
Fizban1978 <gkapulis@gmail.com>
2021-06-27 03:15:45 +0300

    remove a break to get more spread of techs

--- Ship_Game/AI/Research/ShipTechLineFocusing.cs
+++ Ship_Game/AI/Research/ShipTechLineFocusing.cs
@@ -397,8 +397,6 @@ namespace Ship_Game.AI.Research
                     {
                         foreach (var techName in researchableTechs)
                             goodShipTechs.Add(techName);
-
-                        break;
                     }
                 }
             }
