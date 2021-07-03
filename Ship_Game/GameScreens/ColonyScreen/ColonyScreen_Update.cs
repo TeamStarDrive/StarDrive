7cd33d5e44563544ef7f7d567b249c7c0c299779
Fizban1978 <gkapulis@gmail.com>
2021-07-03 14:02:21 +0300

    initial UI

--- Ship_Game/GameScreens/ColonyScreen/ColonyScreen_Update.cs
+++ Ship_Game/GameScreens/ColonyScreen/ColonyScreen_Update.cs
@@ -7,12 +7,20 @@ namespace Ship_Game
 {
     public partial class ColonyScreen
     {
-        
         public override void Update(float elapsedTime)
         {
             P.UpdateIncomes(false);
             UpdateBuildAndConstructLists(elapsedTime);
+            UpdateTerraformTab();
             base.Update(elapsedTime);
+        }
+
+        void UpdateTerraformTab()
+        {
+            if (P.Owner.data.Traits.TerraformingLevel < 1)
+                return;
+
+            TerraformTitle.Visible = PFacilities.SelectedIndex == 2;
         }
     }
 }
