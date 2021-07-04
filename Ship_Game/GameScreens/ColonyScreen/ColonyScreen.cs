d9c0e2e60e47f926494c95a6326cb06860a5ba14
Fizban1978 <gkapulis@gmail.com>
2021-07-04 21:34:58 +0300

    text

--- Ship_Game/GameScreens/ColonyScreen/ColonyScreen.cs
+++ Ship_Game/GameScreens/ColonyScreen/ColonyScreen.cs
@@ -298,7 +298,7 @@ namespace Ship_Game
             {
                 slider            = Slider(rect, text, min, max, value);
                 slider.Visible    = false;
-                slider.ZeroString = Localizer.Token(4329);
+                slider.ZeroString = Localizer.Token(GameText.Automatic);
                 slider.Tip        = tip;
             }
         }
@@ -314,73 +314,73 @@ namespace Ship_Game
             float indentTradeAmount = indent + barWidth + 5;
             float indentSlider      = indentTradeAmount + 60;
 
-            AddLabel(ref TradeTitle, pos, Localizer.Token(4332), LowRes ? Font14 : Font20, Color.White);
+            AddLabel(ref TradeTitle, pos, Localizer.Token(GameText.ColonyTrade), LowRes ? Font14 : Font20, Color.White);
 
             Vector2 incomingTitlePos = new Vector2(pos.X, pos.Y + spacing * (LowRes ? 1 : 1.5f));
-            AddLabel(ref IncomingTradeTitle, incomingTitlePos, Localizer.Token(4330), font, Color.Gray);
+            AddLabel(ref IncomingTradeTitle, incomingTitlePos, Localizer.Token(GameText.IncomingFreighters), font, Color.Gray);
 
             Vector2 manualImportTitlePos = new Vector2(pos.X + indentSlider - 10, incomingTitlePos.Y);
-            AddLabel(ref ManualImportTitle, manualImportTitlePos, Localizer.Token(4333), font, Color.Gray);
+            AddLabel(ref ManualImportTitle, manualImportTitlePos, Localizer.Token(GameText.ManualImport), font, Color.Gray);
 
             // Incoming food
             Vector2 incomingFoodPos = new Vector2(pos.X, incomingTitlePos.Y + spacing + 3);
-            AddPanel(ref IncomingFoodPanel, incomingFoodPos, "NewUI/icon_food", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref IncomingFoodPanel, incomingFoodPos, "NewUI/icon_food", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle incomingFoodRect = new Rectangle((int)(incomingFoodPos.X + indent), (int)incomingFoodPos.Y, barWidth, 20);
             AddProgressBar(ref IncomingFoodBar, incomingFoodRect, P.FoodImportSlots, "green");
             Vector2 incomingFoodAmountPos = new Vector2(pos.X + indentTradeAmount, incomingFoodPos.Y + (LowRes ? 0 : 2));
             AddLabel(ref IncomingFoodAmount, incomingFoodAmountPos, "", Font8, Color.White);
             Rectangle importFoodSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(incomingFoodPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ImportFoodSlotSlider, importFoodSlotsRect, "", 0, 20, P.ManualFoodImportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ImportFoodSlotSlider, importFoodSlotsRect, "", 0, 20, P.ManualFoodImportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
 
             // Incoming Prod
             Vector2 incomingProdPos = new Vector2(pos.X, incomingFoodPos.Y + spacing);
-            AddPanel(ref IncomingProdPanel, incomingProdPos, "NewUI/icon_production", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref IncomingProdPanel, incomingProdPos, "NewUI/icon_production", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle incomingProdRect = new Rectangle((int)(incomingProdPos.X + indent), (int)incomingProdPos.Y, barWidth, 20);
             AddProgressBar(ref IncomingProdBar, incomingProdRect, P.ProdImportSlots, "brown");
             Vector2 incomingProdAmountPos = new Vector2(pos.X + indentTradeAmount, incomingProdPos.Y + (LowRes ? 0 : 2));
             AddLabel(ref IncomingProdAmount, incomingProdAmountPos, "", Font8, Color.White);
             Rectangle importProdSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(incomingProdPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ImportProdSlotSlider, importProdSlotsRect, "", 0, 20, P.ManualProdImportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ImportProdSlotSlider, importProdSlotsRect, "", 0, 20, P.ManualProdImportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
 
             // Incoming Colonists
             Vector2 incomingColoPos = new Vector2(pos.X, incomingProdPos.Y + spacing);
-            AddPanel(ref IncomingColoPanel, incomingColoPos, "UI/icon_pop", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref IncomingColoPanel, incomingColoPos, "UI/icon_pop", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle incomingColoRect = new Rectangle((int)(incomingColoPos.X + indent), (int)incomingColoPos.Y, barWidth, 20);
             AddProgressBar(ref IncomingColoBar, incomingColoRect, P.ColonistsImportSlots, "blue");
             Vector2 incomingColoAmountPos = new Vector2(pos.X + indentTradeAmount, incomingColoPos.Y + (LowRes ? 0 : 2));
             AddLabel(ref IncomingColoAmount, incomingColoAmountPos, "", Font8, Color.White);
             Rectangle importColoSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(incomingColoPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ImportColoSlotSlider, importColoSlotsRect, "", 0, 20, P.ManualColoImportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ImportColoSlotSlider, importColoSlotsRect, "", 0, 20, P.ManualColoImportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
 
             Vector2 outgoingTitlePos = new Vector2(pos.X, incomingColoAmountPos.Y + spacing * (LowRes ? 1 : 1.5f));
-            AddLabel(ref OutgoingTradeTitle, outgoingTitlePos, Localizer.Token(4331), font, Color.Gray);
+            AddLabel(ref OutgoingTradeTitle, outgoingTitlePos, Localizer.Token(GameText.OutgoingFreighters), font, Color.Gray);
 
             Vector2 manualExportTitlePos = new Vector2(pos.X + indentSlider - 10, outgoingTitlePos.Y);
-            AddLabel(ref ManualExportTitle, manualExportTitlePos, Localizer.Token(4334), font, Color.Gray);
+            AddLabel(ref ManualExportTitle, manualExportTitlePos, Localizer.Token(GameText.ManualExport), font, Color.Gray);
 
             // Outgoing food
             Vector2 outgoingFoodPos = new Vector2(pos.X, outgoingTitlePos.Y + spacing + 3);
-            AddPanel(ref OutgoingFoodPanel, outgoingFoodPos, "NewUI/icon_food", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref OutgoingFoodPanel, outgoingFoodPos, "NewUI/icon_food", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle outgoingFoodRect = new Rectangle((int)(outgoingFoodPos.X + indent), (int)outgoingFoodPos.Y, barWidth, 20);
             AddProgressBar(ref OutgoingFoodBar, outgoingFoodRect, P.FoodExportSlots, "green");
             Rectangle exportFoodSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(outgoingFoodPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ExportFoodSlotSlider, exportFoodSlotsRect, "", 0, 20, P.ManualFoodExportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ExportFoodSlotSlider, exportFoodSlotsRect, "", 0, 20, P.ManualFoodExportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
 
             // Outgoing Prod
             Vector2 outgoingProdPos = new Vector2(pos.X, outgoingFoodPos.Y + spacing);
-            AddPanel(ref OutgoingProdPanel, outgoingProdPos, "NewUI/icon_production", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref OutgoingProdPanel, outgoingProdPos, "NewUI/icon_production", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle outgoingProdRect = new Rectangle((int)(outgoingProdPos.X + indent), (int)outgoingProdPos.Y, barWidth, 20);
             AddProgressBar(ref OutgoingProdBar, outgoingProdRect, P.ProdExportSlots, "brown");
             Rectangle exportProdSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(outgoingProdPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ExportProdSlotSlider, exportProdSlotsRect, "", 0, 20, P.ManualProdExportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ExportProdSlotSlider, exportProdSlotsRect, "", 0, 20, P.ManualProdExportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
 
             // Outgoing Colonists
             Vector2 outgoingColoPos = new Vector2(pos.X, outgoingProdPos.Y + spacing);
-            AddPanel(ref OutgoingColoPanel, outgoingColoPos, "UI/icon_pop", font.LineSpacing, Localizer.Token(4335));
+            AddPanel(ref OutgoingColoPanel, outgoingColoPos, "UI/icon_pop", font.LineSpacing, Localizer.Token(GameText.IncomingOutGoingTip));
             Rectangle outgoingColoRect = new Rectangle((int)(outgoingColoPos.X + indent), (int)outgoingColoPos.Y, barWidth, 20);
             AddProgressBar(ref OutgoingColoBar, outgoingColoRect, P.ColonistsExportSlots, "blue");
             Rectangle exportColoSlotsRect = new Rectangle((int)(pos.X + indentSlider), (int)(outgoingColoPos.Y-12), sliderWidth, sliderSize);
-            AddUiSlider(ref ExportColoSlotSlider, exportColoSlotsRect, "", 0, 20, P.ManualColoExportSlots, Localizer.Token(4336));
+            AddUiSlider(ref ExportColoSlotSlider, exportColoSlotsRect, "", 0, 20, P.ManualColoExportSlots, Localizer.Token(GameText.ManualTradeSlotTip));
         }
 
         void CreateTerraformingDetails(Vector2 pos)
