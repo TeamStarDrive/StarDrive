dad08fce17d26a55800e5ea83d9066625ba8623f
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-07-05 14:00:38 -0700

    Simplify research priorities yaml. make yaml per empire and/or all empire specific add comments to yaml. Add simple test to check yaml loaded.

---
+++ UnitTests/Technologies/Research.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ship_Game;
+using Ship_Game.AI.Research;
+
+namespace UnitTests.Technologies
+{
+    [TestClass]
+    public class TestResearch : StarDriveTest
+    {
+        public TestResearch()
+        {
+            CreateGameInstance();
+            LoadTechContent();
+            CreateUniverseAndPlayerEmpire();
+        }
+
+        [TestMethod]
+        public void LoadResearchPriorities()
+        {
+            var researchMods = new ResearchOptions();
+            researchMods.LoadResearchOptions(Player);
+            Assert.IsTrue(researchMods.Count() > 0);
+        }
+    }
+}
