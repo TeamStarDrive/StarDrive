d59d92ac420ca6fe286eb6689961808d12a7af0d
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-07-05 00:01:50 -0700

    add yaml load to research settings.

--- Ship_Game/AI/Research/ResearchOptions.cs
+++ Ship_Game/AI/Research/ResearchOptions.cs
@@ -7,17 +7,23 @@ namespace Ship_Game.AI.Research
 {
     public class ResearchOptions
     {
-        Map<TechnologyType, float> TechTypeMods    = new Map<TechnologyType, float>();
-        Map<ShipCosts, float> ShipMods             = new Map<ShipCosts, float>();
-        Map<ResearchArea, float> Priority          = new Map<ResearchArea, float>();
+        static readonly Map<TechnologyType, float> TechTypeMods = new Map<TechnologyType, float>();
+        static readonly Map<ShipCosts, float> ShipMods          = new Map<ShipCosts, float>();
+        static readonly Map<ResearchArea, float> Priority       = new Map<ResearchArea, float>();
 
+        /// <summary>
+        /// Used for loading yamls
+        /// </summary>
         public enum ResearchMods
         {
-            TechUID,
             TechType,
-            ShipCost
+            ShipCost,
+            AreaPriority
         }
 
+        /// <summary>
+        /// ShipCost cost multiplier for various ship categories.
+        /// </summary>
         public enum ShipCosts
         {
             None,
@@ -30,13 +36,21 @@ namespace Ship_Game.AI.Research
             AllHulls,
             Orbitals,
             GroundCombat,
+            /// <summary>
+            /// Add a random to the tech cost of ship for the ship picker.
+            /// this will be a random of -techCost * randomize tp +techCost * randomize
+            /// </summary>
             Randomize,
             /// <summary>
-            /// How much bonus a similar gives.
+            /// How much bonus a similar tech gives in ship cost.
             /// </summary>
             LineFocusIntensity
         }
 
+        /// <summary>
+        /// When choosing a tech category how much extra bonus will be given to research it.
+        /// the value of 1 adds 1-100 plus empire racial default.
+        /// </summary>
         public enum ResearchArea
         {
             ShipTech,
@@ -51,27 +65,30 @@ namespace Ship_Game.AI.Research
         public ResearchOptions()
         {
             //Set default values.
-            ////ShipCosts
-            ShipMods[ShipCosts.Carrier]      = 0.95f;
-            ShipMods[ShipCosts.Bomber]       = 0.95f;
-            ShipMods[ShipCosts.TroopShip]    = 0.95f;
-            ShipMods[ShipCosts.Support]      = 0.95f;
-            ShipMods[ShipCosts.ColonyShip]   = 2f;
-            ShipMods[ShipCosts.Freighter]    = 2f;
-            ShipMods[ShipCosts.AllHulls]     = 1;
-            ShipMods[ShipCosts.Orbitals]     = 2f;
-            ShipMods[ShipCosts.GroundCombat] = 0.95f;
-            /// Random= TechCost * randomizer
-            /// TechCost += randomBetween -Random and Random.
-            ShipMods[ShipCosts.Randomize]    = 0f;
+            //ShipCosts
+            ShipMods.Clear();
+            ShipMods[ShipCosts.Carrier]            = 0.95f;
+            ShipMods[ShipCosts.Bomber]             = 0.95f;
+            ShipMods[ShipCosts.TroopShip]          = 0.95f;
+            ShipMods[ShipCosts.Support]            = 0.95f;
+            ShipMods[ShipCosts.ColonyShip]         = 2f;
+            ShipMods[ShipCosts.Freighter]          = 2f;
+            ShipMods[ShipCosts.AllHulls]           = 1;
+            ShipMods[ShipCosts.Orbitals]           = 2f;
+            ShipMods[ShipCosts.GroundCombat]       = 0.95f;
+            /// TechCost * random = randomizer
+            /// random is -randomizer to +randomizer
+            ShipMods[ShipCosts.Randomize]          = 0f;
             ShipMods[ShipCosts.LineFocusIntensity] = 1;
 
             //TechUIDS
 
             //TechTypes
+            TechTypeMods.Clear();
             TechTypeMods[TechnologyType.ShipHull] = 1f;
 
             //Tech Priorities
+            Priority.Clear();
             Priority[ResearchArea.Colonization] = 1;
             Priority[ResearchArea.Economic]     = 1;
             Priority[ResearchArea.GroundCombat] = 0.5f;
@@ -79,12 +96,13 @@ namespace Ship_Game.AI.Research
             Priority[ResearchArea.General]      = 1;
             Priority[ResearchArea.Research]     = 1;
             Priority[ResearchArea.ShipTech]     = 1;
+            LoadResearchOptions();
         }
-        
+
         // This may be kinda slow...
         float GetAnyTypeMod(TechEntry tech)
         {
-            foreach(KeyValuePair<TechnologyType, float> kv in TechTypeMods)
+            foreach (KeyValuePair<TechnologyType, float> kv in TechTypeMods)
             {
                 TechnologyType techType = kv.Key;
                 if (tech.IsTechnologyType(techType))
@@ -95,6 +113,9 @@ namespace Ship_Game.AI.Research
             return 1;
         }
 
+        /// <summary>
+        /// tech type cost updater modifier.
+        /// </summary>
         public float CostMultiplier(TechEntry tech)
         {
             float lowPriorityMultiplier = tech.Tech.LowPriorityCostMultiplier.Clamped(0.1f, 10);
@@ -102,6 +123,9 @@ namespace Ship_Game.AI.Research
             return lowPriorityMultiplier * typeMod;
         }
 
+        /// <summary>
+        /// Ship cost multiplies.
+        /// </summary>
         public float CostMultiplier(ShipCosts costType)
         {
             if (ShipMods.TryGetValue(costType, out float modifier))
@@ -109,6 +133,9 @@ namespace Ship_Game.AI.Research
             return 1;
         }
 
+        /// <summary>
+        /// Research category priority bonus
+        /// </summary>/returns>
         public float GetPriority(ResearchArea priorities)
         {
             if (Priority.TryGetValue(priorities, out float modifier))
@@ -120,8 +147,45 @@ namespace Ship_Game.AI.Research
         static void LoadResearchOptions()
         {
             GameLoadingScreen.SetStatus("ResearchOptions");
-            
-            //Array<Research> researchMods = YamlParser.DeserializeArray<Research>("Research.yaml");
+
+            Array<ResearchSettings> researchMods = YamlParser.DeserializeArray<ResearchSettings>("Research.yaml");
+
+            foreach (var area in researchMods)
+            {
+                switch (area.ResearchArea)
+                {
+                    case ResearchMods.TechType: area.ConvertTo(TechTypeMods); break;
+                    case ResearchMods.ShipCost: area.ConvertTo(ShipMods);     break;
+                    case ResearchMods.AreaPriority: area.ConvertTo(Priority); break;
+                    default: throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        [StarDataType]
+        public class ResearchSettings
+        {
+            [StarData] public readonly ResearchMods ResearchArea;
+            [StarData] public readonly Setting[] AreaSettings;
+
+            public void ConvertTo<TEnum>(Map<TEnum, float> addTo) where TEnum : struct
+            {
+                foreach (var item in AreaSettings)
+                {
+                    TEnum techType;
+                    if (Enum.TryParse(item.Key, out techType))
+                        addTo[techType] = item.Value;
+                }
+            }
+
+            public override string ToString() => $"Research Area {ResearchArea} ItemCount= {AreaSettings.Length}";
+        }
+
+        [StarDataType]
+        public class Setting
+        {
+            [StarData] public string Key;
+            [StarData] public float Value;
         }
     }
 }
