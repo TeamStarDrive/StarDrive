9a910fefcb49f4286217b888d5fb3a07408600b4
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-07-11 23:01:56 -0700

    Remove all the extra stuff. split planets into above and blelow average. above average gets a higher ratio to of the build budget. round all below average planets to the nearest 0.5f.
    this wil give some over build but generally pretty small. it will keep pretty close to the budget in most cases.
    
    its fast simple and dont need to worry about bugs in the governor system or any record keeping to avoid scrap loops.

--- Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
+++ Ship_Game/AI/EmpireAI/EmpireAI.RunEconomicPlanner.cs
@@ -14,14 +14,14 @@ namespace Ship_Game.AI
     /// calculate threat to empire.
     /// set budgets for areas.
     /// some areas like civilian freighter budget is not restrictive. It is used to allow the empire to track the money spent in that area.
-    /// most budget areas are not hard restricted. There will be some wiggle room or outright relying on stored money. 
+    /// most budget areas are not hard restricted. There will be some wiggle room or outright relying on stored money.
     /// </summary>
     public sealed partial class EmpireAI
     {
         /// <summary>
         /// value from 0 to 1+
         /// This represents the overall threat to the empire. It is calculated from the EmpireRiskAssessment class.
-        /// it currently looks at expansion threat, border threat, and general threat from each each empire. 
+        /// it currently looks at expansion threat, border threat, and general threat from each each empire.
         /// </summary>
         public float ThreatLevel { get; private set; }    = 0;
         public float EconomicThreat { get; private set; } = 0;
@@ -32,22 +32,11 @@ namespace Ship_Game.AI
         /// </summary>
         public bool SafeToRush => CreditRating > 0.75f;
 
-        /// <summary>
-        /// Stores Maintenance saved by scrapping this turn;
-        /// </summary>
-        public float MaintSavedByBuildingScrappedThisTurn = 0;
-
-        /// <summary>
-        /// How much building cost the empire has subsidized this turn.
-        /// </summary>
-        public float BuildingSupportedByEmpireThisTurn = 0;
-        public float BuildingSupportedByEmpireLastTurn = 0;
-
         // Empire spaceDefensive Reserves high enough to support fractional build budgets
         public bool EmpireCanSupportSpcDefense => OwnerEmpire.data.DefenseBudget > OwnerEmpire.TotalOrbitalMaintenance && CreditRating > 0.90f;
 
         /// <summary>
-        /// This is the budgeted amount of money that will be available to empire looking over 20 years.  
+        /// This is the budgeted amount of money that will be available to empire looking over 20 years.
         /// </summary>
         public float ProjectedMoney { get; private set; } = 0;
 
@@ -55,7 +44,7 @@ namespace Ship_Game.AI
         /// <summary>
         /// This a ratio of projectedMoney and the normalized money then multiplied by 2 then add 1 - taxrate
         /// then the whole thing divided by 3. This puts a large emphasis on money goal to money ratio
-        /// but a high tax will greatly effect the result. 
+        /// but a high tax will greatly effect the result.
         /// </summary>
         public float CreditRating
         {
@@ -69,25 +58,22 @@ namespace Ship_Game.AI
         }
 
         /// <summary>
-        /// Horribly inefficient. best not to use but i havent found a way to get a more accurate tax for amount
+        /// This is not very accurate. Its around 20% off in either direction.
+        /// It is a fast estimate and close enough with some hacks. 
         /// </summary>
         private float FindTaxRateToReturnAmount(float amount)
         {
-            float spending = OwnerEmpire.BuildingAndShipMaint + OwnerEmpire.TroopCostOnPlanets;
-            float initialTaxRate = OwnerEmpire.data.TaxRate;
+            if (amount == 0)
+                return 0;
 
             for (int i = 1; i < 100; i++)
             {
                 float taxRate = i / 100f;
-                OwnerEmpire.data.TaxRate = taxRate;
-                OwnerEmpire.UpdateNetPlanetIncomes();
 
-                float amountMade = OwnerEmpire.NetIncome - amount;
+                float amountMade = OwnerEmpire.MaximumIncome * taxRate;
 
-                if (amountMade >0)
+                if (amountMade >= amount)
                 {
-                    if (initialTaxRate > taxRate)
-                        return initialTaxRate - 0.01f;
                     return taxRate;
                 }
             }
@@ -96,10 +82,6 @@ namespace Ship_Game.AI
 
         public void RunEconomicPlanner(bool fromSave = false)
         {
-            MaintSavedByBuildingScrappedThisTurn = 0;
-            BuildingSupportedByEmpireLastTurn = BuildingSupportedByEmpireThisTurn;
-            BuildingSupportedByEmpireThisTurn    = 0;
-
             float money = OwnerEmpire.Money;
             if (!fromSave)
             {
@@ -110,17 +92,17 @@ namespace Ship_Game.AI
             normalizedBudget = money < 0 ? money : normalizedBudget;
 
             float treasuryGoal = TreasuryGoal(normalizedBudget);
-            ProjectedMoney = treasuryGoal / (OwnerEmpire.isPlayer ? 1 : 2);
-            AutoSetTaxes(ProjectedMoney, normalizedBudget);
+            ProjectedMoney = treasuryGoal;
+            AutoSetTaxes(ProjectedMoney, money);
 
-            // gamestate attempts to increase the budget if there are wars or lack of some resources.  
-            // its primarily geared at ship building. 
+            // gamestate attempts to increase the budget if there are wars or lack of some resources.
+            // its primarily geared at ship building.
             float riskLimit = (CreditRating * 2).Clamped(0.1f, 2);
             float gameState = ThreatLevel = GetRisk(riskLimit);
 
-            // the values below are now weights to adjust the budget areas. 
+            // the values below are now weights to adjust the budget areas.
             float defense = BudgetSettings.GetBudgetFor(BudgetAreas.Defense);
-            float SSP     = BudgetSettings.GetBudgetFor(BudgetAreas.SSP); 
+            float SSP     = BudgetSettings.GetBudgetFor(BudgetAreas.SSP);
             float build   = BudgetSettings.GetBudgetFor(BudgetAreas.Build);
             float spy     = BudgetSettings.GetBudgetFor(BudgetAreas.Spy);
             float colony  = BudgetSettings.GetBudgetFor(BudgetAreas.Colony);
@@ -185,11 +167,11 @@ namespace Ship_Game.AI
             float trustworthiness = OwnerEmpire.data.DiplomaticPersonality?.Trustworthiness ?? 100;
             trustworthiness      /= 100f;
             float militaryRatio   = OwnerEmpire.Research.Strategy.MilitaryRatio;
-            
-            // it is possible that the number of agents can exceed the agent limit. That needs a whole other pr. So this hack to make things work. 
+
+            // it is possible that the number of agents can exceed the agent limit. That needs a whole other pr. So this hack to make things work.
             float agentRatio      =  OwnerEmpire.data.AgentList.Count.UpperBound(EmpireSpyLimit) / (float)EmpireSpyLimit;
-            
-            // here we want to make sure that even if they arent trust worthy that the value they put on war machines will 
+
+            // here we want to make sure that even if they arent trust worthy that the value they put on war machines will
             // get more money.
             float treasuryToSave  = ((0.5f + agentRatio + trustworthiness + militaryRatio) * 0.6f);
             float numAgents       = OwnerEmpire.data.AgentList.Count;
@@ -203,7 +185,7 @@ namespace Ship_Game.AI
 
         private void PlanetBudgetDebugInfo()
         {
-            if (!Empire.Universe.Debug) 
+            if (!Empire.Universe.Debug)
                 return;
 
             var pBudgets = new Array<PlanetBudget>();
@@ -217,22 +199,18 @@ namespace Ship_Game.AI
         }
 
         /// <summary>
-        /// set a target budget of 20 years of growth. 
+        /// set a target budget of 20 years of growth.
         /// </summary>
         public float TreasuryGoal(float normalizedMoney)
         {
-            // calculate income using income at a 100% tax rate - untracked expenditures. 
-            float gross = OwnerEmpire.MaxiumumIncome - OwnerEmpire.TotalCivShipMaintenance -
+            // calculate income using income at a 100% tax rate - untracked expenditures.
+            float gross = OwnerEmpire.MaximumStableIncome - OwnerEmpire.TotalCivShipMaintenance -
                           OwnerEmpire.TroopCostOnPlanets - OwnerEmpire.TotalTroopShipMaintenance;
 
-            float timeSpan = 0;
-            float minimum  = 0;
-
             float treasuryGoal = gross * OwnerEmpire.data.treasuryGoal;
-
-            timeSpan      = 200;
-            treasuryGoal *= timeSpan;
-            return treasuryGoal.LowerBound(minimum);
+            float timeSpan     = 200;
+            treasuryGoal      *= timeSpan;
+            return treasuryGoal.LowerBound(0);
         }
 
         /// <summary>
@@ -240,7 +218,7 @@ namespace Ship_Game.AI
         /// eg. treasury goal is 100, cash on hand is 100, and percent to save is .5
         /// then the result would (100 + 50) / 100 = 1.5
         /// m+m-t*p / t. will always return at least 0.
-        /// 
+        ///
         /// </summary>
         public float OverSpendRatio(float treasuryGoal, float percentageOfTreasuryToSave, float maxRatio)
         {
@@ -267,15 +245,21 @@ namespace Ship_Game.AI
             if (OwnerEmpire.isPlayer && !OwnerEmpire.data.AutoTaxes)
                 return;
 
-            if (OwnerEmpire.Money <= 0)
+            if (money <= 0)
             {
                 OwnerEmpire.data.TaxRate = 1;
                 return;
             }
 
-            float treasuryGap     = treasuryGoal - OwnerEmpire.Money;
+            float treasuryGap = treasuryGoal - money;
+
+            if (treasuryGap < 0)
+            {
+                OwnerEmpire.data.TaxRate = 0;
+                return;
+            }
+
             float treasuryDeficit = treasuryGap > 0 ? treasuryGap : 0;
-            float treasuryExcess  = treasuryGap < 0 ? Math.Abs(treasuryGap) : 0;
 
             // try to meet goal in 20 years.
             // currently logic hits goal in about 10 years.
@@ -283,30 +267,16 @@ namespace Ship_Game.AI
 
             //figure how much is needed to fulfill treasury in timespan and cover current costs
             float neededPerTurnForeTreasury = Math.Max(treasuryDeficit / timeSpan, 0);
-
-            float allSpending = OwnerEmpire.AllSpending;
-            // we cant use grossIncome here because it creates large bouncing in the tax.
-            float baseTax     = (allSpending + neededPerTurnForeTreasury) / OwnerEmpire.MaxiumumIncome * 0.50f;
-
-            // this change amount is the amount needed to make any tax changes. a value less than this will not trigger a change. 
-            float change = baseTax * 0.01f;
-
-            if (treasuryDeficit > change)
+            float closeToGoalCompensator = 1;
+            if (treasuryGoal > 1000f)
             {
-                float netTaxedRevenue = OwnerEmpire.NetIncome;
-
-                float neededGap = netTaxedRevenue - neededPerTurnForeTreasury;
-
-                if (neededGap < change)
-                    OwnerEmpire.data.TaxRate = Math.Max(baseTax, OwnerEmpire.data.TaxRate + change);
-                else if (neededGap > change)
-                    OwnerEmpire.data.TaxRate -= change;
+                // if about 90% to treasury start reducing amount wanted. 
+                float reducer          = treasuryGoal / 9;
+                closeToGoalCompensator = Math.Min(treasuryGap  / reducer, 1);
             }
-            else if (treasuryExcess > change)
-            {
-                float decrease = (OwnerEmpire.data.TaxRate - change).LowerBound(0);
-                OwnerEmpire.data.TaxRate = Math.Min(baseTax, decrease);
-            }
+            float amount = OwnerEmpire.AllSpending + neededPerTurnForeTreasury;
+
+            OwnerEmpire.data.TaxRate = FindTaxRateToReturnAmount(amount * closeToGoalCompensator);
         }
 
         public Array<PlanetBudget> PlanetBudgets;
