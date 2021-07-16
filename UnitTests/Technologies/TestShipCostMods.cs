a123652c3df12baebe6ec6fce4cfbf269696ff77
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-07-16 12:46:40 -0700

    Add stub class for ship tech cost mods

---
+++ UnitTests/Technologies/TestShipCostMods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ship_Game;
+using Ship_Game.AI.Research;
+
+
+namespace UnitTests.Technologies
+{
+    [TestClass]
+    public class TestShipCostMods : StarDriveTest
+    {
+        ChooseTech TechChooser;
+        public TestShipCostMods()
+        {
+            CreateGameInstance();
+            LoadTechContent();
+            LoadStarterShipVulcan();
+            CreateUniverseAndPlayerEmpire();
+            Player.ShipsWeCanBuild.Clear();
+            Enemy.ShipsWeCanBuild.Clear();
+            TechChooser = new ChooseTech(Enemy);
+        }
+
+        [TestMethod]
+        public void LoadShipCostModifiers()
+        {
+            var researchMods = new ResearchOptions();
+            researchMods.LoadResearchOptions(Player);
+            Assert.IsTrue(researchMods.Count() > 0);
+        }
+    }
+}
