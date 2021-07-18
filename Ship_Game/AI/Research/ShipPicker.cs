12d523f2531d684a042043a9e24a3bab846eab76
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-07-17 22:01:47 -0700

    add unit test and make some code more test friendly add a utility with required steps to test ship techs

--- Ship_Game/AI/Research/ShipPicker.cs
+++ Ship_Game/AI/Research/ShipPicker.cs
@@ -56,69 +56,72 @@ namespace Ship_Game.AI.Research
                 if (!tech.Unlocked && tech.Tech.RootNode == 0)
                     minTechCost = Math.Min(techCost, minTechCost);
             }
+            minTechCost = Math.Max(1, minTechCost);
 
             // find cheapest ship to research in current set of ships.
             // adjust cost of some techs to make ships more or less wanted.
-            var pickedShip = ships.FindMin(s =>
-            {
-                float techScore = 0;
-
-                // first adjust cost by the techs in the ship.
-                foreach (string techName in s.shipData.TechsNeeded)
-                {
-                    var tech = empire.GetTechEntry(techName);
-                    if (!tech.Unlocked && tech.Tech.RootNode == 0)
-                    {
-                        var cost = tech.Tech.ActualCost;
-
-                        if (tech.IsTechnologyType(TechnologyType.GroundCombat))              cost *= Options.CostMultiplier(GroundCombat);
-                        if (tech.IsTechnologyType(TechnologyType.ShipHull))                  cost *= Options.CostMultiplier(AllHulls);
-
-                        techScore += cost * Options.CostMultiplier(tech);
-                    }
-                }
-
-                var tags = new Array<WeaponTag>();
-                foreach (var weapon in s.Weapons)
-                {
-                    foreach (var tag in weapon.ActiveWeaponTags)
-                        tags.AddUnique(tag);
-                }
-
-                foreach(var tag in tags)
-                {
-                    techScore *= Options.CostMultiplier(tag);
-                }
-
-
-                // now adjust cost by the role of the ship.
-                switch (s.DesignRole)
-                {
-                    case ShipData.RoleName.platform:
-                    case ShipData.RoleName.station:                                     techScore *= Options.CostMultiplier(Orbitals);   break;
-                    case ShipData.RoleName.colony:                                      techScore *= Options.CostMultiplier(ColonyShip); break;
-                    case ShipData.RoleName.freighter:                                   techScore *= Options.CostMultiplier(Freighter);  break;
-                    case ShipData.RoleName.troopShip when !empire.canBuildTroopShips:   techScore *= Options.CostMultiplier(TroopShip);  break;
-                    case ShipData.RoleName.support   when !empire.canBuildSupportShips: techScore *= Options.CostMultiplier(Support);    break;
-                    case ShipData.RoleName.bomber    when !empire.canBuildBombers:      techScore *= Options.CostMultiplier(Bomber);     break;
-                    case ShipData.RoleName.carrier   when !empire.canBuildCarriers:     techScore *= Options.CostMultiplier(Carrier);    break;
-                }
-
-                // adjust cost by how much it varies from already known tech.
-                float researchDepth               = MostTechs.TryGetValue(s.DesignRole, out int depth) ? depth : 1;
-                float techsResearched             = CountTechsAlreadyResearched(s, KnownTechs);
-                float techRatio                   = Math.Min(researchDepth / techsResearched, 200);
-
-                // adjust cost by how much more expensive it is then the least non ship tech
-                float costRatio  = Math.Min(techScore / minTechCost, 200);
-                float randomBase = techScore * Options.CostMultiplier(Randomize);
-
-                // introduce a random to the cost of the ship to vary what is researched.
-                float random     = randomBase > 0 ? RandomMath.AvgRandomBetween(-randomBase, randomBase) : 0;
-
-                return (int)((techScore + random) * costRatio * techRatio);
-            });
+            var pickedShip = ships.FindMin(s => GetModifiedShipCost(s, empire, minTechCost));
             return pickedShip;
+        }
+
+        public int GetModifiedShipCost(Ship s, Empire empire, float minTechCost)
+        {
+            float techScore = 0;
+
+            // first adjust cost by the techs in the ship.
+            foreach (string techName in s.shipData.TechsNeeded)
+            {
+                var tech = empire.GetTechEntry(techName);
+                if (!tech.Unlocked && tech.Tech.RootNode == 0)
+                {
+                    var cost = tech.Tech.ActualCost;
+
+                    if (tech.IsTechnologyType(TechnologyType.GroundCombat)) cost *= Options.CostMultiplier(GroundCombat);
+                    if (tech.IsTechnologyType(TechnologyType.ShipHull)) cost *= Options.CostMultiplier(AllHulls);
+
+                    techScore += cost * Options.CostMultiplier(tech);
+                }
+            }
+
+            var tags = new Array<WeaponTag>();
+            foreach (var weapon in s.Weapons)
+            {
+                foreach (var tag in weapon.ActiveWeaponTags)
+                    tags.AddUnique(tag);
+            }
+
+            foreach (var tag in tags)
+            {
+                techScore *= Options.CostMultiplier(tag);
+            }
+
+
+            // now adjust cost by the role of the ship.
+            switch (s.DesignRole)
+            {
+                case ShipData.RoleName.platform:
+                case ShipData.RoleName.station: techScore *= Options.CostMultiplier(Orbitals); break;
+                case ShipData.RoleName.colony: techScore *= Options.CostMultiplier(ColonyShip); break;
+                case ShipData.RoleName.freighter: techScore *= Options.CostMultiplier(Freighter); break;
+                case ShipData.RoleName.troopShip when !empire.canBuildTroopShips: techScore *= Options.CostMultiplier(TroopShip); break;
+                case ShipData.RoleName.support when !empire.canBuildSupportShips: techScore *= Options.CostMultiplier(Support); break;
+                case ShipData.RoleName.bomber when !empire.canBuildBombers: techScore *= Options.CostMultiplier(Bomber); break;
+                case ShipData.RoleName.carrier when !empire.canBuildCarriers: techScore *= Options.CostMultiplier(Carrier); break;
+            }
+
+            // adjust cost by how much it varies from already known tech.
+            float researchDepth = MostTechs.TryGetValue(s.DesignRole, out int depth) ? depth : 1;
+            float techsResearched = CountTechsAlreadyResearched(s, KnownTechs);
+            float techRatio = Math.Min(researchDepth / techsResearched, 200);
+
+            // adjust cost by how much more expensive it is then the least non ship tech
+            float costRatio = Math.Min(techScore / (minTechCost * Options.CostMultiplier(BalanceToInfraIntensity)), 200);
+            float randomBase = techScore * Options.CostMultiplier(Randomize);
+
+            // introduce a random to the cost of the ship to vary what is researched.
+            float random = randomBase > 0 ? RandomMath.AvgRandomBetween(-randomBase, randomBase) : 0;
+
+            return (int)((techScore + random) * costRatio * techRatio);
         }
 
         /// <summary>
