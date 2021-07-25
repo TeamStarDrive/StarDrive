7082695fceba7be60adc02e186006fdb3ae6e900
RedFox <jorma.rebane@gmail.com>
2021-07-25 18:09:54 +0300

    deploy: new deploy scripts in Python

---
+++ Deploy/DeployUtils.py
@@ -0,0 +1,22 @@
+import os, sys
+
+def fatal_error(msg): print(msg, file=sys.stderr, flush=True); sys.exit(-1)
+def exit_with_message(msg): print(msg, flush=True); sys.exit(0) # not an error
+
+# allowed CI auto-deploy branches: develop, test/*, release/*
+def should_deploy():
+    branch = appveyor_branch()
+    return branch == "develop" \
+        or branch.startswith("test/") \
+        or branch.startswith("release/")
+
+def env(env_var_name, default=None, fatal=False):
+    var = os.getenv(env_var_name, default=default)
+    if var is None and fatal: fatal_error(f'envvar {env_var_name} undefined')
+    return var
+
+def is_appveyor_build():
+    return env('APPVEYOR_BUILD_FOLDER') is not None
+
+def appveyor_branch():
+    return env('APPVEYOR_REPO_BRANCH', fatal=True)
