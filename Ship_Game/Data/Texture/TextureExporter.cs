bb346c5e4547434a3485f127252d7c6e06a9c33f
RedFox <jorma.rebane@gmail.com>
2021-08-18 00:35:47 +0300

    content: implement Base64 alpha texture import/export

--- Ship_Game/Data/Texture/TextureExporter.cs
+++ Ship_Game/Data/Texture/TextureExporter.cs
@@ -78,5 +78,46 @@ namespace Ship_Game.Data.Texture
                 return false;
             }
         }
+
+        // Takes only the A channel from a Texture2D and converts it into a Base64 string
+        // The texture must be with 1:1 aspect ratio
+        public unsafe string ToBase64StringAlphaOnly(Texture2D texture)
+        {
+            if (texture == null || texture.IsDisposed)
+                return null; // sigh
+
+            if (texture.Width != texture.Height)
+            {
+                Log.Error($"TextureExporter ToBase64String only supports 1:1 aspect ratio but got: {texture.Width}x{texture.Height}");
+                return null;
+            }
+
+            try
+            {
+                // grab a copy of the pixels
+                int numPixels = texture.Width * texture.Height;
+                byte[] rgba = new byte[numPixels * 4];
+                texture.GetData(rgba);
+
+                // allocate temporary buffer for alphas
+                byte[] alphas = new byte[numPixels];
+
+                // copy pixels
+                fixed (byte* pSource = rgba)
+                fixed (byte* pDest = alphas)
+                {
+                    for (int i = 0; i < numPixels; ++i)
+                        pDest[i] = pSource[i*4 + 3]; // dest[i] = color.A
+                }
+
+                // finally to base64, if we had a byte* utility, conversion wouldn't be necessary
+                return Convert.ToBase64String(alphas, Base64FormattingOptions.None);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "TextureExporter ToBase64String failed");
+                return null;
+            }
+        }
     }
 }
