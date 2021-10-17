dc8866c92ec9e511f63d0673b994a7a519813612
Crunchy Gremlin <crunchygremlin@gmail.com>
2021-10-16 20:30:25 -0700

    change != true to == true. Add note that this fix is a possible hack. I dont think the ship should be able to go null at this spot which says something is making it null durring the update before the goal is processed. if this is the way it should be done either the goal should handle it or it should be handled on ship death.

--- Ship_Game/Commands/Goals/ScrapShip.cs
+++ Ship_Game/Commands/Goals/ScrapShip.cs
@@ -96,7 +96,11 @@ namespace Ship_Game.Commands.Goals  // Created by Fat Bastard
 
         GoalStep ImmediateScuttleSelfDestruct()
         {
-            if (OldShip?.Active != true)
+            // Possible Hack. The ship should not be able to go null here. 
+            // the error message was a null ref here "OldShip.ScuttleTimer = 1;" which indicates that the OldShip was null.
+            // there may be a deeper problem.
+
+            if (OldShip?.Active == true)
             {
                 OldShip.ScuttleTimer = 1;
                 OldShip.AI.ClearOrders(AIState.Scuttle, priority: true);
