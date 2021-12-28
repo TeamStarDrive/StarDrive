3bcbf95ee73a491c06b477c28de1b50412babaf9
RedFox <jorma.rebane@gmail.com>
2021-12-28 04:21:26 +0200

    serializer: implement VLQ integer encoding, including signed integer support

--- Ship_Game/Data/Serialization/Types/FloatTypes.cs
+++ Ship_Game/Data/Serialization/Types/FloatTypes.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
 using Ship_Game.Data.Yaml;
 
 namespace Ship_Game.Data.Serialization.Types
