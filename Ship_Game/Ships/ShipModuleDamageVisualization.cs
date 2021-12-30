56034b6b7eece4c1bbc36fde1be01a25bae08493
RedFox <jorma.rebane@gmail.com>
2021-12-31 01:12:13 +0200

    vfx: get junk generation and particle generation under control for ship damage visualization

--- Ship_Game/Ships/ShipModuleDamageVisualization.cs
+++ Ship_Game/Ships/ShipModuleDamageVisualization.cs
@@ -6,7 +6,7 @@ namespace Ship_Game.Ships
     public class ShipModuleDamageVisualization
     {
         private readonly ParticleEmitter Lightning;
-        private readonly ParticleEmitter Trail;
+        private readonly ParticleEmitter Dust;
         private readonly ParticleEmitter Smoke;
         private readonly ParticleEmitter Flame;
         private readonly float LightningVelZ;
@@ -26,36 +26,33 @@ namespace Ship_Game.Ships
             Vector3 center = module.Center3D;
             ShipModuleType type = module.ModuleType;
 
-            float modelZ = module.GetParent().BaseHull.ModelZ;
-            modelZ = modelZ.Clamped(0, 200) * -1;
-
             ParticleManager particles = Empire.Universe.Particles;
 
             switch (type) // FB: other special effects based on some module types, use main moduletypes for performance sake
             {
                 case ShipModuleType.Shield:
-                    Lightning = particles.Sparks.NewEmitter(40f, new Vector3(module.Position, modelZ - 10f));
+                    Lightning = particles.Sparks.NewEmitter(40f, center);
                     LightningVelZ = -6f;
                     break;
                 case ShipModuleType.PowerConduit:
-                    Lightning = particles.Sparks.NewEmitter(25f, new Vector3(module.Position, modelZ - 10f));
+                    Lightning = particles.Sparks.NewEmitter(25f, center);
                     LightningVelZ = -3f;
                     return;
                 case ShipModuleType.PowerPlant:
-                    Lightning = particles.PhotonExplosion.NewEmitter(Area * 6f, center, modelZ);
+                    Lightning = particles.PhotonExplosion.NewEmitter(Area * 6f, center);
                     LightningVelZ = -4;
                     break;
             }
 
             // after all the special cases and removing irrelevant modules, we come to smoke emitters
-            Trail = particles.SmokePlume.NewEmitter(Area * 0.7f, center, modelZ);
-            Smoke = particles.ExplosionSmoke.NewEmitter(Area * 3f, center, modelZ);
+            Dust = particles.SmokePlume.NewEmitter(Area * 0.7f, center);
+            Smoke = particles.ExplosionSmoke.NewEmitter(Area * 3f, center);
 
             // armor doesnt produce flames. 
             if (type != ShipModuleType.Armor)
             {
-                Flame = Area >= 15f ? particles.Flame.NewEmitter(Area * 2 * GlobalStats.DamageIntensity, center, modelZ) :
-                                      particles.SmallFlame.NewEmitter(Area * 3f * GlobalStats.DamageIntensity, center, modelZ);
+                Flame = Area >= 15f ? particles.Flame.NewEmitter(Area * 2 * GlobalStats.DamageIntensity, center) :
+                                      particles.SmallFlame.NewEmitter(Area * 3f * GlobalStats.DamageIntensity, center);
             }
         }
 
@@ -64,12 +61,12 @@ namespace Ship_Game.Ships
         {
             Lightning?.Update(timeStep.FixedTime, center, zVelocity: LightningVelZ);
             //added zaxis offeset contructor. bury flame into model a bit. 
-            Flame?.Update(timeStep.FixedTime, center, zVelocity: Area / -2  - RandomMath.RandomBetween(2f, 6f), zAxisPos: Area, jitter: Area * 2);
+            Flame?.Update(timeStep.FixedTime, center, zVelocity: Area / -2 - RandomMath.RandomBetween(2f, 6f), jitter: Area * 2);
 
             // only spawn smoke from dead modules
             if (isAlive) return;
-            Trail?.Update(timeStep.FixedTime, center, zVelocity: -0.1f);
-            Smoke?.Update(timeStep.FixedTime, center, zVelocity: -2.0f);
+            Dust.Update(timeStep.FixedTime, center, zVelocity: -0.1f);
+            Smoke.Update(timeStep.FixedTime, center, zVelocity: -2.0f);
         }
     }
 }
