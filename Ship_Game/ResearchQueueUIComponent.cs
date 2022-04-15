44ad3da22385c2ef48d9922563feb4e9a8a927a1
RedFox <jorma.rebane@gmail.com>
2022-04-16 00:16:04 +0300

    fix: HandleInput called twice on some UI elements

--- Ship_Game/ResearchQueueUIComponent.cs
+++ Ship_Game/ResearchQueueUIComponent.cs
@@ -80,12 +80,6 @@ namespace Ship_Game
                 return true;
             }
 
-            foreach (ResearchQItem item in ResearchQueueList.AllEntries)
-            {
-                if (item.HandleInput(input))
-                    return true;
-            }
-
             if (ResearchQueueList.Visible && input.RightMouseClick &&
                 ResearchQueueList.HitTest(input.CursorPosition))
             {
