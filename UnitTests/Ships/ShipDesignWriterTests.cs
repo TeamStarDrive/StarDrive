7321936e218714bb1e53ac3d9bcec7d9169a3726
RedFox <jorma.rebane@gmail.com>
2022-04-17 22:03:45 +0300

    perf: implement Integer/Float/Double support for ByteBuffer

--- UnitTests/Ships/ShipDesignWriterTests.cs
+++ UnitTests/Ships/ShipDesignWriterTests.cs
@@ -63,6 +63,38 @@ namespace UnitTests.Ships
         }
 
         [TestMethod]
+        public void ShipDesignWriter_Correctness_Int()
+        {
+            var w = new ShipDesignWriter();
+            w.Write(123456); w.Write(';');
+            Assert.AreEqual("123456;", w.ToString());
+            w.Write(-1231123123); w.Write(';');
+            Assert.AreEqual("123456;-1231123123;", w.ToString());
+            w.Write(2147483647); w.Write(';');
+            Assert.AreEqual("123456;-1231123123;2147483647;", w.ToString());
+        }
+
+        [TestMethod]
+        public void ShipDesignWriter_Correctness_Float()
+        {
+            var w = new ShipDesignWriter();
+            w.Write(1234.5f); w.Write(';');
+            Assert.AreEqual("1234.5;", w.ToString());
+            w.Write(-12312.3125f); w.Write(';');
+            Assert.AreEqual("1234.5;-12312.3125;", w.ToString());
+        }
+
+        [TestMethod]
+        public void ShipDesignWriter_Correctness_Double()
+        {
+            var w = new ShipDesignWriter();
+            w.Write(12345.5432); w.Write(';');
+            Assert.AreEqual("12345.5432;", w.ToString());
+            w.Write(-1234567.54); w.Write(';');
+            Assert.AreEqual("12345.5432;-1234567.54;", w.ToString());
+        }
+
+        [TestMethod]
         public void ShipDesignWriter_Perf_String()
         {
             const int iterations = 1000;
@@ -140,8 +172,6 @@ namespace UnitTests.Ships
             });
         }
 
-
-
         [TestMethod]
         public void ShipDesignWriter_Perf_StrArray()
         {
@@ -181,6 +211,39 @@ namespace UnitTests.Ships
                             w.Append(';');
                     }
                     w.Append('\n');
+                }
+                w.ToString();
+            });
+        }
+
+
+        [TestMethod]
+        public void ShipDesignWriter_Perf_IntFloatDouble()
+        {
+            const int iterations = 1000;
+            const int loopsPerBuffer = 800;
+
+            var sdw = new ShipDesignWriter(); // NOTE: this is the usage pattern in SavedGame.cs
+            Measure("SDW.intfloat", iterations, () =>
+            {
+                sdw.Clear();
+                for (int i = 0; i < loopsPerBuffer; ++i)
+                {
+                    sdw.Write(1337);
+                    sdw.Write(100.50f);
+                    sdw.Write(100.50);
+                }
+                sdw.GetASCIIBytes();
+            });
+            Console.WriteLine($"SDW Capacity = {sdw.Capacity / 1024.0:0.0}KB");
+            Measure("Sb.intfloat", iterations, () =>
+            {
+                var w = new StringBuilder();
+                for (int i = 0; i < loopsPerBuffer; ++i)
+                {
+                    w.Append(1337);
+                    w.Append(100.50f);
+                    w.Append(100.50);
                 }
                 w.ToString();
             });
