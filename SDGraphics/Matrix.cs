b3cff2068c6d33ccae41f349b6fe4b680b0fa109
RedFox <jorma.rebane@gmail.com>
2022-04-25 21:41:30 +0300

    graphics: add Vector2/Vector3/Vector2d/Vector3d implementations into SDGraphics

---
+++ SDGraphics/Matrix.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDGraphics;
+
+// 4x4 Affine transformation matrix, originally based on XNA for compatibility
+public struct Matrix
+{
+    public float M11;
+    public float M12;
+    public float M13;
+    public float M14;
+    public float M21;
+    public float M22;
+    public float M23;
+    public float M24;
+    public float M31;
+    public float M32;
+    public float M33;
+    public float M34;
+    public float M41;
+    public float M42;
+    public float M43;
+    public float M44;
+
+    public static readonly Matrix Identity = new Matrix(1f, 0.0f, 0.0f, 0.0f, 0.0f, 1f, 0.0f, 0.0f, 0.0f, 0.0f, 1f, 0.0f, 0.0f, 0.0f, 0.0f, 1f);
+
+    public Vector3 Up
+    {
+        get
+        {
+            Vector3 up;
+            up.X = M21;
+            up.Y = M22;
+            up.Z = M23;
+            return up;
+        }
+        set
+        {
+            M21 = value.X;
+            M22 = value.Y;
+            M23 = value.Z;
+        }
+    }
+
+    public Vector3 Down
+    {
+        get
+        {
+            Vector3 down;
+            down.X = -M21;
+            down.Y = -M22;
+            down.Z = -M23;
+            return down;
+        }
+        set
+        {
+            M21 = -value.X;
+            M22 = -value.Y;
+            M23 = -value.Z;
+        }
+    }
+
+    public Vector3 Right
+    {
+        get
+        {
+            Vector3 right;
+            right.X = M11;
+            right.Y = M12;
+            right.Z = M13;
+            return right;
+        }
+        set
+        {
+            M11 = value.X;
+            M12 = value.Y;
+            M13 = value.Z;
+        }
+    }
+
+    public Vector3 Left
+    {
+        get
+        {
+            Vector3 left;
+            left.X = -M11;
+            left.Y = -M12;
+            left.Z = -M13;
+            return left;
+        }
+        set
+        {
+            M11 = -value.X;
+            M12 = -value.Y;
+            M13 = -value.Z;
+        }
+    }
+
+    public Vector3 Forward
+    {
+        get
+        {
+            Vector3 forward;
+            forward.X = -M31;
+            forward.Y = -M32;
+            forward.Z = -M33;
+            return forward;
+        }
+        set
+        {
+            M31 = -value.X;
+            M32 = -value.Y;
+            M33 = -value.Z;
+        }
+    }
+
+    public Vector3 Backward
+    {
+        get
+        {
+            Vector3 backward;
+            backward.X = M31;
+            backward.Y = M32;
+            backward.Z = M33;
+            return backward;
+        }
+        set
+        {
+            M31 = value.X;
+            M32 = value.Y;
+            M33 = value.Z;
+        }
+    }
+
+    public Vector3 Translation
+    {
+        get
+        {
+            Vector3 translation;
+            translation.X = M41;
+            translation.Y = M42;
+            translation.Z = M43;
+            return translation;
+        }
+        set
+        {
+            M41 = value.X;
+            M42 = value.Y;
+            M43 = value.Z;
+        }
+    }
+
+    public Matrix(
+        float m11,
+        float m12,
+        float m13,
+        float m14,
+        float m21,
+        float m22,
+        float m23,
+        float m24,
+        float m31,
+        float m32,
+        float m33,
+        float m34,
+        float m41,
+        float m42,
+        float m43,
+        float m44)
+    {
+        M11 = m11;
+        M12 = m12;
+        M13 = m13;
+        M14 = m14;
+        M21 = m21;
+        M22 = m22;
+        M23 = m23;
+        M24 = m24;
+        M31 = m31;
+        M32 = m32;
+        M33 = m33;
+        M34 = m34;
+        M41 = m41;
+        M42 = m42;
+        M43 = m43;
+        M44 = m44;
+    }
+}
---
+++ SDGraphics/Vector2.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using SDUtils;
+
+namespace SDGraphics;
+
+public struct Vector2
+{
+    public float X;
+    public float Y;
+
+    public Vector2(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Vector2(float xy)
+    {
+        X = xy;
+        Y = xy;
+    }
+
+    public static readonly Vector2 Zero = default;
+
+    // Up in the world is -Y, down is +Y
+    public static readonly Vector2 Up = new(0, -1);
+    public static readonly Vector2 Down = new(0, 1);
+    public static readonly Vector2 Left = new(-1, 0);
+    public static readonly Vector2 Right = new(1, 0);
+    public static readonly Vector2 TopLeft = new(-1, -1);
+    public static readonly Vector2 TopRight = new(+1, -1);
+    public static readonly Vector2 BotLeft = new(-1, +1);
+    public static readonly Vector2 BotRight = new(+1, +1);
+
+    public override string ToString()
+    {
+        return $"[{X:0.###}, {Y:0.###}]";
+    }
+
+    public string ToString(int precision)
+    {
+        return $"[{X.String(precision)}, {Y.String(precision)}]";
+    }
+
+    public static implicit operator Microsoft.Xna.Framework.Vector2(in Vector2 v)
+    {
+        return new Microsoft.Xna.Framework.Vector2(v.X, v.Y);
+    }
+
+    [Pure] public Vector2 ToRounded() => new((float)Math.Round(X), (float)Math.Round(Y));
+    [Pure] public float Length() => (float)Math.Sqrt(X*X + Y*Y);
+
+    [Pure] public Vector2 Normalized()
+    {
+        double len = Math.Sqrt(X*X + Y*Y);
+        return len > 0.000001 ? new Vector2((float)(X / len), (float)(Y / len)) : default;
+    }
+
+    [Pure] public double Distance(in Vector2d b)
+    {
+        double dx = X - b.X;
+        double dy = Y - b.Y;
+        return Math.Sqrt(dx*dx + dy*dy);
+    }
+
+    [Pure] public Vector2d DirectionToTarget(Vector2d target)
+    {
+        double dx = target.X - X;
+        double dy = target.Y - Y;
+        double len = Math.Sqrt(dx*dx + dy*dy);
+        if (len.AlmostZero())
+            return new Vector2d(0.0, -1.0); // UP
+        return new Vector2d(dx / len, dy / len);
+    }
+
+    // result between [0, +2PI)
+    [Pure] public float RadiansToTarget(Vector2d target)
+    {
+        return (float)(Math.PI - Math.Atan2(target.X - X, target.Y - Y));
+    }
+
+    // Generates a new point on a circular radius from position
+    // Input angle is given in degrees
+    public Vector2d PointFromAngle(double degrees, double circleRadius)
+    {
+        Vector2d offset = degrees.AngleToDirection() * circleRadius;
+        return new Vector2d(X + offset.X, Y + offset.Y);
+    }
+
+    // assuming this is a direction vector, gives the right side perpendicular vector
+    // @note This assumes that +Y is DOWNWARDS on the screen
+    [Pure] public Vector2d LeftVector()
+    {
+        return new Vector2d(Y, -X);
+    }
+
+    // assuming this is a direction vector, gives the left side perpendicular vector
+    // @note This assumes that +Y is DOWNWARDS on the screen
+    [Pure] public Vector2d RightVector()
+    {
+        return new Vector2d(-Y, X);
+    }
+
+    public static Vector2 operator+(in Vector2 a, in Vector2 b)
+    {
+        return new Vector2(a.X + b.X, a.Y + b.Y);
+    }
+    public static Vector2 operator -(in Vector2 a, in Vector2 b)
+    {
+        return new Vector2(a.X - b.X, a.Y - b.Y);
+    }
+    public static Vector2 operator *(in Vector2 a, in Vector2 b)
+    {
+        return new Vector2(a.X * b.X, a.Y * b.Y);
+    }
+    public static Vector2 operator /(in Vector2 a, in Vector2 b)
+    {
+        return new Vector2(a.X / b.X, a.Y / b.Y);
+    }
+
+    public static Vector2 operator +(in Vector2 a, float f)
+    {
+        return new Vector2(a.X + f, a.Y + f);
+    }
+    public static Vector2 operator -(in Vector2 a, float f)
+    {
+        return new Vector2(a.X - f, a.Y - f);
+    }
+    public static Vector2 operator *(in Vector2 a, float f)
+    {
+        return new Vector2(a.X * f, a.Y * f);
+    }
+    public static Vector2 operator /(in Vector2 a, float f)
+    {
+        return new Vector2(a.X / f, a.Y / f);
+    }
+
+    public static Vector2 operator +(float f, in Vector2 b)
+    {
+        return new Vector2(f + b.X, f + b.Y);
+    }
+    public static Vector2 operator -(float f, in Vector2 b)
+    {
+        return new Vector2(f - b.X, f - b.Y);
+    }
+    public static Vector2 operator *(float f, in Vector2 b)
+    {
+        return new Vector2(f * b.X, f * b.Y);
+    }
+    public static Vector2 operator /(float f, in Vector2 b)
+    {
+        return new Vector2(f / b.X, f / b.Y);
+    }
+}
