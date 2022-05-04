d415542f8a9083f10895434499f9f23523dea81d
Jorma <jorma.rebane@gmail.com>
2022-05-04 23:21:03 +0300

    feature: implement our own Sprite renderer so we can migrate from Xna

---
+++ SDUtils/Memory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDUtils
+{
+    public static class Memory
+    {
+        public static void Dispose<T>(ref T instance) where T : class, IDisposable
+        {
+            if (instance != null)
+            {
+                instance.Dispose();
+                instance = null;
+            }
+        }
+    }
+}
