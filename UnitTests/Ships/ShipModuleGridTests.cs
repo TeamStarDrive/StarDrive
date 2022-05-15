1714f737c7eb1b4fa895c9d2c55b7432f3814759
RedFox <jorma.rebane@gmail.com>
2022-05-15 18:17:53 +0300

    test: expand unit tests for ModuleGrid coordinate conversion

--- UnitTests/Ships/ShipModuleGridTests.cs
+++ UnitTests/Ships/ShipModuleGridTests.cs
@@ -40,7 +40,7 @@ namespace UnitTests.Ships
         [TestMethod]
         public void Regression_StarterShips_ModuleGrid()
         {
-            Ship vulcan = SpawnShip("Vulcan Scout", Player, Vector2.Zero);
+            Ship vulcan = SpawnShip("TEST_Vulcan Scout", Player, Vector2.Zero);
             Assert.AreEqual(4, vulcan.GridWidth);
             Assert.AreEqual(4, vulcan.GridHeight);
 
@@ -49,59 +49,155 @@ namespace UnitTests.Ships
             Assert.AreEqual(30, prototype.GridHeight);
         }
 
+        static Vector2 Vec(float x, float y) => new(x,y);
+        static Point Pos(int x, int y) => new(x,y);
+
         [TestMethod]
         public void WorldToGridLocalCoords()
         {
-            var c = new Vector2(1000, 1000);
-            Ship ship = SpawnShip("Vulcan Scout", Player, c);
+            var c = Vec(1000, 1000);
+            Ship ship = SpawnShip("TEST_Vulcan Scout", Player, c);
             Assert.AreEqual(4, ship.GridWidth);
 
-            Assert.AreEqual(new Vector2(32,32),   ship.WorldToGridLocal( c ));
-            Assert.AreEqual(new Vector2(0,0),     ship.WorldToGridLocal( c - new Vector2(32,32) ));
-            Assert.AreEqual(new Vector2(-32,-32), ship.WorldToGridLocal( c - new Vector2(64,64) ));
-            Assert.AreEqual(new Vector2(64,64),   ship.WorldToGridLocal( c + new Vector2(32,32) ));
+            //  0  16  32  48    center = 32,32
+            // x0  x1  x2  x3
+            // ___|IO_|IO_|___ y0 0
+            // ___|I__|I__|___ y1 16
+            // IO_|I__|IC_|IO_ y2 32
+            // IO_|E__|E__|IO_ y3 48
 
-            Assert.AreEqual(new Point(2,2),   ship.WorldToGridLocalPoint( c ));
-            Assert.AreEqual(new Point(0,0),   ship.WorldToGridLocalPoint( c - new Vector2(32,32) ));
-            Assert.AreEqual(new Point(-2,-2), ship.WorldToGridLocalPoint( c - new Vector2(64,64) ));
-            Assert.AreEqual(new Point(4,4),   ship.WorldToGridLocalPoint( c + new Vector2(32,32) ));
+            Assert.AreEqual(Vec(32,32),   ship.WorldToGridLocal( c ));
+            Assert.AreEqual(Vec(0,0),     ship.WorldToGridLocal( c - Vec(32,32) ));
+            Assert.AreEqual(Vec(-32,-32), ship.WorldToGridLocal( c - Vec(64,64) ));
+            Assert.AreEqual(Vec(64,64),   ship.WorldToGridLocal( c + Vec(32,32) ));
 
-            Assert.AreEqual(new Point(0,0), ship.WorldToGridLocalPointClipped( c - new Vector2(64,64) ));
-            Assert.AreEqual(new Point(3,3), ship.WorldToGridLocalPointClipped( c + new Vector2(32,32) ));
+            Assert.AreEqual(Pos(2,2),   ship.WorldToGridLocalPoint( c ));
+            Assert.AreEqual(Pos(0,0),   ship.WorldToGridLocalPoint( c - Vec(32,32) ));
+            Assert.AreEqual(Pos(-2,-2), ship.WorldToGridLocalPoint( c - Vec(64,64) ));
+            Assert.AreEqual(Pos(4,4),   ship.WorldToGridLocalPoint( c + Vec(32,32) ));
+
+            Assert.AreEqual(Pos(0,0), ship.WorldToGridLocalPointClipped( c - Vec(64,64)  )); // outside TopLeft
+            Assert.AreEqual(Pos(3,0), ship.WorldToGridLocalPointClipped( c + Vec(48,-48) )); // outside TopRight
+            Assert.AreEqual(Pos(3,3), ship.WorldToGridLocalPointClipped( c + Vec(32,32)  )); // outside BotRight
+            Assert.AreEqual(Pos(0,3), ship.WorldToGridLocalPointClipped( c + Vec(-48,48) )); // outside BotLeft
+
+            // one coordinate in range, other out of bounds
+            Assert.AreEqual(Pos(0,2), ship.WorldToGridLocalPointClipped( c - Vec(48,0) ));
+            Assert.AreEqual(Pos(2,0), ship.WorldToGridLocalPointClipped( c - Vec(0,48) ));
+            Assert.AreEqual(Pos(3,2), ship.WorldToGridLocalPointClipped( c + Vec(32,0) ));
+            Assert.AreEqual(Pos(3,2), ship.WorldToGridLocalPointClipped( c + Vec(48,0) ));
+            Assert.AreEqual(Pos(2,3), ship.WorldToGridLocalPointClipped( c + Vec(0,32) ));
+            Assert.AreEqual(Pos(2,3), ship.WorldToGridLocalPointClipped( c + Vec(0,48) ));
+        }
+
+        [TestMethod]
+        public void WorldToGridLocalCoords_RotatedToLeft()
+        {
+            var c = Vec(1000, 1000);
+            Ship ship = SpawnShip("TEST_Vulcan Scout", Player, c);
+            Assert.AreEqual(4, ship.GridWidth);
+            
+            ship.RotationDegrees = -90; // facing towards left
+            ship.UpdateModulePositions(TestSimStep, true, forceUpdate:true);
+
+            // pick a 1x1 module which should be at [0,2]
+            Assert.AreEqual(Pos(1,1), ship.GetModuleAt(0,2).GetSize());
+            // and now check the expected CENTER of the module
+            Assert.That.Equal(0.1f, c + Vec(8,24), ship.GetModuleAt(0,2).Position);
+
+            //  0  16  32  48            0  16  32  48
+            // x0  x1  x2  x3           y0  y1  y2  y3
+            // ___|IO_|IO_|___ y0 0     ___|___|IO_|IO_ x3 48
+            // ___|I__|I__|___ y1 16    IO_|I__|IC_|E__ x2 32
+            // IO_|I__|IC_|IO_ y2 32    IO_|I__|I__|E__ x1 16
+            // IO_|E__|E__|IO_ y3 48    ___|___|IO_|IO_ x0 0
+
+            Assert.AreEqual(Vec(32,32),  ship.WorldToGridLocal( c ));
+            Assert.AreEqual(Vec(64,0),   ship.WorldToGridLocal( c - Vec(32,32) ));
+            Assert.AreEqual(Vec(96,-32), ship.WorldToGridLocal( c - Vec(64,64) ));
+            Assert.AreEqual(Vec(0,64),   ship.WorldToGridLocal( c + Vec(32,32) ));
+
+            Assert.AreEqual(Pos(2,2),  ship.WorldToGridLocalPoint( c ));
+            Assert.AreEqual(Pos(4,0),  ship.WorldToGridLocalPoint( c - Vec(32,32) ));
+            Assert.AreEqual(Pos(6,-2), ship.WorldToGridLocalPoint( c - Vec(64,64) ));
+            Assert.AreEqual(Pos(0,4),  ship.WorldToGridLocalPoint( c + Vec(32,32) ));
+
+            Assert.AreEqual(Pos(3,0), ship.WorldToGridLocalPointClipped( c - Vec(64,64)  )); // outside TopLeft
+            Assert.AreEqual(Pos(3,3), ship.WorldToGridLocalPointClipped( c + Vec(48,-48) )); // outside TopRight
+            Assert.AreEqual(Pos(0,3), ship.WorldToGridLocalPointClipped( c + Vec(32,32)  )); // outside BotRight
+            Assert.AreEqual(Pos(0,0), ship.WorldToGridLocalPointClipped( c + Vec(-48,48) )); // outside BotLeft
+        }
+
+        [TestMethod]
+        public void WorldToGridLocalCoords_Rotated180()
+        {
+            var c = Vec(1000, 1000);
+            Ship ship = SpawnShip("TEST_Vulcan Scout", Player, c);
+            Assert.AreEqual(4, ship.GridWidth);
+
+            ship.RotationDegrees = 180; // facing towards down
+            ship.UpdateModulePositions(TestSimStep, true, forceUpdate:true);
+
+            // pick a 1x1 module which should be at [0,2]
+            Assert.AreEqual(Pos(1,1), ship.GetModuleAt(0,2).GetSize());
+            // and now check the expected CENTER of the module
+            Assert.That.Equal(0.1f, c + Vec(24,-8), ship.GetModuleAt(0,2).Position);
+
+            //  0  16  32  48           48  32  16   0
+            // x0  x1  x2  x3           x3  x2  x1  x0
+            // ___|IO_|IO_|___ y0 0     IO_|E__|E__|IO_ y3 48
+            // ___|I__|I__|___ y1 16    IO_|IC_|I__|IO_ y2 32
+            // IO_|I__|IC_|IO_ y2 32    ___|I__|I__|___ y1 16
+            // IO_|E__|E__|IO_ y3 48    ___|IO_|IO_|___ y0 0
+
+            Assert.AreEqual(Vec(32,32), ship.WorldToGridLocal( c ));
+            Assert.AreEqual(Vec(64,64), ship.WorldToGridLocal( c - Vec(32,32) ));
+            Assert.AreEqual(Vec(96,96), ship.WorldToGridLocal( c - Vec(64,64) ));
+            Assert.AreEqual(Vec(0,0),   ship.WorldToGridLocal( c + Vec(32,32) ));
+
+            Assert.AreEqual(Pos(2,2), ship.WorldToGridLocalPoint( c ));
+            Assert.AreEqual(Pos(4,4), ship.WorldToGridLocalPoint( c - Vec(32,32) ));
+            Assert.AreEqual(Pos(6,6), ship.WorldToGridLocalPoint( c - Vec(64,64) ));
+            Assert.AreEqual(Pos(0,0), ship.WorldToGridLocalPoint( c + Vec(32,32) ));
+
+            Assert.AreEqual(Pos(3,3), ship.WorldToGridLocalPointClipped( c - Vec(64,64)  )); // outside TopLeft
+            Assert.AreEqual(Pos(0,3), ship.WorldToGridLocalPointClipped( c + Vec(48,-48) )); // outside TopRight
+            Assert.AreEqual(Pos(0,0), ship.WorldToGridLocalPointClipped( c + Vec(32,32)  )); // outside BotRight
+            Assert.AreEqual(Pos(3,0), ship.WorldToGridLocalPointClipped( c + Vec(-48,48) )); // outside BotLeft
         }
 
         [TestMethod]
         public void GridLocalToWorldCoords()
         {
-            var c = new Vector2(1000, 1000);
-            Ship ship = SpawnShip("Vulcan Scout", Player, c);
+            var c = Vec(1000, 1000);
+            Ship ship = SpawnShip("TEST_Vulcan Scout", Player, c);
             Assert.AreEqual(4, ship.GridWidth);
 
-            Assert.AreEqual(c + new Vector2(0,0),   ship.GridLocalToWorld( new Vector2(32,32)   ));
-            Assert.AreEqual(c - new Vector2(32,32), ship.GridLocalToWorld( new Vector2(0,0)     ));
-            Assert.AreEqual(c - new Vector2(64,64), ship.GridLocalToWorld( new Vector2(-32,-32) ));
-            Assert.AreEqual(c + new Vector2(32,32), ship.GridLocalToWorld( new Vector2(64,64)   ));
+            Assert.AreEqual(c + Vec(0,0),   ship.GridLocalToWorld( Vec(32,32)   ));
+            Assert.AreEqual(c - Vec(32,32), ship.GridLocalToWorld( Vec(0,0)     ));
+            Assert.AreEqual(c - Vec(64,64), ship.GridLocalToWorld( Vec(-32,-32) ));
+            Assert.AreEqual(c + Vec(32,32), ship.GridLocalToWorld( Vec(64,64)   ));
 
-            Assert.AreEqual(c + new Vector2(0,0),   ship.GridLocalPointToWorld( new Point(2,2)   ));
-            Assert.AreEqual(c - new Vector2(32,32), ship.GridLocalPointToWorld( new Point(0,0)   ));
-            Assert.AreEqual(c - new Vector2(64,64), ship.GridLocalPointToWorld( new Point(-2,-2) ));
-            Assert.AreEqual(c + new Vector2(32,32), ship.GridLocalPointToWorld( new Point(4,4)   ));
+            Assert.AreEqual(c + Vec(0,0),   ship.GridLocalPointToWorld( Pos(2,2)   ));
+            Assert.AreEqual(c - Vec(32,32), ship.GridLocalPointToWorld( Pos(0,0)   ));
+            Assert.AreEqual(c - Vec(64,64), ship.GridLocalPointToWorld( Pos(-2,-2) ));
+            Assert.AreEqual(c + Vec(32,32), ship.GridLocalPointToWorld( Pos(4,4)   ));
         }
 
         [TestMethod]
         public void DesignModuleGrid_WorldToGridLocalCoords()
         {
             Vector2 c = Vector2.Zero;
-            Ship ship = SpawnShip("Vulcan Scout", Player, c);
+            Ship ship = SpawnShip("TEST_Vulcan Scout", Player, c);
             var grid = new DesignModuleGrid(null, ship.ShipData as ShipDesign);
 
             Assert.AreEqual(4, grid.Width);
             Assert.AreEqual(4, grid.Height);
 
-            Assert.AreEqual(new Point(2,2),   grid.WorldToGridPos( c ));
-            Assert.AreEqual(new Point(0,0),   grid.WorldToGridPos( c - new Vector2(32,32) ));
-            Assert.AreEqual(new Point(-2,-2), grid.WorldToGridPos( c - new Vector2(64,64) ));
-            Assert.AreEqual(new Point(4,4),   grid.WorldToGridPos( c + new Vector2(32,32) ));
+            Assert.AreEqual(Pos(2,2),   grid.WorldToGridPos( c ));
+            Assert.AreEqual(Pos(0,0),   grid.WorldToGridPos( c - Vec(32,32) ));
+            Assert.AreEqual(Pos(-2,-2), grid.WorldToGridPos( c - Vec(64,64) ));
+            Assert.AreEqual(Pos(4,4),   grid.WorldToGridPos( c + Vec(32,32) ));
         }
     }
 }
