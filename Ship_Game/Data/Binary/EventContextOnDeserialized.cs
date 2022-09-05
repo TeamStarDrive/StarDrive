d05031fb183bebf4b4535e1b0fca701a53c89eac
RedFox <jorma.rebane@gmail.com>
2022-09-05 22:58:47 +0300

    serialization: use DependencySorter for OnDeserialized event

--- Ship_Game/Data/Binary/EventContextOnDeserialized.cs
+++ Ship_Game/Data/Binary/EventContextOnDeserialized.cs
@@ -9,51 +9,60 @@ namespace Ship_Game.Data.Binary;
 /// </summary>
 public class EventContextOnDeserialized
 {
-    object Root;
-    object[] Objects;
+    readonly object Root;
+    readonly object[] Objects;
 
-    public EventContextOnDeserialized(TypeSerializerMap typeMap, object root, object[] objects)
+    public EventContextOnDeserialized(object root, object[] objects)
     {
         Root = root;
         Objects = objects;
     }
 
-    void CallEvents(UserTypeSerializer.Deserialized deserialized, int count, int baseIndex)
+    class Event
     {
-        for (int i = 0; i < count; ++i)
-        {
-            object instance = Objects[baseIndex + i];
-            deserialized(instance, Root);
-        }
+        public TypeGroup Group;
+        public StarDataDeserialized Attr;
+        public UserTypeSerializer.Deserialized Evt;
     }
-
-    record struct PendingEvent(TypeGroup g, UserTypeSerializer.Deserialized evt, StarDataDeserialized a);
 
     // Invokes events in the order the types were sorted by the Serializer
     // Except that if [StarDataOnDeserialized] defines additional dependencies via `Required`
     // some events will be reordered to satisfy the event dependency order.
     public void InvokeEvents(TypeGroup[] allGroups)
     {
-        Array<PendingEvent> pending = GetPendingEvents(allGroups);
+        Event[] events = GetEvents(allGroups);
 
+        DependencySorter<Event>.Sort(events, e =>
+        {
+            // find all Event instances that match the Type's from Attr.Required
+            return e.Attr.Required?.Select(t => events.Find(d => d.Group.Type.Type == t)!);
+        });
 
+        // Call the events
+        foreach (Event e in events)
+        {
+            for (int i = 0; i < e.Group.Count; ++i)
+            {
+                object instance = Objects[e.Group.BaseIndex + i];
+                e.Evt(instance, Root);
+            }
+        }
     }
 
-    Array<PendingEvent> GetPendingEvents(TypeGroup[] allGroups)
+    Event[] GetEvents(TypeGroup[] allGroups)
     {
-        var pending = new Array<PendingEvent>();
-        foreach (var g in allGroups)
+        var events = new Array<Event>();
+        foreach (TypeGroup g in allGroups)
         {
             if (g.Ser is UserTypeSerializer us)
             {
                 var (evt, a) = us.GetOnDeserializedEvt();
                 if (evt != null)
                 {
-                    pending.Add(new(g, evt, a));
-                    continue;
+                    events.Add(new Event { Group = g, Attr = a, Evt = evt });
                 }
             }
         }
-        return pending;
+        return events.ToArr();
     }
 }
