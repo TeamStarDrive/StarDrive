bec0f60ed81f911f367a1bb6ecff14b841f677ee
RedFox <jorma.rebane@gmail.com>
2022-09-27 04:01:46 +0300

    refactor: flatten file hierarchies

--- Ship_Game/Data/Binary/TypeInfo.cs
+++ Ship_Game/Data/Binary/TypeInfo.cs
@@ -1,44 +1,42 @@
 ﻿using System;
 using Ship_Game.Data.Serialization;
 
-namespace Ship_Game.Data.Binary
+namespace Ship_Game.Data.Binary;
+
+public class TypeInfo
 {
-    public class TypeInfo
+    public readonly ushort StreamTypeId; // TypeId in Stream
+    public readonly string Name;
+    public TypeSerializer Ser; // Actual serializer
+    public readonly FieldInfo[] Fields;
+    public readonly bool IsStruct;
+    public readonly SerializerCategory Category;
+
+    public Type Type => Ser.Type;
+
+    public override string ToString() => $"{StreamTypeId}:{Name}{FieldString}";
+
+    string FieldString => Fields != null ? $" Fields={Fields.Length}" : "";
+
+    public TypeInfo(uint streamTypeId, string name, TypeSerializer s, FieldInfo[] fields,
+                    bool isStruct, SerializerCategory c)
     {
-        public readonly ushort StreamTypeId; // TypeId in Stream
-        public readonly string Name;
-        public TypeSerializer Ser; // Actual serializer
-        public readonly FieldInfo[] Fields;
-        public readonly bool IsStruct;
-        public readonly SerializerCategory Category;
+        StreamTypeId = (ushort)streamTypeId;
+        Name = name;
+        Fields = fields;
+        IsStruct = s?.IsStruct ?? isStruct;
+        Category = c;
+        Ser = s;
 
-        public Type Type => Ser.Type;
-
-        public override string ToString() => $"{StreamTypeId}:{Name}{FieldString}";
-
-        string FieldString => Fields != null ? $" Fields={Fields.Length}" : "";
-
-        public TypeInfo(uint streamTypeId, string name, TypeSerializer s, FieldInfo[] fields,
-                        bool isStruct, SerializerCategory c)
+        if (Fields != null && s is UserTypeSerializer us)
         {
-            StreamTypeId = (ushort)streamTypeId;
-            Name = name;
-            Fields = fields;
-            IsStruct = s?.IsStruct ?? isStruct;
-            Category = c;
-            Ser = s;
-
-            if (Fields != null && s is UserTypeSerializer us)
+            for (uint fieldIdx = 0; fieldIdx < Fields.Length; ++fieldIdx)
             {
-                for (uint fieldIdx = 0; fieldIdx < Fields.Length; ++fieldIdx)
-                {
-                    FieldInfo f = Fields[fieldIdx];
-                    f.Field = us.GetFieldOrNull(f.Name);
-                    if (f.Field != null)
-                        f.Ser = f.Field.Serializer;
-                }
+                FieldInfo f = Fields[fieldIdx];
+                f.Field = us.GetFieldOrNull(f.Name);
+                if (f.Field != null)
+                    f.Ser = f.Field.Serializer;
             }
         }
     }
-
 }
