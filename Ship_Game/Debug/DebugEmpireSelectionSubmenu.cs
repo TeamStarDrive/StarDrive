bbcd3e2c2665956ceaed113cbc120be39bae5a40
RedFox <jorma.rebane@gmail.com>
2022-10-08 16:13:49 +0300

    refactor: a bit of renaming in DebugInfoScreen, and added a placeholder for Empire selection submenu

---
+++ Ship_Game/Debug/DebugEmpireSelectionSubmenu.cs
@@ -0,0 +1,22 @@
+﻿using SDGraphics;
+using Ship_Game.Universe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship_Game.Debug
+{
+    public class DebugEmpireSelectionSubmenu : Submenu
+    {
+        new readonly DebugInfoScreen Parent;
+        UniverseScreen Screen => Parent.Screen;
+        UniverseState Universe => Parent.Screen.UState;
+
+        public DebugEmpireSelectionSubmenu(DebugInfoScreen parent, RectF rect) : base(rect)
+        {
+            Parent = parent;
+        }
+    }
+}
