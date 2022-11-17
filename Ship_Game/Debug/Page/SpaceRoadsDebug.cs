6cee13e7a6a0a3cbb5ac9c50864e4929ab2a8ef9
Fizban1978 <gkapulis@gmail.com>
2022-11-17 22:24:35 +0200

    add space roads debug window

---
+++ Ship_Game/Debug/Page/SpaceRoadsDebug.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+using Microsoft.Xna.Framework.Graphics;
+using SDUtils;
+using Ship_Game.AI.Tasks;
+using Ship_Game.Gameplay;
+
+namespace Ship_Game.Debug.Page;
+
+public class SpaceRoadsDebug : DebugPage
+{
+
+    int Down;
+    int InProgress;
+    int Online;
+    float Maint;
+    float timer;
+    public SpaceRoadsDebug(DebugInfoScreen parent) : base(parent, DebugModes.Tasks)
+    {
+    }
+
+    public override void Draw(SpriteBatch batch, DrawTimes elapsed)
+    {
+        if (!Visible)
+            return;
+
+        int column = 0;
+        foreach (Empire e in Universe.NonPlayerMajorEmpires)
+        {
+            if (!e.data.Defeated)
+            {
+                DrawSpaceRoads(e, column);
+                ++column;
+            }
+        }
+
+        base.Draw(batch, elapsed);
+    }
+
+    void DrawSpaceRoads(Empire e, int column)
+    {
+        Text.SetCursor(Parent.Win.X + 10 + 300 * column, Parent.Win.Y + 200, e.EmpireColor);
+        Text.String("--------------------------");
+        Text.String(e.Name);
+        if (--timer < 0)
+        {
+            (Down, InProgress, Online, Maint) = CountRoads(e.AI.ProjectorHeatMap.Values.ToArray());
+            timer = 60;
+        }
+        Text.String($"Number of Roads: {e.AI.ProjectorHeatMap.Count} - (d{Down}, ip{InProgress}, o{Online}");
+        Text.String($"Total Maintenance/Budget: {Maint.String(2)}/{e.AI.SSPBudget.String(2)}");
+        Text.String("----------------------------");
+        Text.NewLine();
+
+        var spaceRoads = e.AI.ProjectorHeatMap.Values.SortedDescending(r => r.Heat);
+        foreach (SpaceRoad road in spaceRoads)
+        {
+            Text.String($"{road.Name}, #P {road.NumProjectors}, #H {road.Heat}, S {road.Status}");
+        }
+    }
+
+    (int down, int inProgress, int active, float maint) CountRoads(SpaceRoad[]roads)
+    {
+        int down = 0;
+        int inProgress = 0;
+        int online = 0;
+        float maint = 0;
+
+        foreach (SpaceRoad road in roads)
+        {
+            maint += road.Maintenance;
+            switch (road.Status)
+            {
+                case SpaceRoad.SpaceRoadStatus.Down:       down++;       break;
+                case SpaceRoad.SpaceRoadStatus.InProgress: inProgress++; break;
+                case SpaceRoad.SpaceRoadStatus.Online:     online++;     break;
+            }
+        }
+
+        return (down, inProgress, online, maint);
+    }
+    void DrawTask(MilitaryTask t, Empire e)
+    {
+        Color color = t.TargetEmpire?.EmpireColor ?? e.EmpireColor;
+        string target = t.TargetPlanet?.Name ?? t.TargetSystem?.Name ?? "";
+        string fleet = t.Fleet != null ? $"Fleet Step: {t.Fleet.TaskStep}" : "";
+        float str = t.Fleet?.GetStrength() ?? t.MinimumTaskForceStrength;
+        Text.String(color, $"({t.Priority}) {t.Type}, {target}, str: {(int)str}, {fleet}");
+    }
+
+    public override bool HandleInput(InputState input)
+    {
+        return base.HandleInput(input);
+    }
+
+    public override void Update(float fixedDeltaTime)
+    {
+        base.Update(fixedDeltaTime);
+    }
+}
