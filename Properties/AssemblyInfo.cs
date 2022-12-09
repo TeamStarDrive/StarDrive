f0c01057674e0c4b4821b87e62cffd53e7eac4e0
Jorma <jorma.rebane@gmail.com>
2022-12-09 03:21:15 +0200

    release: 1.41.14509

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Zero Sum Games")]
-[assembly: AssemblyCopyright("Copyright © Zero Sum Games 2021")]
+[assembly: AssemblyCopyright("Copyright © Zero Sum Games 2022")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("StarDrive BlackBox")]
 
@@ -26,4 +26,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b38aad3b-18b8-41a8-b758-0e5614dafc49")]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
 
-[assembly: AssemblyInformationalVersion("1.41.14509 release/mars-1.41")]
+[assembly: AssemblyInformationalVersion("1.41.14509 release/mars-1.41/a05317c60")]
