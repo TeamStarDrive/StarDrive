b5385abf671331aabf300bd0dc0354532f2c809c
Jorma <jorma.rebane@gmail.com>
2022-12-19 19:48:18 +0200

    refactor: use event based input in DiplomacyScreen, now it's suddenly much easier to understand

--- Ship_Game/GameScreens/DiplomacyScreen/DialogOptionListItem.cs
+++ Ship_Game/GameScreens/DiplomacyScreen/DialogOptionListItem.cs
@@ -1,28 +1,41 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using SDGraphics;
+using SDUtils;
 
 namespace Ship_Game.GameScreens.DiplomacyScreen
 {
     public class DialogOptionListItem : ScrollListItem<DialogOptionListItem>
     {
         readonly Graphics.Font Font = Fonts.Consolas18;
-        public override int ItemHeight => 24;
+        int TextHeightWithPadding;
+        public override int ItemHeight => TextHeightWithPadding;
+
         public DialogOption Option { get; }
         readonly UILabel Text;
 
-        public DialogOptionListItem(DialogOption option)
+        public DialogOptionListItem(DialogOption option, float maxWidth)
         {
             Option = option;
-            Text = Add(new UILabel($"{Option.Number}. {Option.Words}", Font));
-            Text.DropShadow = true;
-            Text.Color = Color.White;
-            Text.Highlight = Color.LightYellow;
-            Text.TextAlign = TextAlign.VerticalCenter;
+            Text = Add(new UILabel(Font)
+            {
+                DropShadow = true,
+                Color = Color.White,
+                Highlight = Color.LightYellow,
+            });
+        }
+
+        void UpdateMultilineText(float maxWidth)
+        {
+            string text = $"{Option.Number}. {Option.Words}";
+            string[] lines = Font.ParseTextToLines(text, maxWidth);
+            TextHeightWithPadding = lines.Length * 18;
+            Text.MultilineText = new(lines);
         }
 
         public override void PerformLayout()
         {
             Text.Rect = Rect;
+            UpdateMultilineText(Rect.Width);
             base.PerformLayout();
         }
 
