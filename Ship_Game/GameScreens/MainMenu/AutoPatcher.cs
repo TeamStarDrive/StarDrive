652f33c933dc5e5c62a19639b406f424d8270a27
Jorma <jorma.rebane@gmail.com>
2022-12-28 18:25:16 +0200

    updater: implemented all steps for auto-updater

--- Ship_Game/GameScreens/MainMenu/AutoPatcher.cs
+++ Ship_Game/GameScreens/MainMenu/AutoPatcher.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
-using SDGraphics;
 using SDUtils;
+using Color = Microsoft.Xna.Framework.Graphics.Color;
 
 namespace Ship_Game.GameScreens.MainMenu;
 
@@ -21,11 +20,12 @@ internal class AutoPatcher : PopupWindow
 
     UIList ProgressSteps;
 
-    public AutoPatcher(GameScreen screen, in ReleaseInfo info) : base(screen, 500, 180)
+    public AutoPatcher(GameScreen screen, in ReleaseInfo info) : base(screen, 520, 220)
     {
         Screen = screen;
         Info = info;
         TitleText = "StarDrive BlackBox AutoPatcher";
+        CanEscapeFromScreen = false;
     }
 
     public override void LoadContent()
@@ -53,18 +53,32 @@ internal class AutoPatcher : PopupWindow
         return p;
     }
 
+    string GetPatchOutputFolder() => Path.GetFullPath(Path.Combine(Dir.StarDriveAppData, "Patches", Info.Version));
+    string GetPatchTempFolder() => Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "PatchTemp"));
+
+    void TryDeleteFolder(string folder)
+    {
+        try
+        {
+            if (Directory.Exists(folder))
+                Directory.Delete(folder, recursive:true);
+        }
+        catch {}
+    }
+
     void Download(ProgressBarElement dp)
     {
         try
         {
-            string outputFolder = Path.GetFullPath(Path.Combine(Dir.StarDriveAppData, "Patches", Info.Version));
-            if (Directory.Exists(outputFolder))
-                Directory.Delete(outputFolder, recursive:true); // delete all stale data, just in case
+            TryDeleteFolder(folder: GetPatchTempFolder());
+
+            string outputFolder = GetPatchOutputFolder();
+            TryDeleteFolder(outputFolder); // delete all stale data, just in case
             Directory.CreateDirectory(outputFolder);
 
             Log.Write($"Downloading {Info.ZipUrl} to {outputFolder}");
             TimeSpan timeout = TimeSpan.FromMinutes(60);
-            string zipArchive = AutoUpdater.DownloadZip(Info.ZipUrl, outputFolder, CurrentTask, dp.SetProgress, timeout);
+            string zipArchive = AutoUpdateChecker.DownloadZip(Info.ZipUrl, outputFolder, CurrentTask, dp.SetProgress, timeout);
             Log.Write($"Download finished: {outputFolder}");
             
             RunOnNextFrame(() =>
@@ -144,12 +158,20 @@ internal class AutoPatcher : PopupWindow
     {
         try
         {
+            ScreenManager.ResetHotLoadTargets(); // disable hotloading while patcher is running
+            string workingDir = Directory.GetCurrentDirectory();
+            string tempDir = GetPatchTempFolder();
+
             FileInfo[] files = Dir.GetFiles(patchFilesFolder);
             int lastPercent = -1;
             for (int i = 0; i < files.Length; ++i)
             {
-                FileInfo sourceFile = files[i];
-                Log.Write($"Applying Patch: {sourceFile.FullName}");
+                string srcFile = files[i].FullName;
+                string relPath = srcFile.Replace(patchFilesFolder, "");
+                string dstFile = workingDir + relPath;
+                
+                Log.Write($"ApplyPatch: {relPath}");
+                SafeMove(srcFile, dstFile, relPath, tempDir);
 
                 int percent = ProgressBarElement.GetPercent(i+1, files.Length);
                 if (lastPercent != percent)
@@ -169,7 +191,76 @@ internal class AutoPatcher : PopupWindow
         catch (Exception e)
         {
             Log.Error(e, "ApplyPatch failed");
+            RunOnNextFrame(() =>
+            {
+                CanEscapeFromScreen = true;
+
+                var label = ProgressSteps.AddLabel("Apply Patch failed!");
+                label.Color = Color.Red;
+                label.Anim().Alpha(new(0.5f,1.0f)).Loop();
+
+                var details = ProgressSteps.AddLabel(e.Message);
+                details.Color = Color.Red;
+            });
         }
+    }
+
+    /// <summary>
+    /// If the file is in use, it must be moved or renamed,
+    /// however, moving between different drives would cause the file to be copied,
+    /// so we always move it into game/PatchTemp folder
+    /// </summary>
+    static void SafeMove(string srcFile, string dstFile, string relPath, string tempDir)
+    {
+        string tmpFile = null;
+        try
+        {
+            if (File.Exists(dstFile))
+            {
+                tmpFile = MoveToTempPath(tempDir, relPath, dstFile);
+            }
+            MoveAndCreateDirs(srcFile, dstFile);
+        }
+        catch (Exception e)
+        {
+            if (tmpFile != null) // restore the file if needed
+            {
+                File.Move(tmpFile, dstFile);
+            }
+
+            throw new IOException(relPath, e);
+        }
+    }
+
+    /// <summary>
+    /// Moves `theFile` into `tempPath`, returning full path to the temp file,
+    /// so that it can be restored if necessary
+    /// </summary>
+    static string MoveToTempPath(string tempPath, string relPath, string theFile)
+    {
+        string tmpFile = tempPath + relPath;
+        if (File.Exists(tmpFile))
+        {
+            try
+            {
+                File.Delete(tmpFile);
+            }
+            catch
+            {
+                // sometimes even the temp file might still be in use! in that case, copy the OLD temp file
+                string tempTemp = tmpFile + "." + DateTime.Now.Ticks;
+                File.Move(tmpFile, tempTemp);
+            }
+        }
+
+        MoveAndCreateDirs(theFile, tmpFile);
+        return tmpFile;
+    }
+
+    static void MoveAndCreateDirs(string sourceFile, string destinationFile)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(destinationFile)!);
+        File.Move(sourceFile, destinationFile);
     }
 
     void RestartAsync()
