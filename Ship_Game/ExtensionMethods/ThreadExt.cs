7d17442f442ba7d789e762b42bf241da6679dabc
RedFox <jorma.rebane@gmail.com>
2023-01-14 23:38:34 +0200

    performance: optimize a few performance bottlenecks caused by object boxing

--- Ship_Game/ExtensionMethods/ThreadExt.cs
+++ Ship_Game/ExtensionMethods/ThreadExt.cs
@@ -15,25 +15,5 @@ namespace Ship_Game
         {
             return new ScopedWriteLock(writelock);
         }
-
-        // Partitioned parallel range iteration
-        // Use this rather than Parallel.ForEach, to improve iteration performance and avoid callback overhead
-        // @note This is about 3x slower than Ship_Game.Parallel.For, but it's tens of times faster
-        //       than System.Threading.Tasks.Parallel.ForEach
-        public static void ParallelRange<T>(this T[] array, Action<ArrayView<T>> action)
-        {
-            Parallel.For(0, array.Length, (start, end) =>
-            {
-                action(new ArrayView<T>(start, end, array));
-            });
-        }
-
-        public static void ParallelRange<T>(this Array<T> array, Action<ArrayView<T>> action)
-        {
-            Parallel.For(0, array.Count, (start, end) =>
-            {
-                action(array.SubRange(start, end));
-            });
-        }
     }
 }
