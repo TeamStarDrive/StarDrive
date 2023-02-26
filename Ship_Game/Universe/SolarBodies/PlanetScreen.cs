27e328b499ae387c371d0f8c51a77a2e1d072f10
RedFox <jorma.rebane@gmail.com>
2023-02-26 21:05:20 +0200

    feature: much better planet / ship / solarsystem click logic

--- Ship_Game/Universe/SolarBodies/PlanetScreen.cs
+++ Ship_Game/Universe/SolarBodies/PlanetScreen.cs
@@ -1,12 +1,19 @@
-using Microsoft.Xna.Framework.Graphics;
+using System;
+using Ship_Game.Universe;
 
-namespace Ship_Game
+namespace Ship_Game;
+
+public abstract class PlanetScreen : GameScreen
 {
-    public abstract class PlanetScreen : GameScreen
+    public readonly Planet P;
+    public readonly UniverseState Universe;
+    public readonly Empire Player;
+
+    protected PlanetScreen(GameScreen parent, Planet p) : base(parent, toPause: null)
     {
-        protected PlanetScreen(GameScreen parent) : base(parent, toPause: null)
-        {
-            IsPopup = true; // auto-dismiss with right-click
-        }
+        P = p ?? throw new ArgumentNullException(nameof(p));
+        Universe = p.Universe;
+        Player = Universe.Player;
+        IsPopup = true; // auto-dismiss with right-click
     }
 }
