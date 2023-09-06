d293a77017dd9b75b7bedbfca8bb635fe065a2e6
Gilad Kapulis <gkapulis@gmail.com>
2023-09-06 22:07:54 +0300

    more work

--- Ship_Game/Ships/LaunchShip.cs
+++ Ship_Game/Ships/LaunchShip.cs
@@ -13,33 +13,38 @@ namespace Ship_Game.Ships
     {
         [StarData] readonly Ship Owner;
         [StarData] public float Scale;
-        [StarData] float RotationX = 75;
+        [StarData] float RotationDegX = 75;
+        [StarData] float RotationRadZ;
         [StarData] float StartingScale;
         [StarData] readonly float SecondsToZeroX;
         [StarData] readonly float SecondsHalfScale;
         [StarData] readonly float TotalDuration;
         const int DistanceMovementUp = 1000;
         const int MinSecondsToHalfScale = 2;
-        const int MaxSecondsToHalfScale = 5;
+        const int MaxSecondsToHalfScale = 10;
 
 
         [StarDataConstructor]
-        public LaunchShip(Ship owner)
+        public LaunchShip(Ship owner, bool dynamicRotationZ)
         {
             Owner = owner;
             SecondsHalfScale = (DistanceMovementUp / owner.MaxSTLSpeed.LowerBound(100)).Clamped(MinSecondsToHalfScale, MaxSecondsToHalfScale);
-            SecondsToZeroX = RotationX / owner.RotationRadsPerSecond.ToDegrees().LowerBound(5);
+            SecondsToZeroX = RotationDegX / owner.RotationRadsPerSecond.ToDegrees().LowerBound(5);
             TotalDuration = SecondsHalfScale + SecondsToZeroX;
+            RotationRadZ = (45f * ((int)(owner.Universe.StarDate*10) % 10 % 8)).ToRadians();
+            int lala = (int)(owner.Universe.StarDate * 10);
+            int lala2 = lala % 10;
+            int lala3 = lala2 % 8;
         }
 
         public void Update(bool visibleToPlayer, FixedSimTime timeStep)
         {
             Owner.AI.IgnoreCombat = true;
             Owner.YRotation = 0;
-            Owner.Rotation = 270f.ToRadians();
+            Owner.Rotation = RotationRadZ;
             Scale += timeStep.FixedTime / TotalDuration;
             if (Scale >= 0.5f)
-                RotationX = 75 - ((Scale - 0.5f)/0.5f * 75);
+                RotationDegX = 75 - ((Scale - 0.5f)/0.5f * 75);
 
             if (Scale >= 1 && !Owner.IsConstructor && !Owner.IsSupplyShuttle)
                 Owner.AI.IgnoreCombat = false;
@@ -53,7 +58,7 @@ namespace Ship_Game.Ships
                 SO.World = Matrix.CreateTranslation(new Vector3(Owner.ShipData.BaseHull.MeshOffset, 0f))
                              * Matrix.CreateScale(Scale)
                              * Matrix.CreateRotationY(Owner.YRotation)
-                             * Matrix.CreateRotationX(RotationX.ToRadians())
+                             * Matrix.CreateRotationX(RotationDegX.ToRadians())
                              * Matrix.CreateRotationZ(Owner.Rotation)
                              * Matrix.CreateTranslation(new Vector3(Owner.Position, 0f));
                 SO.UpdateAnimation(timeStep.FixedTime);
