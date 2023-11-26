04db2b02f5719e01925eb60bf29d2e571d000c23
Gilad Kapulis <gkapulis@gmail.com>
2023-11-26 03:03:43 +0200

    Initial work on AI mining ops planner

--- Ship_Game/AI/ExpansionAI/MiningOpsPlanner.cs
+++ Ship_Game/AI/ExpansionAI/MiningOpsPlanner.cs
@@ -10,6 +10,7 @@ namespace Ship_Game.AI.ExpansionAI
     public class MiningOpsPlanner
     {
         [StarData] readonly Empire Owner;
+        [StarData] int TurnTimer;
 
 
         UniverseState Universe => Owner.Universe;
@@ -18,6 +19,7 @@ namespace Ship_Game.AI.ExpansionAI
         public MiningOpsPlanner(Empire empire)
         {
             Owner = empire;
+            TurnTimer = 5 + empire.Id;
         }
         InfluenceStatus Influense(Vector2 pos) => Owner.Universe.Influence.GetInfluenceStatus(Owner, pos);
 
@@ -33,62 +35,39 @@ namespace Ship_Game.AI.ExpansionAI
                 return;
 
             Planet[] potentialPlanets = GetPotentialMineablePlanets(ignoreDistance);
+            Goal[] MiningGoals = Owner.AI.FindGoals(g => g is MiningOps);
             foreach (Planet mineable in potentialPlanets)
             {
-                ProcessReserchable(mineable, Influense(mineable.Position));
+                ProcessMinables(mineable, Influense(mineable.Position));
             }
         }
 
-        void ProcessReserchable(ExplorableGameObject solarBody, InfluenceStatus influense)
+        void ProcessMinables(Planet planet, InfluenceStatus influense)
         {
-            if (influense == InfluenceStatus.Enemy)
-                return; // Leave killing research stations to war logic
-
-            SolarSystem system = solarBody.System ?? solarBody as SolarSystem;
-            if (!system.HasPlanetsOwnedBy(Owner) && system.HasPlanetsOwnedByHostiles(Owner))
-                return;
-
-            Planet planet = solarBody as Planet;
-            if (planet != null && !planet.System.InSafeDistanceFromRadiation(planet.Position))
-                return;
-
-            float str = Owner.KnownEnemyStrengthNoResearchStationsIn(system);
-            if (str > 0)
+            if (influense == InfluenceStatus.Enemy
+                || !planet.System.InSafeDistanceFromRadiation(planet.Position)
+                || planet.Mining.OpsOwnedBySomeoneElseThan(Owner))
             {
-                TryClearArea(system, influense, Owner.AI.ThreatMatrix.GetStrongestHostileAt(system), str);
-            }
-            else
-            {
-                if (planet != null)
-                    Owner.AI.AddGoalAndEvaluate(new ProcessResearchStation(Owner, planet));
-                else
-                    Owner.AI.AddGoalAndEvaluate(new ProcessResearchStation(Owner, system, system.SelectStarResearchStationPos()));
-            }
-        }
-
-        void TryClearArea(SolarSystem system, InfluenceStatus influense, Empire enemy, float knownStr)
-        {
-            if (Owner.isPlayer || knownStr > Owner.OffensiveStrength * 0.334f)
                 return;
+            }
 
-            bool shouldClearArea = !Owner.PersonalityModifiers.ClearNeutralExoticSystems && influense == InfluenceStatus.Friendly
-                || Owner.PersonalityModifiers.ClearNeutralExoticSystems && influense <= InfluenceStatus.Friendly
-                || enemy.IsFaction;
-
-            if (shouldClearArea && !Owner.HasWarTaskTargetingSystem(system))
-                Owner.AddDefenseSystemGoal(system, Owner.KnownEnemyStrengthIn(system), Tasks.MilitaryTaskImportance.Normal);
+            if (!planet.Mining.HasOpsOwner)
+                Owner.AI.AddGoalAndEvaluate(new MiningOps(Owner));
         }
 
         bool ShouldRunMiningOpsPlanner()
         {
             if (Universe.P.DisableMiningOps
-                || (Owner.Universe.StarDate % 1).Greater(0)
                 || !Owner.CanBuildMiningStations
                 || Owner.isPlayer && !Owner.AutoBuildMiningStations)
             {
                 return false;
             }
 
+            if (--TurnTimer > 0 )
+                return false;
+
+            TurnTimer = 5;
             return true;
         }
 
@@ -96,27 +75,20 @@ namespace Ship_Game.AI.ExpansionAI
         {
             Array<Planet> mineables = new();
             float averageDist = Owner.AverageSystemsSqdistFromCenter;
-            foreach (Planet planet in Universe.MineablePlanets)
+            for (int i = 0; i < Universe.MineablePlanets.Count; i++)
             {
+                Planet planet = Universe.MineablePlanets[i];
                 if (planet.IsExploredBy(Owner)
-                    && planet.Mining.Owner == null || planet.Mining.Owner == Owner
-                    && (ignoreDistance || InGoodDistance(planet.System, averageDist))
+                    && (!planet.Mining.HasOpsOwner || planet.Mining.OpsOwnedByEmpire(Owner))
+                    && (ignoreDistance || HelperFunctions.InGoodDistanceForReseachOrMiningOps(Owner, planet.System, averageDist, Influense(planet.System.Position)))
                     && (Owner.Universe.Remnants == null
                         || !Owner.Universe.Remnants.AI.HasGoal(g => g is RemnantPortal && g.TargetShip.System == planet.System)))
                 {
                     mineables.Add(planet);
                 }
             }
-            //|| Owner.GetOwnedSystems().Any(s => s.FiveClosestSystems.Any(s => s.HasPlanetsOwnedBy(Owner)))
-            return mineables.ToArray();
 
-            bool InGoodDistance(SolarSystem system, float averageDist)
-            {
-                return system.HasPlanetsOwnedBy(Owner)
-                       || system.Position.SqDist(Owner.WeightedCenter) < averageDist * 1.5f
-                       || system.FiveClosestSystems.Any(s => s.HasPlanetsOwnedBy(Owner))
-                       || Influense(system.Position) == InfluenceStatus.Friendly;
-            }
+            return mineables.ToArray();
         }
     }
 }
