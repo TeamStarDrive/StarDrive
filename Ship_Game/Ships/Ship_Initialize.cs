77151f65c173d1e344b8ab2d00f9af6096258274
Bohdan Smishchenko <bogdan.smishenko@gmail.com>
2024-01-25 22:05:12 +0000

    Update Ship_Game/Ships/Ship_Initialize.cs
    Co-authored-by: Gilad Kapulis <109026976+gkapulis@users.noreply.github.com>

--- Ship_Game/Ships/Ship_Initialize.cs
+++ Ship_Game/Ships/Ship_Initialize.cs
@@ -330,7 +330,8 @@ namespace Ship_Game.Ships
         {
             float randomRadius = owner.Random.Float(p.Radius - 100, p.Radius + 100);
             Ship ship = CreateShipAt(us, shipName, owner, p, p.Position.GenerateRandomPointOnCircle(randomRadius, owner.Random), doOrbit);
-            if (initLaunch && ship != null && !ship.IsPlatformOrStation && !(ship.MaxFTLSpeed == 0 || ship.MaxSTLSpeed == 0))
+            if (initLaunch && ship != null && !ship.IsPlatformOrStation && (ship.MaxFTLSpeed > 0 || ship.MaxSTLSpeed > 0))
+
                 ship.InitLaunch(LaunchPlan.Planet, p);
             else if (initLaunch && ship != null && (ship.MaxFTLSpeed == 0 || ship.MaxSTLSpeed == 0))
             {
