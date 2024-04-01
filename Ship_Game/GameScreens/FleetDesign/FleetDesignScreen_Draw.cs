11dc7156bbbe122fd510ab9dba40281ae83f349a
Bohdan Smishchenko <bogdan.smishenko@gmail.com>
2024-04-01 11:58:24 +0100

    Fix some typos

--- Ship_Game/GameScreens/FleetDesign/FleetDesignScreen_Draw.cs
+++ Ship_Game/GameScreens/FleetDesign/FleetDesignScreen_Draw.cs
@@ -355,7 +355,7 @@ namespace Ship_Game
                 RequisitionForces.Visible = true;
                 SaveDesign.Visible = true;
                 LoadDesign.Visible = true;
-     AutoArrange.Visible = !f.DataNodes.Any(n => n.Ship == null && n.Goal == null);
+                AutoArrange.Visible = !f.DataNodes.Any(n => n.Ship == null && n.Goal == null);
             }
         }
     }
