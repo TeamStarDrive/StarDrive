b8d7c7f93763a45b36e5e61824180c8f7576563e
Gilad Kapulis <gkapulis@gmail.com>
2024-08-15 02:50:09 +0300

    upperbound for terraformer limit

--- Ship_Game/Universe/SolarBodies/Planet/Planet_Generate.cs
+++ Ship_Game/Universe/SolarBodies/Planet/Planet_Generate.cs
@@ -286,7 +286,8 @@ namespace Ship_Game
 
         public bool HasTilesToTerraform     => TilesList.Any(t => t.CanTerraform);
         public bool BioSpheresToTerraform   => TilesList.Any(t => t.BioCanTerraform);
-        public int TerraformerLimit         => Owner != null && Owner.data.Traits.TerraformingLevel < 2 ? 2 : TilesList.Count(t => t.CanTerraform)/2 + 2;
+        public int TerraformerLimit         => Owner != null 
+            && Owner.data.Traits.TerraformingLevel < 2 ? 2 : (TilesList.Count(t => t.CanTerraform)/2 + 2).UpperBound(TileArea/5);
 
         public bool HasTerrainToTerraform => HasBuilding(b => b.CanBeTerraformed);
 
