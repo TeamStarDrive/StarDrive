b0a5db3e0c5032e28031eefafd99c20e5d03ad84
Gilad Kapulis <gkapulis@gmail.com>
2025-06-07 01:52:05 +0300

    align fixrs

--- Ship_Game/GameScreens/NewGame/RaceDesignScreen.cs
+++ Ship_Game/GameScreens/NewGame/RaceDesignScreen.cs
@@ -217,6 +217,7 @@ namespace Ship_Game
 
             Button(ButtonStyle.Military, ScreenWidth - 180, ScreenHeight - 40, GameText.Engage, click: OnEngageClicked);
             Button(ButtonStyle.BigDip, 10, ScreenHeight - 40, GameText.Abort, click: OnAbortClicked);
+
             DoRaceDescription();
             SetRacialTraits(SelectedData.Traits);
 
